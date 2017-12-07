0a031290d0c546167d2ed8dc15d98bccd12cc12c
Salvador Ariza <sariza@thoughtworks.com>
2017-12-07 11:16:35 -0600

    Sal/Brian #9 Add git.heroku.com authentication to setup script

--- scripts/setup-heroku.sh
+++ scripts/setup-heroku.sh
@@ -10,6 +10,9 @@ cat > ~/.netrc <<-EOF
   machine api.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
+  machine git.heroku.com
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
 EOF
 
 cat >> ~/.ssh/config << EOF
