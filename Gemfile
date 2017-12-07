506c4f506118c01da57770a476338c7b3faa3e07
lilyevans <evanslm2@g.cofc.edu>
2017-12-07 15:35:57 -0600

    Ed/Lily #14 Remove sinatra server.

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-ruby '2.4.1'
-
-gem 'puma'
-gem 'sinatra'
--- lib/app.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-configure { set :server, :puma }
-
-set :views, Proc.new { File.join(root, "../build") }
-set :public_folder, Proc.new { File.join(root, "../build") }
-
-get '/' do
-  render :html, :index
-end
