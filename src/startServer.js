be849f48c4833951f928ea7a070856ba36ceb590
Brian Sayler <bsayler@thoughtworks.com>
2017-12-11 14:02:49 -0600

    Ed/Brian #9 - Configure server to listen to PORT env variable for heroku

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,6 @@
 const app = require('./server/server');
 
-app.listen(1234, () => {
+app.listen(process.env.PORT || 1234, () => {
     console.log('Application is listening on port 1234');
     console.log('Please visit http://localhost:1234');
 });
