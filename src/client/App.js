540a861cd9d0a52d9cef2c5c9981e4f3f7510152
Brian Sayler <bsayler@thoughtworks.com>
2017-12-12 11:19:25 -0600

    Brian/Lily #9 add a change to test rollback

--- src/client/App.js
+++ src/client/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload!
         </p>
         <p className="App-change">
-            A small change
+            A <em>large</em> change!
         </p>
       </div>
     );
