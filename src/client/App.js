521e5cecf3d81e368bacc1a3eddfbc0f3196dca3
lilyevans <evanslm2@g.cofc.edu>
2017-12-13 14:19:15 -0600

    Brian/Lily #115 add react-router-dom and material-ui dependencies & add routing to styleguide

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+
+
+const Home = () => (
+    <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React V2</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!
+            To get started, edit <code>src/App.js</code> and save to reload!
         </p>
-      </div>
-    );
-  }
-}
+    </div>
+);
 
+const StyleGuide = () => (
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <AppBar title="Style Guide" />
+    </MuiThemeProvider>
+);
+
+const App = () => (
+    <Router>
+        <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/styleguide" component={StyleGuide}/>
+        </div>
+    </Router>
+)
 export default App;
