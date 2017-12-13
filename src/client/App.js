dcd48ae17d8c7fb54f31b5b585d40eb3be985596
lilyevans <evanslm2@g.cofc.edu>
2017-12-13 14:48:11 -0600

    Brian/Lily #115 remove unused import in App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
