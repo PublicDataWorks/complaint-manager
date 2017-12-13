87d147c978713c2b6a37bdc29a03e8a3c4cb5fda
Chris Kozak <ckozak@gmail.com>
2017-12-13 12:04:55 -0600

    Kozak #9 Breaking the end to end test.  This should stop production deployment but allow staging deployment

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 circleci build --job test
-docker-compose up &
 
+docker-compose build
+docker-compose up &
 circleci build --job e2e-testing -e ENVIRONMENT=local
 docker-compose down
