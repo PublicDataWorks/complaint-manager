216a6d75c3e93f72d2342c8279d50daddf821167
Brian Sayler <bsayler@thoughtworks.com>
2017-12-20 15:38:52 -0600

    Brian/Chris #11 - Add app-db healthcheck endpoint

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
+const models = require('./models');
 
 const app = express();
 const buildDirectory = path.join(__dirname, '../../build');
 
 app.use(express.static(buildDirectory));
+
+app.get('/health-check', (req, res) => {
+    models.case.sequelize
+        .authenticate()
+        .then(() => {
+            res.status(200).send({message: "Success"});
+        })
+        .catch(err => {
+            res.status(500).send({message: "Failed to connect"});
+        });
+});
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(buildDirectory, 'index.html'));
