6aab3eb00a1fbfe911d40bcdcecef0b64378213c
Salvador Ariza <sariza@thoughtworks.com>
2018-01-04 15:17:56 -0600

    Sal/Lily Tech Task - Add Secondary - Lighter as a background color

--- src/client/App.js
+++ src/client/App.js
@@ -10,12 +10,14 @@ import CaseDashboard from "./cases/CaseDashboard";
 import { Provider } from 'react-redux'
 import store from './reduxStore'
 import UserDashboard from './users/UserDashboard'
+import {Paper} from "material-ui";
+import colors from "./globalStyling/colors";
 
 const App = () => (
     <Provider store={store}>
         <Router>
             <MuiThemeProvider theme={customTheme}>
-                <div>
+                <Paper elevation={0} style={{height: '100vh', backgroundColor: colors.secondary[50]}}>
                     <Route exact path="/" component={CaseDashboard} />
                     <Route exact path="/styleguide" component={StyleGuide}/>
                     <Route exact path="/admin" component={UserDashboard}/>
