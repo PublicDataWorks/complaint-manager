1fd3292908430e090f79794d2904f181479ece17
lilyevans <evanslm2@g.cofc.edu>
2018-01-31 13:22:52 -0600

    Lily #190 unregister service worker in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
-import registerServiceWorker from './client/registerServiceWorker';
+import { unregister } from './client/registerServiceWorker';
 import RootContainer from './client/RootContainer';
 
 render(<RootContainer />, document.getElementById('root'));
-registerServiceWorker();
+unregister()
