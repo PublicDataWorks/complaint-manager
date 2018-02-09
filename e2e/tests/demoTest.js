98c8f1d7115b9b500ec037900e4505a3e3f87465
Salvador Ariza <sariza@thoughtworks.com>
2018-02-09 12:02:04 -0600

    Sal/Tom/Kozak #118 - Add e2e test to verify user login works.

--- e2e/tests/demoTest.js
+++ e2e/tests/demoTest.js
@@ -1,9 +1,30 @@
+const TEST_USER = process.env.TEST_USER
+const TEST_PASS = process.env.TEST_PASS
+
+if(!TEST_PASS){
+    console.log("Set the password in the ENV VAR 'TEST_PASS' for login")
+}
+if(!TEST_USER){
+    console.log("Set the username in the ENV VAR 'TEST_USER' for login")
+}
+
 module.exports = {
-  'should see sign-in title' : function (browser) {
-    browser
-        .url("https://noipm-staging.herokuapp.com")
-        .waitForElementVisible("body", 3000)
-        .verify.title('Sign In with Auth0')
-      .end();
-  }
+    "should see sign-in title": (browser) => {
+        browser
+            .url("https://noipm-staging.herokuapp.com")
+            .waitForElementVisible("body", 3000)
+            .verify.title("Sign In with Auth0")
+    },
+
+    "should log-in and authenticate test user": (browser) => {
+        browser
+            .waitForElementVisible("[name=email]", 1000)
+            .setValue("[name=email]", TEST_USER)
+            .setValue("[name=password]", TEST_PASS)
+            .click('button[type=submit]')
+            .waitForElementVisible("[data-test=createCaseButton]", 20000)
+            .verify.title("Complaint Manager")
+            .assert.urlEquals('https://noipm-staging.herokuapp.com/')
+            .end()
+    }
 };
