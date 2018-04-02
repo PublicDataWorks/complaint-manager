83caec57024b6c51826c97bf0ab6bd1ea62c6789
Salvador Ariza <sariza@thoughtworks.com>
2018-04-02 11:45:14 -0500

    Salvador #229 - Modify security check to run before each commit

--- scripts/security-checks.sh
+++ scripts/security-checks.sh
@@ -4,16 +4,23 @@ set +e
 
 ALPINE_VERSION=3.4
 ISSUES_REPORT_FILE=hawkeye_report.json
+TO_EXCLUDE="^src/(server/config/(.)*|server/(server.test.js|(models|handlers/users)/(.)*.test.js))$"
 
 function create_container_with_code() {
-  docker rm -f /target-code
-  docker create -v /target --name target-code alpine:${ALPINE_VERSION} /bin/true;
-  docker cp . target-code:/target;
+    if [  "$(docker ps -a | grep "target-code")" ]
+    then
+         docker rm -f target-code
+         echo "Removing container with target-code.... done"
+    fi
+
+    echo "Bundling target-code in a container...."
+    docker create -v /target --name target-code alpine:${ALPINE_VERSION} /bin/true;
+    docker cp . target-code:/target;
 }
 
 function run_hawkeye_on_container_code() {
   docker rm -f /hawkeye
-  docker run --volumes-from target-code --name hawkeye stono/hawkeye:latest scan /target --json ${ISSUES_REPORT_FILE}
+  docker run --volumes-from target-code --name hawkeye stono/hawkeye:latest scan /target --exclude $TO_EXCLUDE --json ${ISSUES_REPORT_FILE}
   hawkeye_return=$?
 }
 
@@ -37,6 +44,4 @@ else
     echo "Security checks failed. Report is available on artifacts tab."
 fi
 
-#TODO Uncomment this and fix the problems!
-#exit ${hawkeye_return}
-exit 0
+exit ${hawkeye_return}
