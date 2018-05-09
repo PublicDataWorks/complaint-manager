f2e07c4eb5270305db3e02f3b9158089f2b3761a
Brian Sayler <bsayler@thoughtworks.com>
2018-05-09 14:24:05 -0500

    Brian/Sal #272 - Add Complainant and Witness role to cases_officers table

---
+++ src/server/migrations/20180509183807-add-complainant-and-witness-roles-to-cases-officers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.query("ALTER TYPE enum_cases_officers_role_on_case ADD VALUE 'Complainant'");
+        await queryInterface.sequelize.query("ALTER TYPE enum_cases_officers_role_on_case ADD VALUE 'Witness'");
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.sequelize.query(`
+        DELETE 
+        FROM
+            pg_enum
+        WHERE
+            (enumlabel = 'Complainant' OR enumlabel = 'Witness') 
+        AND
+            enumtypid = (
+                SELECT
+                    oid
+                FROM
+                    pg_type
+                WHERE
+                    typname = 'enum_cases_officers_role_on_case'
+            )
+    `);
+    }
+};
