79b0ab7270dc9ea1b3dcb741e75f5bbaf4fe6764
Brian Sayler <bsayler@thoughtworks.com>
2018-05-11 16:46:57 -0500

    Brian - Add Prettier config and pre commit hook

--- scripts/setup-git-hooks.sh
+++ scripts/setup-git-hooks.sh
@@ -2,5 +2,6 @@
 
 ## Install git pre-push hook to your local .git directory
 cd `dirname $0`
-ln -f ./git-hooks/pre-push ../.git/hooks/pre-push
+find ../.git/hooks -type f -not -name '*.sample' | xargs rm
+ln -f -v ./git-hooks/* ../.git/hooks/
 echo "Setting up git-hooks.... done"
