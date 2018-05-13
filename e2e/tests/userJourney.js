694ac49c0e79b41aca39ac4b6f18e20f9d48571f
egonzale <egonzale@thoughtworks.com>
2018-05-13 07:12:15 -0700

    Edgar #251 - Change verify to assert to produce screenshot.

--- e2e/tests/userJourney.js
+++ e2e/tests/userJourney.js
@@ -23,7 +23,7 @@ if (TEST_PASS && TEST_USER && HOST) {
             browser
                 .url(HOST)
                 .waitForElementVisible("body", rerenderWait)
-                .verify.title("Sign In with Auth0")
+                .assert.title("Sign In with Auth0X")
         },
 
         "should authenticate": (browser) => {
