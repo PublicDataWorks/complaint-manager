10a29a32aa3ce04d7ccac2f00c96e2250bb77da4
Chris Kozak <ckozak@gmail.com>
2018-06-28 16:05:35 -0500

    Kozak/Lily #358 Error handler now pushes server errors to New Relic for alerting.

--- src/server/handlers/errorHandler.js
+++ src/server/handlers/errorHandler.js
@@ -1,5 +1,9 @@
+const newRelic = require("newrelic");
+
+
 const errorHandler = (error, request, response, next) => {
   if (error.isServer) {
+     newRelic.recordCustomEvent('ServerErrorEvent', error);
     response
       .status(error.output.statusCode)
       .json({ ...error.output.payload, message: "Something went wrong!" });
