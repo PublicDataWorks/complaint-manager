c73feecdb7582fa4ad72c1ab69592cfdfd4881cf
Phoebe Wooldridge <pwooldri@thoughtworks.com>
2018-08-20 17:01:21 -0500

    Phoebe #383 move add case note button

--- src/client/cases/CaseDetails/CaseNotes/CaseNotes.js
+++ src/client/cases/CaseDetails/CaseNotes/CaseNotes.js
@@ -6,11 +6,11 @@ import { Typography } from "@material-ui/core";
 import RemoveCaseNoteDialog from "../RemoveCaseNoteDialog/RemoveCaseNoteDialog";
 import LinkButton from "../../../shared/components/LinkButton";
 import { Link } from "react-router-dom";
-import { TIMEZONE } from "../../../../sharedUtilities/constants";
-import { initialize } from "redux-form";
+import { connect } from "react-redux";
 import { openCaseNoteDialog } from "../../../actionCreators/casesActionCreators";
 import timezone from "moment-timezone";
-import { connect } from "react-redux";
+import { TIMEZONE } from "../../../../sharedUtilities/constants";
+import { initialize } from "redux-form";
 
 class CaseNotes extends Component {
   componentDidMount() {
@@ -20,44 +20,47 @@ class CaseNotes extends Component {
   render() {
     const { caseNotes, caseId } = this.props;
     return (
-      <div style={{ margin: "0px 24px" }}>
-        <div style={{ display: "flex" }}>
-          <Typography
-            variant={"title"}
-            style={{
-              marginBottom: "16px",
-              flex: 1
-            }}
-          >
-            Case Notes
-          </Typography>
-          {this.props.featureToggles.removePlusButton ? (
-            <LinkButton
-              component={Link}
-              to={`/cases/${this.props.caseId}/history`}
-              style={{ flex: 1, textAlign: "right", marginBottom: "16px" }}
+      <div>
+        <div style={{ margin: "0px 24px" }}>
+          <div style={{ display: "flex" }}>
+            <Typography
+              variant={"title"}
+              style={{
+                marginBottom: "16px",
+                flex: 1
+              }}
             >
-              View Case History
-            </LinkButton>
-          ) : null}
-        </div>
-        <div data-test="caseNotesContainer" style={{ paddingBottom: "16px" }}>
-          {caseNotes.length === 0 ? (
-            <Typography variant="body1">
-              No case notes have been added
+              Case Notes
             </Typography>
-          ) : (
-            _.orderBy(caseNotes, ["actionTakenAt"], "desc").map(activity => {
-              return (
-                <ActivityDisplay
-                  key={activity.id}
-                  activity={activity}
-                  caseId={caseId}
-                  data-test="caseNotesItem"
-                />
-              );
-            })
-          )}
+            {this.props.featureToggles.removePlusButton ? (
+              <LinkButton
+                component={Link}
+                to={`/cases/${this.props.caseId}/history`}
+                style={{ textAlign: "right", marginBottom: "16px" }}
+              >
+                View Case History
+              </LinkButton>
+            ) : null}
+          </div>
+          <div data-test="caseNotesContainer" style={{ paddingBottom: "16px" }}>
+            {caseNotes.length === 0 ? (
+              <Typography variant="body1">
+                No case notes have been added
+              </Typography>
+            ) : (
+              _.orderBy(caseNotes, ["actionTakenAt"], "desc").map(activity => {
+                return (
+                  <ActivityDisplay
+                    key={activity.id}
+                    activity={activity}
+                    caseId={caseId}
+                    data-test="caseNotesItem"
+                  />
+                );
+              })
+            )}
+          </div>
+          <RemoveCaseNoteDialog />
         </div>
         {this.props.featureToggles.removePlusButton ? (
           <LinkButton
@@ -71,6 +74,7 @@ class CaseNotes extends Component {
               );
               this.props.dispatch(openCaseNoteDialog("Add", {}));
             }}
+            style={{ margin: "0% 0% 5% 2%" }}
           >
             + Add Case Note
           </LinkButton>
@@ -79,13 +83,12 @@ class CaseNotes extends Component {
             <LinkButton
               component={Link}
               to={`/cases/${this.props.caseId}/history`}
+              style={{ margin: "0px 24px" }}
             >
               View Case History
             </LinkButton>
           </div>
         )}
-
-        <RemoveCaseNoteDialog />
       </div>
     );
   }
