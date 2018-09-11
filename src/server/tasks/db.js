b27551512f4e94c51c95a4c43edded90540cbbf1
egonzale <egonzale@thoughtworks.com>
2018-09-11 16:44:37 -0500

    Edgar #454 remove unneeded logic for instantiating Sequelize object

--- src/server/tasks/db.js
+++ src/server/tasks/db.js
@@ -3,16 +3,12 @@ var env = process.env.NODE_ENV || "development";
 var config = require("./task_config")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+var sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
