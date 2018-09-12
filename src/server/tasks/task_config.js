d80d9cf986377cad46e6abebb63ddc73e34e366b
egonzale <egonzale@thoughtworks.com>
2018-09-12 08:26:39 -0500

    Edgar #454 add CI config options for task migrations.

--- src/server/tasks/task_config.js
+++ src/server/tasks/task_config.js
@@ -14,6 +14,26 @@ module.exports = {
       acquire: 20000
     }
   },
+  ci: {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    dialectOptions: {
+      ssl: true
+    },
+    dialect: "postgres",
+    operatorsAliases: false,
+    migrationStorageTableName: "sequelize_meta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "sequelize_data",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+      acquire: 20000
+    }
+  },
   staging: {
     host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
