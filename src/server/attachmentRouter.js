df69119afb828862237965a19f5782a150d0748a
Sarah Cnota <scnota@gmail.com>
2018-09-24 10:45:22 -0500

    Sarah #388 remove unused download attachment handler

--- src/server/attachmentRouter.js
+++ src/server/attachmentRouter.js
@@ -1,11 +1,9 @@
 const uploadAttachment = require("./handlers/cases/attachments/uploadAttachment");
-const downloadAttachment = require("./handlers/cases/attachments/downloadAttachment");
 const deleteAttachment = require("./handlers/cases/attachments/deleteAttachment");
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.post("/", uploadAttachment);
-router.get("/:fileName", downloadAttachment);
 router.delete("/:fileName", deleteAttachment);
 
 module.exports = router;
