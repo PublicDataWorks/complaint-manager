67841ec37a09e4962895fec599b3823beffb4e64
Nicholas Felicidario <nicholas.f@thoughtworks.com>
2018-10-22 11:33:51 -0500

    Nick #384 add or to create case page

--- src/client/cases/CreateCaseDialog/CivilianComplainantFields.js
+++ src/client/cases/CreateCaseDialog/CivilianComplainantFields.js
@@ -5,6 +5,7 @@ import LastNameField from "../sharedFormComponents/LastNameField";
 import SuffixField from "../sharedFormComponents/SuffixField";
 import PhoneNumberField from "../sharedFormComponents/PhoneNumberField";
 import EmailField from "../sharedFormComponents/EmailField";
+import { Typography } from "@material-ui/core";
 
 const CivilianComplainantFields = () => (
   <div>
@@ -24,8 +25,20 @@ const CivilianComplainantFields = () => (
       }}
     />
     <br />
+      <div style={{display: "flex"}}>
     <PhoneNumberField name={"civilian.phoneNumber"} />
-    <EmailField name={"civilian.email"} />
+      <Typography
+          variant="button"
+          style={{
+              marginLeft: "22px",
+              marginTop: "22px",
+              marginRight: "22px"
+          }}
+      >
+          OR
+      </Typography>
+      <EmailField name={"civilian.email"} />
+      </div>
   </div>
 );
 
