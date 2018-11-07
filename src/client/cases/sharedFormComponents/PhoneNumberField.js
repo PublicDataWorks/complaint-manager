254d60b926dec294b577a71ecdb99553a5220f1a
Nicholas Felicidario <nicholas.f@thoughtworks.com>
2018-11-07 09:46:10 -0600

    Nick & Phoebe #482 use react text mask for phone number

--- src/client/cases/sharedFormComponents/PhoneNumberField.js
+++ src/client/cases/sharedFormComponents/PhoneNumberField.js
@@ -2,30 +2,46 @@ import React from "react";
 import { Field } from "redux-form";
 import { TextField } from "redux-form-material-ui";
 import { isPhoneNumber } from "../../formFieldLevelValidations";
-import formatPhoneNumberField from "../../utilities/formatPhoneNumberField";
+import MaskedInput from 'react-text-mask';
+import { withStyles } from '@material-ui/core/styles';
 
 function strip_nondigits(value) {
   return value.replace(/[^\d]/g, "");
 }
 
-const PhoneNumberField = props => (
-  <Field
-    name={props.name}
-    component={TextField}
-    label="Phone Number"
-    inputProps={{
-      "data-test": "phoneNumberInput"
-    }}
-    data-test="phoneNumberField"
-    validate={[isPhoneNumber]}
-    normalize={strip_nondigits}
-    style={{ width: "35%", marginRight: "0%", marginBottom: "3%" }}
-    InputLabelProps={{
-      shrink: true
-    }}
-    placeholder="Ex. 1231231234"
-    format={formatPhoneNumberField}
-  />
-);
+const TextMaskCustom = (props) => {
+    const { inputRef, ...other } = props;
+    return (
+        <MaskedInput
+            {...other}
+            ref={inputRef}
+            mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+            placeholderChar={'\u2000'}
+            showMask
+        />
+    );
+};
+
+const PhoneNumberField = (props) => {
+    return (
+        <Field
+            name={props.name}
+            InputProps={{inputComponent: TextMaskCustom}}
+            component={TextField}
+            label="Phone Number"
+            inputProps={{
+                "data-test": "phoneNumberInput"
+            }}
+            data-test="phoneNumberField"
+            validate={[isPhoneNumber]}
+            normalize={strip_nondigits}
+            style={{ width: "35%", marginRight: "0%", marginBottom: "3%" }}
+            InputLabelProps={{
+            shrink: true
+            }}
+            placeholder="Ex. 1231231234"
+        />
+    )
+};
 
 export default PhoneNumberField;
