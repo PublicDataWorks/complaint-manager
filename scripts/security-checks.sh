1c24f19c9c61e037b2464f6240e22fdff9bb1a04
Sarah Cnota <scnota@gmail.com>
2018-12-19 10:23:38 -0600

    Sarah #000 Add note about react-pdf upgrade blocker

--- scripts/security-checks.sh
+++ scripts/security-checks.sh
@@ -4,10 +4,13 @@ set +e
 
 ISSUES_REPORT_FILE=hawkeye_report.json
 TO_EXCLUDE="^src/(server/config/(.)*|server/(server.test.js|(models|handlers/users)/(.)*.test.js))$"
+LIBRARIES_TO_BE_UPGRADED="winston,csv-parse,react-pdf,csv-stringify,react-redux,redux-form"
+# We tried upgrading to react-pdf 4.0.0, but it violates unsafe-eval CSP.
+# This issue tracks it: https://github.com/mozilla/pdf.js/issues/10229. It's closed but has not made it into a release yet.
 
 function run_hawkeye_on_container_code() {
   docker rm -f /hawkeye
-  docker run -v $(pwd):/target --name hawkeye --entrypoint /bin/bash stono/hawkeye:latest -c "hawkeye scan -t /target --exclude \"$TO_EXCLUDE\" --json $ISSUES_REPORT_FILE && npm install -g npm-check-updates && ncu -e 2 --packageFile /target/package.json -x winston,csv-parse,react-pdf,csv-stringify,react-redux,redux-form"
+  docker run -v $(pwd):/target --name hawkeye --entrypoint /bin/bash stono/hawkeye:latest -c "hawkeye scan -t /target --exclude \"$TO_EXCLUDE\" --json $ISSUES_REPORT_FILE && npm install -g npm-check-updates && ncu -e 2 --packageFile /target/package.json -x \"$LIBRARIES_TO_BE_UPGRADED\""
   hawkeye_return=$?
 }
 
