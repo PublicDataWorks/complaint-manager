9698d3ed9e9f0a50fb459cd4bd311768ad707253
Meg Crenshaw <meg.crenshaw@gmail.com>
2018-12-20 09:23:18 -0600

    Bodhi/Meg #510 test for returning complainants by createdAt date in GetCaseWithAllAssociations

--- src/server/handlers/getCaseWithAllAssociations.test.js
+++ src/server/handlers/getCaseWithAllAssociations.test.js
@@ -5,6 +5,8 @@ import ReferralLetter from "../../client/testUtilities/ReferralLetter";
 import { cleanupDatabase } from "../testHelpers/requestTestHelpers";
 import CaseOfficer from "../../client/testUtilities/caseOfficer";
 import Officer from "../../client/testUtilities/Officer";
+import { ACCUSED, COMPLAINANT, WITNESS } from "../../sharedUtilities/constants";
+import Civilian from "../../client/testUtilities/civilian";
 
 describe("getCaseWithAllAssocations", () => {
   let existingCase, referralLetter;
@@ -60,8 +62,8 @@ describe("getCaseWithAllAssocations", () => {
     expect(caseWithAllAssociations.referralLetter).toBeUndefined();
   });
   test("returns accusedOfficers in ascending order of their createdAt date", async () => {
-    await createUnknownCaseOfficer(existingCase);
-    await createKnownCaseOfficer(existingCase);
+    await createUnknownAccusedCaseOfficer(existingCase);
+    await createCaseOfficer(existingCase, ACCUSED, 912);
 
     let caseWithAllAssociations;
     await models.sequelize.transaction(async transaction => {
@@ -75,11 +77,34 @@ describe("getCaseWithAllAssocations", () => {
         caseWithAllAssociations.accusedOfficers[1].createdAt
     ).toEqual(true);
   });
+  test("returns complainants in ascending order of their createdAt date", async () => {
+    await createCaseOfficer(existingCase, COMPLAINANT, 234);
+    await createCaseOfficer(existingCase, COMPLAINANT, 123);
+    await createCivilian(existingCase, COMPLAINANT);
+    await createCivilian(existingCase, COMPLAINANT);
+
+    let caseWithAllAssociations;
+    await models.sequelize.transaction(async transaction => {
+      caseWithAllAssociations = await getCaseWithAllAssociations(
+        existingCase.id,
+        transaction
+      );
+    });
+    expect(
+      caseWithAllAssociations.complainantOfficers[0].createdAt <
+        caseWithAllAssociations.complainantOfficers[1].createdAt
+    ).toEqual(true);
+    expect(
+      caseWithAllAssociations.complainantCivilians[0].createdAt <
+        caseWithAllAssociations.complainantCivilians[1].createdAt
+    ).toEqual(true);
+  });
 });
 
-async function createKnownCaseOfficer(existingCase) {
+async function createCaseOfficer(existingCase, role, officerNumber) {
   const officerAttributes = new Officer.Builder()
     .defaultOfficer()
+    .withOfficerNumber(officerNumber)
     .withId(undefined);
 
   const officer = await models.officer.create(officerAttributes, {
@@ -90,20 +115,33 @@ async function createKnownCaseOfficer(existingCase) {
     .defaultCaseOfficer()
     .withId(undefined)
     .withOfficerId(officer.id)
-    .withCaseId(existingCase.id);
+    .withCaseId(existingCase.id)
+    .withRoleOnCase(role);
 
   await models.case_officer.create(caseOfficerAttributes, {
     auditUser: "someone"
   });
 }
 
-async function createUnknownCaseOfficer(existingCase) {
+async function createUnknownAccusedCaseOfficer(existingCase) {
   const unknownCaseOfficerAttributes = new CaseOfficer.Builder()
     .defaultCaseOfficer()
     .withUnknownOfficer()
     .withCaseId(existingCase.id);
 
   await models.case_officer.create(unknownCaseOfficerAttributes, {
+    auditUser: "someone"
+  });
+}
+
+async function createCivilian(existingCase, role) {
+  const civilianAttributes = new Civilian.Builder()
+    .defaultCivilian()
+    .withCaseId(existingCase.id)
+    .withId(undefined)
+    .withRoleOnCase(role);
+
+  await models.civilian.create(civilianAttributes, {
     auditUser: "someone"
   });
 }
