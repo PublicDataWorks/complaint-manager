e1b39aac46c7a252d75e856ee7bbae0764bac77b
Adam Eichelkraut <adam.eichelkraut@thoughtworks.com>
2018-12-21 12:01:02 -0600

    Adam #529 Address fields are trimmed

--- src/server/models/__tests__/address.test.js
+++ src/server/models/__tests__/address.test.js
@@ -65,4 +65,26 @@ describe("address", () => {
       "very legit street. i mean it"
     );
   });
+
+  test("should trim extra whitespace from fields on save: streetAddress, streetAddress2, additionalLocationInfo", async () => {
+    const initialCase = await createCaseWithoutCivilian();
+
+    const addressToCreate = new Address.Builder()
+      .defaultAddress()
+      .withId(undefined)
+      .withStreetAddress("   123 The mean streets  ")
+      .withStreetAddress2("   Apartment 1337 ")
+      .withAdditionalLocationInfo(" 3rd door down    ")
+      .build();
+
+    await initialCase.createIncidentLocation(addressToCreate, {
+      auditUser: "someone"
+    });
+    await initialCase.reload();
+    const createdIncidentLocation = await initialCase.getIncidentLocation();
+
+    expect(createdIncidentLocation.streetAddress).toEqual("123 The mean streets");
+    expect(createdIncidentLocation.streetAddress2).toEqual("Apartment 1337");
+    expect(createdIncidentLocation.additionalLocationInfo).toEqual("3rd door down");
+  });
 });
