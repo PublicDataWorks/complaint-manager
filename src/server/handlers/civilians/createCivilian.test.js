29ce86cfc0ee645a1b8e1e28de6fd123e1c09a44
Adam Eichelkraut <adam.eichelkraut@thoughtworks.com>
2018-12-21 11:26:56 -0600

    Adam #529 Civilian first and last name trimmed on save

--- src/server/handlers/civilians/createCivilian.test.js
+++ src/server/handlers/civilians/createCivilian.test.js
@@ -94,4 +94,37 @@ describe("createCivilian handler", () => {
 
     expect(civilianAddress).toEqual(null);
   });
+
+  test("should trim extra whitespace from fields: firstName, lastName", async () => {
+    const caseAttributes = new Case.Builder().defaultCase().build();
+    const createdCase = await models.cases.create(caseAttributes, {
+      auditUser: "someone"
+    });
+
+    const civilianValues = {
+      firstName: "      Test White-space ",
+      lastName: "  O'Hare  ",
+      phoneNumber: "1234567890",
+      caseId: createdCase.id
+    };
+
+    const request = httpMocks.createRequest({
+      method: "POST",
+      headers: {
+        authorization: "Bearer SOME_MOCK_TOKEN"
+      },
+      body: civilianValues,
+      nickname: "TEST_USER_NICKNAME"
+    });
+    const response = httpMocks.createResponse();
+
+    await createCivilian(request, response, jest.fn());
+
+    const createdCivilian = await models.civilian.find({
+      where: { caseId: createdCase.id }
+    });
+
+    expect(createdCivilian.firstName).toEqual("Test White-space");
+    expect(createdCivilian.lastName).toEqual("O'Hare");
+  });
 });
