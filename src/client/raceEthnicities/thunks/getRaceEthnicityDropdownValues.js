35fd0a71b59f7c9bd65f8e93ab23ec2c3042f889
Susie Riley <sriley@thoughtworks.com>
2019-01-07 15:35:50 -0600

    Susie/Nich #540 use base URL for getRaceEthnicityDropdownValues

--- src/client/raceEthnicities/thunks/getRaceEthnicityDropdownValues.js
+++ src/client/raceEthnicities/thunks/getRaceEthnicityDropdownValues.js
@@ -4,10 +4,8 @@ import { getRaceEthnicitiesSuccess } from "../../actionCreators/raceEthnicityAct
 import { snackbarError } from "../../actionCreators/snackBarActionCreators";
 
 const getRaceEthnicityDropdownValues = () => async dispatch => {
-  const hostname = config[process.env.REACT_APP_ENV].hostname;
-
   try {
-    const response = await axios.get(`${hostname}/api/race-ethnicities`);
+    const response = await axios.get(`api/race-ethnicities`);
     return dispatch(getRaceEthnicitiesSuccess(response.data));
   } catch (error) {
     return dispatch(
