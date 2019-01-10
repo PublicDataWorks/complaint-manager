8b93595eb567770ce56b6403d4845b7652c88aed
Kamil Markow <kamil.markow@thoughtworks.com>
2019-01-10 10:02:12 -0500

    Kamil #350 Disable button after click on remove person form

--- src/client/cases/thunks/removePerson.js
+++ src/client/cases/thunks/removePerson.js
@@ -4,17 +4,26 @@ import {
   removePersonSuccess
 } from "../../actionCreators/casesActionCreators";
 import axios from "axios";
+import {
+  startSubmit,
+  stopSubmit,
+} from "redux-form";
+import {REMOVE_PERSON_FORM_NAME} from "../../../sharedUtilities/constants";
 
 const removePerson = ({ personType, id, caseId }) => async dispatch => {
   const personTypeForDisplay =
     personType === "civilians" ? "civilian" : "officer";
   try {
+    dispatch(startSubmit(REMOVE_PERSON_FORM_NAME));
     const response = await axios.delete(
       `api/cases/${caseId}/${personType}/${id}`
     );
     dispatch(closeRemovePersonDialog());
-    return dispatch(removePersonSuccess(response.data, personTypeForDisplay));
+    const event = await dispatch(removePersonSuccess(response.data, personTypeForDisplay));
+    dispatch(stopSubmit(REMOVE_PERSON_FORM_NAME));
+    return event;
   } catch (error) {
+    dispatch(stopSubmit(REMOVE_PERSON_FORM_NAME));
     return dispatch(removePersonFailure(personTypeForDisplay));
   }
 };
