e75aca2732cdf1696cf00723496a8c5246672a9f
Kamil Markow <kamil.markow@thoughtworks.com>
2019-01-11 11:39:40 -0500

    Kamil #350 Disable button after click on remove case note

--- src/client/cases/thunks/removeCaseNote.js
+++ src/client/cases/thunks/removeCaseNote.js
@@ -3,16 +3,24 @@ import {
   removeCaseNoteFailure,
   removeCaseNoteSuccess
 } from "../../actionCreators/casesActionCreators";
+import { REMOVE_CASE_NOTE_FORM_NAME } from "../../../sharedUtilities/constants";
 import axios from "axios";
+import {
+  startSubmit,
+  stopSubmit,
+} from 'redux-form';
 
 const removeCaseNote = (caseId, caseNoteId) => async dispatch => {
   try {
+    dispatch(startSubmit(REMOVE_CASE_NOTE_FORM_NAME));
     const response = await axios.delete(
       `api/cases/${caseId}/case-notes/${caseNoteId}`
     );
     dispatch(closeRemoveCaseNoteDialog());
+    dispatch(stopSubmit(REMOVE_CASE_NOTE_FORM_NAME));
     return dispatch(removeCaseNoteSuccess(response.data));
   } catch (error) {
+    dispatch(stopSubmit(REMOVE_CASE_NOTE_FORM_NAME));
     return dispatch(removeCaseNoteFailure());
   }
 };
