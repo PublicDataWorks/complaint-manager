3ae72444667a0fbd85a752f6fa0adab3e9f219c8
Sarah Cnota <scnota@gmail.com>
2019-01-15 12:39:14 -0600

    Sarah #560 add case reference number as model description for case auditing

--- src/server/sequelizeHooks/dataChangeAuditHooks.case.test.js
+++ src/server/sequelizeHooks/dataChangeAuditHooks.case.test.js
@@ -375,7 +375,9 @@ describe("dataChangeAuditHooks", () => {
       expect(auditUpdate.modelId).toEqual(existingCase.id);
       expect(auditUpdate.action).toEqual(AUDIT_ACTION.DATA_UPDATED);
       expect(auditUpdate.user).toEqual("someoneWhoUpdated");
-      expect(auditUpdate.modelDescription).toEqual([]);
+      expect(auditUpdate.modelDescription).toEqual([
+        { "Case Reference": `PO2017-${existingCase.id}` }
+      ]);
     });
 
     test("it saves the changes when only one field has changed and status triggered", async () => {
