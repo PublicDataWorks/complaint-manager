879ab6df2b0002571aa3c7517d7216de8d851850
Susie Riley <sriley@thoughtworks.com>
2019-02-01 12:23:47 -0600

    Revert "Susie #424 Comment out migration tasks that already have run" - migration tasks only run once
    This reverts commit a10621cfabbae2ff3cd18678184d78810f8471cc.

--- src/server/tasks/migrations/002_renumber_case_reference_numbers.js
+++ src/server/tasks/migrations/002_renumber_case_reference_numbers.js
@@ -3,13 +3,9 @@ import renumberCaseReferenceNumbers from "../taskMigrationJobs/renumberCaseRefer
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     //can comment this task out after it runs in case models change in future
-    /*
-    - Task ran in production for the first time on 1/22/2019 in v2.4.0
-    - Task ran for a second time on 1/29/2019 before commented out in v2.6.0
-     */
-    // await renumberCaseReferenceNumbers();
+    await renumberCaseReferenceNumbers();
   },
   down: async () => {
-    // await renumberCaseReferenceNumbers(true);
+    await renumberCaseReferenceNumbers(true);
   }
 };
