cbf536722787e4c7bb1207af7d49dd7416313467
Susie Riley <sriley@thoughtworks.com>
2019-02-01 11:43:46 -0600

    Susie #424 Push failing test migration to ensure build fails (will remove)

---
+++ src/server/migrations/20190201172816-failing-test-migration.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    throw Error("test migration up failed");
+  },
+
+  down: (queryInterface, Sequelize) => {
+    throw Error("test migration down failed");
+  }
+};
