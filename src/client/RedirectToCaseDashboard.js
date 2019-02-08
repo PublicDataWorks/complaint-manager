54b87ae2bd883a453bcbe87b535a7e8ce920dccc
Nicholas Felicidario <nicholas.f@thoughtworks.com>
2019-02-08 17:07:04 -0600

    Nich/Tyler #541 moved all frontend routes into switch and create redirect for invalid routes

---
+++ src/client/RedirectToCaseDashboard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { push } from "connected-react-router";
+import { connect } from "react-redux";
+import { snackbarError } from "./actionCreators/snackBarActionCreators";
+
+class RedirectToCaseDashboard extends React.Component {
+  componentDidMount() {
+    const caseId = this.props.match.params.id;
+    if (!caseId) {
+      this.props.redirectBackToHome();
+    } else {
+      this.props.redirectToCaseDetailsPage(caseId);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+const redirectBackToHome = () => dispatch => {
+  dispatch(push("/"));
+  dispatch(snackbarError("Sorry, that page is not available"));
+};
+
+const redirectToCaseDetailsPage = caseId => dispatch => {
+  dispatch(push(`/cases/${caseId}`));
+  dispatch(snackbarError("Sorry, that page is not available"));
+};
+
+const mapDispatchToProps = {
+  redirectBackToHome,
+  redirectToCaseDetailsPage
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(RedirectToCaseDashboard);
