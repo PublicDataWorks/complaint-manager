1e8caa6e4d16ef7ffadeef974abcb4dfe14d4d55
Susie Riley <sriley@thoughtworks.com>
2019-02-11 10:12:08 -0600

    Susie #595 Try new blue color scheme

--- src/client/globalStyling/colors.js
+++ src/client/globalStyling/colors.js
@@ -1,17 +1,17 @@
 const colors = {
   primary: {
-    light: "#9a67ea",
-    main: "#673ab7",
-    dark: "#320b86"
+    light: "#5472d3",
+    main: "#0d47a1",
+    dark: "#002171"
   },
   secondary: {
     lighter: "#eceff1",
-    light: "rgba(98,117,127,0.4)",
+    light: "#c1d5e0",
     medium: "rgba(0,0,0,0.54)",
     main: "#62757f",
     dark: "#000a12"
   },
-  blue: "#1565c0",
+  blue: "#5472d3",
   yellow: "#fbc02d",
   error: {
     main: "#d32f2f"
