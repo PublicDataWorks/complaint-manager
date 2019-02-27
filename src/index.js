3e2be0de456b99692ba702dff10ab09676176e62
Susie Riley <sriley@thoughtworks.com>
2019-02-27 10:55:12 -0600

    Susie/Tyler #439 Delete registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-import { unregister } from "./client/registerServiceWorker";
 import RootContainer from "./client/RootContainer";
 
 render(<RootContainer />, document.getElementById("root"));
-unregister();
