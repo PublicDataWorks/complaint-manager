6298baf0eec47b52ef48208227bbeee254fff0e2
Susie Riley <sriley@thoughtworks.com>
2019-03-22 16:39:49 -0500

    Susie/Karan #622 Run security checks in circle-ci

--- scripts/hawkeye-container-script.sh
+++ scripts/hawkeye-container-script.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
+
 ISSUES_REPORT_FILE=hawkeye_report.json
 
-#function run_hawkeye_modules() {
-#  mkdir -p /tmp/artifacts/;
-#}
-#
-#run_hawkeye_modules
-
-hawkeye scan --show-code -t /target --json $ISSUES_REPORT_FILE -m node-yarnoutdated -f medium;
+hawkeye scan --show-code -t $(pwd) --json $ISSUES_REPORT_FILE -m node-yarnoutdated -f medium;
 yarn_outdated_return=$?;
-hawkeye scan --show-code -t /target --json $ISSUES_REPORT_FILE -m "files-contents" -m "node-crossenv" -m "node-yarnaudit" -f low | grep -v -P --line-buffered '^\[info\] (?!Checking|Running|Scan complete)';
+hawkeye scan --show-code -t $(pwd) --json $ISSUES_REPORT_FILE -m "files-contents" -m "node-crossenv" -m "node-yarnaudit" -f low | grep -v -P --line-buffered '^\[info\] (?!Checking|Running|Scan complete)';
 yarn_audit_return=${PIPESTATUS[0]};
 
 if [[ ${yarn_outdated_return} -ne 0  || ${yarn_audit_return} -ne 0 ]]
