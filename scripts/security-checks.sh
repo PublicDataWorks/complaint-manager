72932b071e9804fd2a9d6ab42c8abe3eb1ab5784
Susie Riley <sriley@thoughtworks.com>
2019-03-22 16:39:49 -0500

    Susie/Karan #622 Run security checks in circle-ci

--- scripts/security-checks.sh
+++ scripts/security-checks.sh
@@ -2,37 +2,14 @@
 
 set +e
 
-ISSUES_REPORT_FILE=hawkeye_report.json
-# We tried upgrading to react-pdf 4.0.0, but it violates unsafe-eval CSP.
-# This issue tracks it: https://github.com/mozilla/pdf.js/issues/10229. It's closed but has not made it into a release yet.
-
-function run_hawkeye_on_container_code() {
-  docker rm -f /hawkeye
-  docker run -v $(pwd):/target --name hawkeye --entrypoint /bin/bash hawkeyesec/scanner-cli:latest -c "./scripts/hawkeye-container-script.sh";
-  hawkeye_return=$?;
-}
-function create_artifacts_folder() {
-  mkdir -p /tmp/artifacts/;
-}
-
-function copy_report_from_docker_remote_to_artifacts() {
-  docker cp hawkeye:/target/${ISSUES_REPORT_FILE} /tmp/artifacts/${ISSUES_REPORT_FILE}
-}
-
-function remove_hawkeye_report() {
-  rm hawkeye_report.json
-}
-
-run_hawkeye_on_container_code
-create_artifacts_folder
-copy_report_from_docker_remote_to_artifacts
-remove_hawkeye_report
+bash ./scripts/hawkeye-container-script.sh
+hawkeye_return=$?;
 
 if [[ ${hawkeye_return} == 0 ]]
 then
   echo "\033[0;32mSecurity checks passed\033[0m"
 else
-  echo "\033[0;31mSecurity checks failed. Report is available in the directory /tmp/artifacts/.\033[0m"
+  echo "\033[0;31mSecurity checks failed. Report is available in the root directory in hawkeye_report.json.\033[0m"
 fi
 
 exit ${hawkeye_return}
