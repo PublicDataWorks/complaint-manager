ea07143244ede467404067cb9b71ab3881b074b3
Tyler Pearson <tyler.pearson@thoughtworks.com>
2019-04-01 10:51:28 -0500

    Karan/Tyler #304 Enhanced search results component in prepartion for use with case dashboard.

--- src/client/shared/components/SearchResults.test.js
+++ src/client/shared/components/SearchResults.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme/build/index";
 import SearchResults from "./SearchResults";
 import Pagination from "rc-pagination";
+import { mount } from "enzyme";
 
 describe("SearchResults", () => {
   test("should display spinner when spinnerVisible is true", () => {
@@ -95,6 +96,106 @@ describe("SearchResults", () => {
         .children()
         .text()
     ).toEqual("2 results found");
+  });
+
+  test("should not show result count in subtitle when 1 or more case(s) exists", () => {
+    const wrapper = mount(
+      <SearchResults
+        spinnerVisible={false}
+        searchResults={[
+          { firstName: "bob", id: 1 },
+          { firstName: "joan", id: 2 }
+        ]}
+        searchResultsIds={[1, 2]}
+        render={jest.fn()}
+        subtitleResultCount={false}
+      />
+    );
+    const containsText = wrapper
+      .find("[data-test='searchResultsMessage'] p")
+      .first();
+
+    return expect(containsText.text()).toBeFalsy();
+  });
+
+  test("should show correct no results message when passed in as parameter", () => {
+    const noResultsMessage = "Test No Results Message";
+    const wrapper = shallow(
+      <SearchResults
+        spinnerVisible={false}
+        searchResults={[]}
+        render={jest.fn()}
+        noResultsMessage={noResultsMessage}
+      />
+    );
+    const resultsMessage = wrapper
+      .find("[data-test='searchResultsMessage']")
+      .children()
+      .text();
+
+    return expect(resultsMessage).toEqual(noResultsMessage);
+  });
+
+  test("should show default no results message when no parameter is passed.", () => {
+    const defaultNoResultsMessage = "No results found";
+    const wrapper = shallow(
+      <SearchResults
+        spinnerVisible={false}
+        searchResults={[]}
+        render={jest.fn()}
+      />
+    );
+    const resultsMessage = wrapper
+      .find("[data-test='searchResultsMessage']")
+      .children()
+      .text();
+
+    return expect(resultsMessage).toEqual(defaultNoResultsMessage);
+  });
+
+  test("should show correct header when supplied", () => {
+    const testHeader = "Special Header";
+    const wrapper = shallow(
+      <SearchResults
+        header={testHeader}
+        spinnerVisible={false}
+        searchResults={[
+          { firstName: "bob", id: 1 },
+          { firstName: "joanne", id: 2 }
+        ]}
+        searchResultsIds={[1, 2]}
+        render={jest.fn()}
+      />
+    );
+
+    const header = wrapper
+      .find("[data-test='searchHeader']")
+      .children()
+      .text();
+
+    return expect(header).toEqual(testHeader);
+  });
+
+  test("should show default header when no header is supplied", () => {
+    const defaultHeader = "Search Results";
+    const wrapper = shallow(
+      <SearchResults
+        spinnerVisible={false}
+        searchResults={[
+          { firstName: "bob", id: 1 },
+          { firstName: "joanne", id: 2 }
+        ]}
+        searchResultsIds={[1, 2]}
+        render={jest.fn()}
+      />
+    );
+
+    const header = wrapper
+      .find("[data-test='searchHeader']")
+      .children()
+      .text();
+
+    return expect(header).toEqual(defaultHeader);
   });
 
   test("should not find pagination component when not paginating", () => {
