0e9f9907d06826efe977512b4285955de3ec977d
Tyler Pearson <tyler.pearson@thoughtworks.com>
2019-04-02 14:14:39 -0500

    Karan/Tyler #000 Updated security scripts to only warn, not fail, on out of date packages

--- scripts/security-checks.sh
+++ scripts/security-checks.sh
@@ -7,9 +7,13 @@ hawkeye_return=$?;
 
 if [[ ${hawkeye_return} == 0 ]]
 then
-  echo "\033[0;32mSecurity checks passed\033[0m"
+  echo -e "\033[0;32mSecurity checks passed\033[0m"
+elif [[ ${hawkeye_return} == 2 ]]
+then
+  echo -e "\033[0;93mThere are package updates available, consider upgrading.\033[0m"
+  exit 0
 else
-  echo "\033[0;31mSecurity checks failed. Report is available in the root directory in hawkeye_report.json.\033[0m"
+  echo -e "\033[0;31mSecurity checks failed. Report is available in the root directory in hawkeye_report.json.\033[0m"
 fi
 
 exit ${hawkeye_return}
