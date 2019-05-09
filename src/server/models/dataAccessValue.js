ab68b2f9d0de4596830b63bf0862afa3512bf18d
Susie Riley <sriley@thoughtworks.com>
2019-05-09 11:13:40 -0500

    Susie/Tyler [#165801763] Create models for data access audits

---
+++ src/server/models/dataAccessValue.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const DataAccessValue = sequelize.define(
+    "data_access_value",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      association: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      fields: {
+        allowNull: false,
+        type: DataTypes.JSONB
+      }
+    },
+    {
+      tableName: "data_access_value",
+      timestamps: false
+    }
+  );
+
+  DataAccessValue.associate = models => {
+    DataAccessValue.belongsTo(models.data_access_audit, {
+      as: "dataAccessValues",
+      foreignKey: { name: "dataAccessAuditId", field: "data_access_audit_id" }
+    });
+  };
+
+  return DataAccessValue;
+};
