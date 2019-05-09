db0831c59f0e6782079968880068b461913171ed
Susie Riley <sriley@thoughtworks.com>
2019-05-09 17:10:12 -0500

    Susie/Tyler [#165801763] Implement new auditing for getWorkingCases handler

--- src/server/handlers/cases/getCases/getWorkingCases.request.test.js
+++ src/server/handlers/cases/getCases/getWorkingCases.request.test.js
@@ -1,27 +1,22 @@
 import {
   buildTokenWithPermissions,
-  cleanupDatabase,
-  expectResponse
+  cleanupDatabase
 } from "../../../testHelpers/requestTestHelpers";
 import request from "supertest";
 import app from "../../../server";
-import models from "../../../models";
 import {
-  AUDIT_ACTION,
-  AUDIT_SUBJECT,
-  AUDIT_TYPE,
-  CASE_STATUS,
-  COMPLAINANT,
   DESCENDING,
   SORT_CASES_BY
 } from "../../../../sharedUtilities/constants";
-import Civilian from "../../../../client/testUtilities/civilian";
-import Officer from "../../../../client/testUtilities/Officer";
-import CaseOfficer from "../../../../client/testUtilities/caseOfficer";
-import Case from "../../../../client/testUtilities/case";
-import moment from "moment";
+import getWorkingCases from "./getWorkingCases";
 
 jest.mock("../export/jobQueue");
+
+jest.mock("./getWorkingCases", () =>
+  jest.fn((request, response, next) => {
+    response.send();
+  })
+);
 
 describe("getWorkingCases", () => {
   let token;
@@ -38,133 +33,13 @@ describe("getWorkingCases", () => {
   });
 
   describe("GET /cases", () => {
-    test("should audit data access", async () => {
-      const responsePromise = request(app)
+    test("should call getWorkingCases", async () => {
+      await request(app)
         .get(`/api/cases?sortBy=${sortBy}&sortDirection=${sortDirection}`)
         .set("Content-Header", "application/json")
         .set("Authorization", `Bearer ${token}`);
 
-      await expectResponse(responsePromise, 200);
-
-      const audit = await models.action_audit.findOne({
-        where: { subject: AUDIT_SUBJECT.ALL_WORKING_CASES }
-      });
-
-      expect(audit).toEqual(
-        expect.objectContaining({
-          auditType: AUDIT_TYPE.DATA_ACCESS,
-          action: AUDIT_ACTION.DATA_ACCESSED,
-          subject: AUDIT_SUBJECT.ALL_WORKING_CASES,
-          user: "some_nickname"
-        })
-      );
-    });
-
-    test("should get all cases", async () => {
-      const civilian = new Civilian.Builder()
-        .defaultCivilian()
-        .withId(undefined)
-        .withCreatedAt(new Date("2016-03-01"))
-        .withNoAddress()
-        .withFirstName("Robert")
-        .build();
-
-      const officer = new Officer.Builder()
-        .defaultOfficer()
-        .withFirstName("Jeff")
-        .withOfficerNumber(123)
-        .withId(undefined)
-        .build();
-      const createdOfficer = await models.officer.create(officer);
-
-      const officerComplainant = new Officer.Builder()
-        .defaultOfficer()
-        .withOfficerNumber(321)
-        .withFirstName("Marty")
-        .withId(undefined)
-        .build();
-      const createdOfficerComplainant = await models.officer.create(
-        officerComplainant
-      );
-
-      const accusedOfficer = new CaseOfficer.Builder()
-        .defaultCaseOfficer()
-        .withId(undefined)
-        .withOfficerAttributes(createdOfficer)
-        .withNoSupervisor()
-        .build();
-
-      const complainantOfficer = new CaseOfficer.Builder()
-        .defaultCaseOfficer()
-        .withId(undefined)
-        .withOfficerAttributes(createdOfficerComplainant)
-        .withNoSupervisor()
-        .withCreatedAt(new Date("2018-02-01"))
-        .withRoleOnCase(COMPLAINANT)
-        .build();
-
-      const defaultCase = new Case.Builder()
-        .defaultCase()
-        .withId(undefined)
-        .withComplainantCivilians([civilian])
-        .withAccusedOfficers([accusedOfficer])
-        .withComplainantOfficers([complainantOfficer])
-        .withAttachments(undefined)
-        .withIncidentLocation(undefined)
-        .withWitnessOfficers(undefined)
-        .withWitnessCivilians(undefined)
-        .build();
-
-      const seededCase = await models.cases.create(defaultCase, {
-        include: [
-          {
-            model: models.civilian,
-            as: "complainantCivilians",
-            auditUser: "someone"
-          },
-          {
-            model: models.case_officer,
-            as: "accusedOfficers",
-            auditUser: "someone"
-          },
-          {
-            model: models.case_officer,
-            as: "complainantOfficers",
-            auditUser: "someone"
-          }
-        ],
-        auditUser: "someone"
-      });
-
-      const responsePromise = request(app)
-        .get(`/api/cases?sortBy=${sortBy}&sortDirection=${sortDirection}`)
-        .set("Content-Header", "application/json")
-        .set("Authorization", `Bearer ${token}`);
-
-      await expectResponse(
-        responsePromise,
-        200,
-        expect.objectContaining({
-          cases: expect.objectContaining({
-            count: 1,
-            rows: expect.arrayContaining([
-              expect.objectContaining({
-                complainantFirstName:
-                  seededCase.complainantCivilians[0].firstName,
-                complainantLastName:
-                  seededCase.complainantCivilians[0].lastName,
-                accusedFirstName: officer.firstName,
-                complaintType: seededCase.complaintType,
-                firstContactDate: moment(seededCase.firstContactDate).format(
-                  "YYYY-MM-DD"
-                ),
-                status: CASE_STATUS.INITIAL,
-                assignedTo: "tuser"
-              })
-            ])
-          })
-        })
-      );
+      expect(getWorkingCases).toHaveBeenCalled();
     });
   });
 });
--- src/server/handlers/cases/getCases/getWorkingCases.test.js
+++ src/server/handlers/cases/getCases/getWorkingCases.test.js
@@ -10,21 +10,18 @@ import {
 import legacyAuditDataAccess from "../../legacyAuditDataAccess";
 import getCases, { CASES_TYPE, GET_CASES_AUDIT_DETAILS } from "./getCases";
 import checkFeatureToggleEnabled from "../../../checkFeatureToggleEnabled";
+import mockFflipObject from "../../../testHelpers/mockFflipObject";
+import auditDataAccess from "../../auditDataAccess";
 
 const httpMocks = require("node-mocks-http");
 
 jest.mock("../../legacyAuditDataAccess");
+jest.mock("../../auditDataAccess");
 
 jest.mock("./getCases");
 
-jest.mock("../../../checkFeatureToggleEnabled");
-
 getCases.mockImplementation((caseType, sortBy, sortDirection, transaction) => {
   return "MOCK_GET_CASES";
-});
-
-checkFeatureToggleEnabled.mockImplementation((request, featureName) => {
-  return featureName === "caseDashboardPaginationFeature";
 });
 
 describe("getWorkingCases", () => {
@@ -44,6 +41,9 @@ describe("getWorkingCases", () => {
       headers: {
         authorization: "Bearer token"
       },
+      fflip: mockFflipObject({
+        caseDashboardPaginationFeature: true
+      }),
       query: {
         sortBy: "by",
         sortDirection: "direction",
@@ -70,27 +70,60 @@ describe("getWorkingCases", () => {
     );
   });
 
-  test("Should call legacyAuditDataAccess with auditDetails", async () => {
-    const request = httpMocks.createRequest({
-      method: "GET",
-      headers: {
-        authorization: "Bearer token"
-      },
-      nickname: "nickname"
+  describe("newAuditFeature enabled", () => {
+    test("should call auditDataAccess with correct arguments", async () => {
+      const request = httpMocks.createRequest({
+        method: "GET",
+        headers: {
+          authorization: "Bearer token"
+        },
+        fflip: mockFflipObject({
+          newAuditFeature: true
+        }),
+        nickname: "nickname"
+      });
+
+      const response = httpMocks.createResponse();
+      const next = jest.fn();
+
+      await getWorkingCases(request, response, next);
+
+      expect(auditDataAccess).toHaveBeenCalledWith(
+        request.nickname,
+        null,
+        AUDIT_SUBJECT.ALL_WORKING_CASES,
+        GET_CASES_AUDIT_DETAILS,
+        expect.anything()
+      );
     });
+  });
 
-    const response = httpMocks.createResponse();
-    const next = jest.fn();
+  describe("new AuditFeature disabled", () => {
+    test("Should call legacyAuditDataAccess with auditDetails", async () => {
+      const request = httpMocks.createRequest({
+        method: "GET",
+        headers: {
+          authorization: "Bearer token"
+        },
+        fflip: mockFflipObject({
+          newAuditFeature: false
+        }),
+        nickname: "nickname"
+      });
 
-    await getWorkingCases(request, response, next);
+      const response = httpMocks.createResponse();
+      const next = jest.fn();
 
-    expect(legacyAuditDataAccess).toHaveBeenCalledWith(
-      "nickname",
-      undefined,
-      AUDIT_SUBJECT.ALL_WORKING_CASES,
-      expect.anything(),
-      AUDIT_ACTION.DATA_ACCESSED,
-      GET_CASES_AUDIT_DETAILS
-    );
+      await getWorkingCases(request, response, next);
+
+      expect(legacyAuditDataAccess).toHaveBeenCalledWith(
+        "nickname",
+        undefined,
+        AUDIT_SUBJECT.ALL_WORKING_CASES,
+        expect.anything(),
+        AUDIT_ACTION.DATA_ACCESSED,
+        GET_CASES_AUDIT_DETAILS
+      );
+    });
   });
 });
