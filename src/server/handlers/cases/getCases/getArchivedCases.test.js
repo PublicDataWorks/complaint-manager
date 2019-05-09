cd91503e0fe7cc5e214e55c993ff719e613c852a
Susie Riley <sriley@thoughtworks.com>
2019-05-09 16:11:36 -0500

    Susie/Tyler [#165801763] Implement new auditing for getArchivedCases handler

--- src/server/handlers/cases/getCases/getArchivedCases.test.js
+++ src/server/handlers/cases/getCases/getArchivedCases.test.js
@@ -7,15 +7,12 @@ import {
   AUDIT_SUBJECT
 } from "../../../../sharedUtilities/constants";
 import getCases, { CASES_TYPE, GET_CASES_AUDIT_DETAILS } from "./getCases";
+import mockFflipObject from "../../../testHelpers/mockFflipObject";
+import auditDataAccess from "../../auditDataAccess";
 
 jest.mock("../../legacyAuditDataAccess");
+jest.mock("../../auditDataAccess");
 jest.mock("./getCases");
-jest.mock(
-  "../../../checkFeatureToggleEnabled",
-  () => (request, featureName) => {
-    return featureName === "caseDashboardPaginationFeature";
-  }
-);
 
 getCases.mockImplementation((caseType, sortBy, sortDirection, transaction) => {
   return "MOCK_GET_CASES";
@@ -24,12 +21,19 @@ getCases.mockImplementation((caseType, sortBy, sortDirection, transaction) => {
 const httpMocks = require("node-mocks-http");
 
 describe("getArchivedCases", () => {
+  afterEach(async () => {
+    await cleanupDatabase();
+  });
+
   test("should call getCases with sortBy and sortDirection params", async () => {
     const request = httpMocks.createRequest({
       method: "GET",
       headers: {
         authorization: "Bearer token"
       },
+      fflip: mockFflipObject({
+        caseDashboardPaginationFeature: true
+      }),
       query: {
         page: 2,
         sortBy: "by",
@@ -51,9 +55,10 @@ describe("getArchivedCases", () => {
       2
     );
   });
-  describe("test audits", () => {
+  describe("test audits with newAuditFeature off", () => {
     const auditUser = "testUser";
     let existingArchivedCase, request, response, next;
+
     beforeEach(async () => {
       existingArchivedCase = await createTestCaseWithCivilian();
       await existingArchivedCase.destroy({ auditUser: auditUser });
@@ -63,15 +68,14 @@ describe("getArchivedCases", () => {
         headers: {
           authorization: "Bearer token"
         },
+        fflip: mockFflipObject({
+          newAuditFeature: false
+        }),
         nickname: auditUser
       });
 
       response = httpMocks.createResponse();
       next = jest.fn();
-    });
-
-    afterEach(async () => {
-      await cleanupDatabase();
     });
 
     test("should audit data access", async () => {
@@ -84,6 +88,40 @@ describe("getArchivedCases", () => {
         expect.anything(),
         AUDIT_ACTION.DATA_ACCESSED,
         GET_CASES_AUDIT_DETAILS
+      );
+    });
+  });
+  describe("test audits with newAuditFeature on", () => {
+    const auditUser = "testUser";
+    let existingArchivedCase, request, response, next;
+    beforeEach(async () => {
+      existingArchivedCase = await createTestCaseWithCivilian();
+      await existingArchivedCase.destroy({ auditUser: auditUser });
+
+      request = httpMocks.createRequest({
+        method: "GET",
+        headers: {
+          authorization: "Bearer token"
+        },
+        fflip: mockFflipObject({
+          newAuditFeature: true
+        }),
+        nickname: auditUser
+      });
+
+      response = httpMocks.createResponse();
+      next = jest.fn();
+    });
+
+    test("should audit data access", async () => {
+      await getArchivedCases(request, response, next);
+
+      expect(auditDataAccess).toHaveBeenCalledWith(
+        auditUser,
+        null,
+        AUDIT_SUBJECT.ALL_ARCHIVED_CASES,
+        GET_CASES_AUDIT_DETAILS,
+        expect.anything()
       );
     });
   });
