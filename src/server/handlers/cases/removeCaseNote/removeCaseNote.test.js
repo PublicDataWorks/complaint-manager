09e6ef5ddc315fa710c09ec294a8b2287f4849f1
Susie Riley <sriley@thoughtworks.com>
2019-05-16 14:07:27 -0500

    Susie/Karan [#165801763] Add model to auditDetails in mocked getQueryAuditAccessDetails functions

--- src/server/handlers/cases/removeCaseNote/removeCaseNote.test.js
+++ src/server/handlers/cases/removeCaseNote/removeCaseNote.test.js
@@ -16,13 +16,15 @@ jest.mock("../../getQueryAuditAccessDetails", () => ({
   generateAndAddAuditDetailsFromQuery: jest.fn(
     (existingDetails, queryOptions, topLevelModelName) => {
       existingDetails[mockLodash.camelCase(topLevelModelName)] = {
-        attributes: ["mockDetails"]
+        attributes: ["mockDetails"],
+        model: "mockModelName"
       };
     }
   ),
   addToExistingAuditDetails: jest.fn((existingDetails, detailsToAdd) => {
     existingDetails["mockAttribute"] = {
-      attributes: ["mockDetails"]
+      attributes: ["mockDetails"],
+      model: "mockModelName"
     };
   }),
   removeFromExistingAuditDetails: jest.fn()
