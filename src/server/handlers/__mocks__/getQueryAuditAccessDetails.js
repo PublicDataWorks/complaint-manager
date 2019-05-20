fe11040e01c8604a04f4035a17f68063d8ef9c31
Susie Riley <sriley@thoughtworks.com>
2019-05-20 15:12:22 -0500

    Susie/Karan [#165801763] Implement new auditing for createCase handler

--- src/server/handlers/__mocks__/getQueryAuditAccessDetails.js
+++ src/server/handlers/__mocks__/getQueryAuditAccessDetails.js
@@ -1,24 +1,23 @@
 import _ from "lodash";
 
-export const removeFromExistingAuditDetails = (
-  existingDetails,
-  detailsToRemove
-) => {};
+export const removeFromExistingAuditDetails = jest.fn(
+  (existingDetails, detailsToRemove) => {}
+);
 
-export const addToExistingAuditDetails = (existingDetails, detailsToAdd) => {
-  existingDetails["mockAssociation"] = {
-    attributes: ["mockDetails"],
-    model: "mockModelName"
-  };
-};
+export const addToExistingAuditDetails = jest.fn(
+  (existingDetails, detailsToAdd) => {
+    existingDetails["mockAssociation"] = {
+      attributes: ["mockDetails"],
+      model: "mockModelName"
+    };
+  }
+);
 
-export const generateAndAddAuditDetailsFromQuery = (
-  existingDetails,
-  queryOptions,
-  topLevelModelName
-) => {
-  existingDetails[_.camelCase(topLevelModelName)] = {
-    attributes: ["mockDetails"],
-    model: topLevelModelName
-  };
-};
+export const generateAndAddAuditDetailsFromQuery = jest.fn(
+  (existingDetails, queryOptions, topLevelModelName) => {
+    existingDetails[_.camelCase(topLevelModelName)] = {
+      attributes: ["mockDetails"],
+      model: topLevelModelName
+    };
+  }
+);
