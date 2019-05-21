f5ccf082f2939c8274155eb41a31ed13e4fa332b
Susie Riley <sriley@thoughtworks.com>
2019-05-21 16:33:20 -0500

    Susie/Jacob [#165801763] Implement new auditing for createCaseNote

--- src/server/handlers/cases/createCaseNote.js
+++ src/server/handlers/cases/createCaseNote.js
@@ -1,12 +1,20 @@
 import { AUDIT_ACTION } from "../../../sharedUtilities/constants";
 import legacyAuditDataAccess from "../legacyAuditDataAccess";
 import { getCaseWithAllAssociations } from "../getCaseHelpers";
+import checkFeatureToggleEnabled from "../../checkFeatureToggleEnabled";
+import auditDataAccess from "../auditDataAccess";
+import { generateAndAddAuditDetailsFromQuery } from "../getQueryAuditAccessDetails";
 
 const { AUDIT_SUBJECT } = require("../../../sharedUtilities/constants");
 const asyncMiddleware = require("../asyncMiddleware");
 const models = require("../../models");
 
 const createCaseNote = asyncMiddleware(async (request, response) => {
+  const newAuditFeatureToggle = checkFeatureToggleEnabled(
+    request,
+    "newAuditFeature"
+  );
+
   const currentCase = await models.sequelize.transaction(async transaction => {
     await models.case_note.create(
       {
@@ -20,6 +28,14 @@ const createCaseNote = asyncMiddleware(async (request, response) => {
       }
     );
 
+    let caseNoteAuditDetails = {};
+
+    const caseNotes = await getCaseNotes(
+      request.params.caseId,
+      transaction,
+      caseNoteAuditDetails
+    );
+
     let caseAuditDetails = {};
     const caseDetails = await getCaseWithAllAssociations(
       request.params.caseId,
@@ -27,34 +43,62 @@ const createCaseNote = asyncMiddleware(async (request, response) => {
       caseAuditDetails
     );
 
-    await legacyAuditDataAccess(
-      request.nickname,
-      request.params.caseId,
-      AUDIT_SUBJECT.CASE_NOTES,
-      transaction
-    );
+    if (newAuditFeatureToggle) {
+      await auditDataAccess(
+        request.nickname,
+        request.params.caseId,
+        AUDIT_SUBJECT.CASE_NOTES,
+        caseNoteAuditDetails,
+        transaction
+      );
+      await auditDataAccess(
+        request.nickname,
+        request.params.caseId,
+        AUDIT_SUBJECT.CASE_DETAILS,
+        caseAuditDetails,
+        transaction
+      );
+    } else {
+      await legacyAuditDataAccess(
+        request.nickname,
+        request.params.caseId,
+        AUDIT_SUBJECT.CASE_NOTES,
+        transaction
+      );
 
-    await legacyAuditDataAccess(
-      request.nickname,
-      request.params.caseId,
-      AUDIT_SUBJECT.CASE_DETAILS,
-      transaction,
-      AUDIT_ACTION.DATA_ACCESSED,
-      caseAuditDetails
-    );
-
-    const caseNotes = await models.case_note.findAll({
-      where: {
-        caseId: request.params.caseId
-      },
-      include: [{ model: models.case_note_action, as: "caseNoteAction" }],
-      transaction
-    });
+      await legacyAuditDataAccess(
+        request.nickname,
+        request.params.caseId,
+        AUDIT_SUBJECT.CASE_DETAILS,
+        transaction,
+        AUDIT_ACTION.DATA_ACCESSED,
+        caseAuditDetails
+      );
+    }
 
     return { caseNotes, caseDetails };
   });
 
   response.status(201).send(currentCase);
 });
+
+async function getCaseNotes(caseId, transaction, caseNoteAuditDetails) {
+  const caseNotesQueryOptions = {
+    where: {
+      caseId: caseId
+    },
+    include: [{ model: models.case_note_action, as: "caseNoteAction" }],
+    transaction
+  };
+
+  const caseNotes = await models.case_note.findAll(caseNotesQueryOptions);
+
+  generateAndAddAuditDetailsFromQuery(
+    caseNoteAuditDetails,
+    caseNotesQueryOptions,
+    models.case_note.name
+  );
+  return caseNotes;
+}
 
 module.exports = createCaseNote;
