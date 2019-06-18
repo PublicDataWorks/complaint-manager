2d19b2a0c9814853a6877e772499a8ae66244b7c
Jacob Gacek <jacob.gacek@thoughtworks.com>
2019-06-18 16:32:19 -0500

    Jacob/Karan [#165801718] Create master audit migration file for running all helper migrations

---
+++ src/server/migrationJobs/auditMigrationHelpers/auditMasterMigration.js
@@ -0,0 +1,44 @@
+import {
+  transformNewAuthenticationAuditsToOld,
+  transformOldAuthenticationAuditsToNew
+} from "./transformAuthenticationAudits";
+import {
+  transformNewDataAccessAuditsToOld,
+  transformOldDataAccessAuditsToNew
+} from "./transformDataAccessAudits";
+import {
+  transformNewExportAuditsToOld,
+  transformOldExportAuditsToNew
+} from "./transformExportAudits";
+import {
+  copyAttachmentDataChangeAuditsToActionAudits,
+  deleteUploadAttachmentAudits
+} from "./copyAttachmentDataChangeAuditsToActionAudits";
+import {
+  transformNewFileAuditsToOldUploadDownloadActionAudits,
+  transformOldUploadDownloadAccessAuditsToNewFileAudits
+} from "./transformUploadDownloadActionAudits";
+
+export const runAllAuditMigrationHelpers = async transaction => {
+  await transformOldAuthenticationAuditsToNew(transaction);
+
+  await transformOldDataAccessAuditsToNew(transaction);
+
+  await transformOldExportAuditsToNew(transaction);
+
+  // Transform old upload download access audits is dependent on copy attachment audits running first
+  await copyAttachmentDataChangeAuditsToActionAudits(transaction);
+  await transformOldUploadDownloadAccessAuditsToNewFileAudits(transaction);
+};
+
+export const undoAllAuditMigrationHelpers = async transaction => {
+  // Delete upload attachment is dependent on transform old to new being run before it
+  await transformNewFileAuditsToOldUploadDownloadActionAudits(transaction);
+  await deleteUploadAttachmentAudits(transaction);
+
+  await transformNewExportAuditsToOld(transaction);
+
+  await transformNewDataAccessAuditsToOld(transaction);
+
+  await transformNewAuthenticationAuditsToOld(transaction);
+};
