c231a3313e204ef5c648fdb11cd5a86d1aee2208
Tyler Pearson <tyler.pearson@thoughtworks.com>
2019-06-28 12:16:04 -0500

    Karan/Tyler [#165432832] Fix fullName and age to work as a virtual field on officer

--- src/server/models/officer.js
+++ src/server/models/officer.js
@@ -4,119 +4,122 @@ import { getOfficerFullName } from "./modelUtilities/getFullName";
 const moment = require("moment/moment");
 
 module.exports = (sequelize, DataTypes) => {
-  var Officer = sequelize.define(
-    "officer",
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      officerNumber: {
-        type: DataTypes.INTEGER,
-        field: "officer_number"
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        field: "first_name"
-      },
-      middleName: {
-        type: DataTypes.STRING,
-        field: "middle_name"
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        field: "last_name"
-      },
-      rank: {
-        type: DataTypes.STRING
-      },
-      race: {
-        type: DataTypes.STRING
-      },
-      sex: {
-        type: DataTypes.STRING
-      },
-      dob: {
-        type: DataTypes.DATEONLY
-      },
-      bureau: {
-        type: DataTypes.STRING
-      },
-      district: {
-        type: DataTypes.STRING,
-        get() {
-          switch (this.getDataValue("district")) {
-            case "First District":
-              return "1st District";
-            case "Second District":
-              return "2nd District";
-            case "Third District":
-              return "3rd District";
-            case "Fourth District":
-              return "4th District";
-            case "Fifth District":
-              return "5th District";
-            case "Sixth District":
-              return "6th District";
-            case "Seventh District":
-              return "7th District";
-            case "Eighth District":
-              return "8th District";
-            default:
-              return this.getDataValue("district");
-          }
-        }
-      },
-      workStatus: {
-        type: DataTypes.STRING,
-        field: "work_status"
-      },
-      supervisorOfficerNumber: {
-        type: DataTypes.INTEGER,
-        field: "supervisor_officer_number"
-      },
-      hireDate: {
-        type: DataTypes.DATEONLY,
-        field: "hire_date"
-      },
-      endDate: {
-        type: DataTypes.DATEONLY,
-        field: "end_date"
-      },
-      employeeType: {
-        type: DataTypes.ENUM(["Commissioned", "Non-Commissioned", "Recruit"]),
-        field: "employee_type"
-      },
-      windowsUsername: {
-        type: DataTypes.INTEGER,
-        field: "windows_username"
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        field: "created_at"
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        field: "updated_at"
+  var Officer = sequelize.define("officer", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    officerNumber: {
+      type: DataTypes.INTEGER,
+      field: "officer_number"
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      field: "first_name"
+    },
+    middleName: {
+      type: DataTypes.STRING,
+      field: "middle_name"
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      field: "last_name"
+    },
+    fullName: {
+      type: new DataTypes.VIRTUAL(DataTypes.STRING, [
+        "firstName",
+        "middleName",
+        "lastName"
+      ]),
+      get: function() {
+        return getOfficerFullName(
+          this.get("firstName"),
+          this.get("middleName"),
+          this.get("lastName")
+        );
       }
     },
-    {
-      getterMethods: {
-        fullName() {
-          return getOfficerFullName(
-            this.firstName,
-            this.middleName,
-            this.lastName
-          );
-        },
-        age() {
-          return moment().diff(this.dob, "years", false);
+    rank: {
+      type: DataTypes.STRING
+    },
+    race: {
+      type: DataTypes.STRING
+    },
+    sex: {
+      type: DataTypes.STRING
+    },
+    dob: {
+      type: DataTypes.DATEONLY
+    },
+    age: {
+      type: new DataTypes.VIRTUAL(DataTypes.INTEGER, ["dob"]),
+      get: function() {
+        return moment().diff(this.get("dob"), "years", false);
+      }
+    },
+    bureau: {
+      type: DataTypes.STRING
+    },
+    district: {
+      type: DataTypes.STRING,
+      get() {
+        switch (this.getDataValue("district")) {
+          case "First District":
+            return "1st District";
+          case "Second District":
+            return "2nd District";
+          case "Third District":
+            return "3rd District";
+          case "Fourth District":
+            return "4th District";
+          case "Fifth District":
+            return "5th District";
+          case "Sixth District":
+            return "6th District";
+          case "Seventh District":
+            return "7th District";
+          case "Eighth District":
+            return "8th District";
+          default:
+            return this.getDataValue("district");
         }
       }
+    },
+    workStatus: {
+      type: DataTypes.STRING,
+      field: "work_status"
+    },
+    supervisorOfficerNumber: {
+      type: DataTypes.INTEGER,
+      field: "supervisor_officer_number"
+    },
+    hireDate: {
+      type: DataTypes.DATEONLY,
+      field: "hire_date"
+    },
+    endDate: {
+      type: DataTypes.DATEONLY,
+      field: "end_date"
+    },
+    employeeType: {
+      type: DataTypes.ENUM(["Commissioned", "Non-Commissioned", "Recruit"]),
+      field: "employee_type"
+    },
+    windowsUsername: {
+      type: DataTypes.INTEGER,
+      field: "windows_username"
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: "created_at"
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: "updated_at"
     }
-  );
+  });
 
   Officer.associate = models => {
     Officer.belongsToMany(models.cases, {
