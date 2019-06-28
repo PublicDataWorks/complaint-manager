dd11e43b9c66fb8ed13869dbc74af6023f112393
Jacob Gacek <jacob.gacek@thoughtworks.com>
2019-06-28 16:20:23 -0500

    Susie/Jacob [#166126984] extend case history to properly handle the structure of the new audits

--- src/server/handlers/cases/getCaseHistory/getCaseHistory.js
+++ src/server/handlers/cases/getCaseHistory/getCaseHistory.js
@@ -2,11 +2,13 @@ import {
   AUDIT_ACTION,
   AUDIT_TYPE
 } from "../../../../sharedUtilities/constants";
-import transformAuditToCaseHistory from "./transformAuditToCaseHistory";
+import transformAuditsToCaseHistory from "./transformAuditsToCaseHistory";
 import getQueryAuditAccessDetails, {
   combineAuditDetails
 } from "../../audits/getQueryAuditAccessDetails";
 import legacyAuditDataAccess from "../../audits/legacyAuditDataAccess";
+import checkFeatureToggleEnabled from "../../../checkFeatureToggleEnabled";
+import auditDataAccess from "../../audits/auditDataAccess";
 
 const { AUDIT_SUBJECT } = require("../../../../sharedUtilities/constants");
 const asyncMiddleware = require("../../asyncMiddleware");
@@ -14,36 +16,47 @@ const models = require("../../../models");
 
 const getCaseHistory = asyncMiddleware(async (request, response) => {
   const caseId = request.params.caseId;
+  const newAuditFeatureToggle = checkFeatureToggleEnabled(
+    request,
+    "newAuditFeature"
+  );
+
   const caseHistoryAudits = await models.sequelize.transaction(
     async transaction => {
-      const dataChangeAuditsAndAuditDetails = await getDataChangeAuditsAndAuditDetails(
+      let {
+        dataChangeAudits,
+        dataChangeAuditDetails,
+        uploadAudits,
+        uploadAuditDetails
+      } = await getDataChangeAndUploadAuditsAndAuditDetails(
         caseId,
-        transaction
+        transaction,
+        newAuditFeatureToggle
       );
-      const dataChangeAudits = dataChangeAuditsAndAuditDetails.dataChangeAudits;
-      const dataChangeAuditDetails =
-        dataChangeAuditsAndAuditDetails.auditDetails;
-
-      const uploadAuditsAndAuditDetails = await getUploadAuditsAndAuditDetails(
-        caseId,
-        transaction
-      );
-      const uploadAudits = uploadAuditsAndAuditDetails.uploadAudits;
-      const uploadAuditDetails = uploadAuditsAndAuditDetails.auditDetails;
 
       const auditDetails = combineAuditDetails(
         dataChangeAuditDetails,
         uploadAuditDetails
       );
 
-      await legacyAuditDataAccess(
-        request.nickname,
-        caseId,
-        AUDIT_SUBJECT.CASE_HISTORY,
-        transaction,
-        AUDIT_ACTION.DATA_ACCESSED,
-        auditDetails
-      );
+      if (newAuditFeatureToggle) {
+        await auditDataAccess(
+          request.nickname,
+          request.params.caseId,
+          AUDIT_SUBJECT.CASE_HISTORY,
+          auditDetails,
+          transaction
+        );
+      } else {
+        await legacyAuditDataAccess(
+          request.nickname,
+          caseId,
+          AUDIT_SUBJECT.CASE_HISTORY,
+          transaction,
+          AUDIT_ACTION.DATA_ACCESSED,
+          auditDetails
+        );
+      }
 
       return {
         dataChangeAudits: dataChangeAudits,
@@ -51,11 +64,123 @@ const getCaseHistory = asyncMiddleware(async (request, response) => {
       };
     }
   );
-  const caseHistory = transformAuditToCaseHistory(caseHistoryAudits);
+  const caseHistory = transformAuditsToCaseHistory(
+    caseHistoryAudits,
+    newAuditFeatureToggle
+  );
   response.status(200).send(caseHistory);
 });
 
+const getDataChangeAndUploadAuditsAndAuditDetails = async (
+  caseId,
+  transaction,
+  newAuditFeatureToggle
+) => {
+  let dataChangeAudits,
+    uploadAudits,
+    dataChangeAuditDetails,
+    uploadAuditDetails,
+    dataChangeAuditsAndAuditDetails,
+    uploadAuditsAndAuditDetails;
+
+  // TODO remove following else statement when removing newAuditFeatureToggle flag
+  if (newAuditFeatureToggle) {
+    dataChangeAuditsAndAuditDetails = await getDataChangeAuditsAndAuditDetails(
+      caseId,
+      transaction
+    );
+    uploadAuditsAndAuditDetails = await getUploadAuditsAndAuditDetails(
+      caseId,
+      transaction
+    );
+  } else {
+    dataChangeAuditsAndAuditDetails = await getLegacyDataChangeAuditsAndAuditDetails(
+      caseId,
+      transaction
+    );
+    uploadAuditsAndAuditDetails = await getLegacyUploadAuditsAndAuditDetails(
+      caseId,
+      transaction
+    );
+  }
+
+  dataChangeAudits = dataChangeAuditsAndAuditDetails.dataChangeAudits;
+  dataChangeAuditDetails = dataChangeAuditsAndAuditDetails.auditDetails;
+
+  uploadAudits = uploadAuditsAndAuditDetails.uploadAudits;
+  uploadAuditDetails = uploadAuditsAndAuditDetails.auditDetails;
+
+  return {
+    dataChangeAudits: dataChangeAudits,
+    dataChangeAuditDetails: dataChangeAuditDetails,
+    uploadAudits: uploadAudits,
+    uploadAuditDetails: uploadAuditDetails
+  };
+};
+
+const getDataChangeAuditsAndAuditDetails = async (caseId, transaction) => {
+  const queryOptions = {
+    where: {
+      auditAction: {
+        [models.sequelize.Op.or]: [
+          AUDIT_ACTION.DATA_UPDATED,
+          AUDIT_ACTION.DATA_RESTORED,
+          AUDIT_ACTION.DATA_ARCHIVED,
+          AUDIT_ACTION.DATA_CREATED,
+          AUDIT_ACTION.DATA_DELETED
+        ]
+      },
+      caseId: caseId
+    },
+    attributes: ["auditAction", "user", "createdAt"],
+    include: [
+      {
+        model: models.data_change_audit,
+        as: "dataChangeAudit",
+        attributes: ["modelName", "modelDescription", "changes"]
+      }
+    ],
+    transaction
+  };
+
+  const dataChangeAudits = await models.audit.findAll(queryOptions);
+
+  const dataChangeAuditDetails = getQueryAuditAccessDetails(
+    queryOptions,
+    models.audit.name
+  );
+
+  return {
+    dataChangeAudits: dataChangeAudits,
+    auditDetails: dataChangeAuditDetails
+  };
+};
+
 const getUploadAuditsAndAuditDetails = async (caseId, transaction) => {
+  const queryOptions = {
+    where: { auditAction: AUDIT_ACTION.UPLOADED, caseId: caseId },
+    attributes: ["auditAction", "user", "createdAt"],
+    include: [
+      {
+        model: models.file_audit,
+        as: "fileAudit",
+        attributes: ["fileType", "fileName"]
+      }
+    ],
+    transaction
+  };
+
+  const uploadAudits = await models.audit.findAll(queryOptions);
+
+  const uploadAuditDetails = getQueryAuditAccessDetails(
+    queryOptions,
+    models.audit.name
+  );
+
+  return { uploadAudits: uploadAudits, auditDetails: uploadAuditDetails };
+};
+
+const getLegacyUploadAuditsAndAuditDetails = async (caseId, transaction) => {
   const queryOptions = {
     where: { auditType: AUDIT_TYPE.UPLOAD, caseId: caseId },
     attributes: ["action", "user", "createdAt", "subject"],
@@ -64,7 +189,6 @@ const getUploadAuditsAndAuditDetails = async (caseId, transaction) => {
   };
 
   const uploadAudits = await models.action_audit.findAll(queryOptions);
-
   const uploadAuditDetails = getQueryAuditAccessDetails(
     queryOptions,
     models.action_audit.name
@@ -73,7 +197,10 @@ const getUploadAuditsAndAuditDetails = async (caseId, transaction) => {
   return { uploadAudits: uploadAudits, auditDetails: uploadAuditDetails };
 };
 
-const getDataChangeAuditsAndAuditDetails = async (caseId, transaction) => {
+const getLegacyDataChangeAuditsAndAuditDetails = async (
+  caseId,
+  transaction
+) => {
   const queryOptions = {
     where: { caseId: caseId },
     attributes: [
