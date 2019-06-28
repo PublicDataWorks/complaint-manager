7a950f09ef0db24736a28d8f375546e15549f4bc
Tyler Pearson <tyler.pearson@thoughtworks.com>
2019-06-28 16:15:07 -0500

    Karan/Tyler [#165432832] Remove console.logs.

--- src/client/cases/CaseDetails/StatusButton/StatusButton.js
+++ src/client/cases/CaseDetails/StatusButton/StatusButton.js
@@ -44,7 +44,6 @@ class StatusButton extends Component {
   openUpdateCaseStatusDialog = () => {
     const { nextStatus, caseId } = this.props;
     let redirectUrl;
-    console.log("Here's the next status: ", nextStatus);
     if (nextStatus === CASE_STATUS.LETTER_IN_PROGRESS) {
       redirectUrl = `/cases/${caseId}/letter/review`;
     }
