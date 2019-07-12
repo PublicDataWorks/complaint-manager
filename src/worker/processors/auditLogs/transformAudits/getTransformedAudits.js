69e5a18726dbbce90f21738991d66ede47aeb37d
KatieMLarson <katie.larson@thoughtworks.com>
2019-07-12 13:36:35 -0500

    Katie/Karan/Tyler batch processed sql query for export audits

--- src/worker/processors/auditLogs/transformAudits/getTransformedAudits.js
+++ src/worker/processors/auditLogs/transformAudits/getTransformedAudits.js
@@ -1,45 +1,68 @@
 import transformAuditsForExport from "./transformAuditsForExport";
 import models from "../../../../server/models";
+import _ from "lodash";
 
 const winston = require("winston");
 
-const getTransformedAudits = async dateRangeCondition => {
-  const audits = await models.audit.findAll({
-    where: dateRangeCondition,
-    include: [
-      {
-        model: models.export_audit,
-        as: "exportAudit"
-      },
-      {
-        model: models.data_access_audit,
-        as: "dataAccessAudit",
-        include: [
-          {
-            model: models.data_access_value,
-            as: "dataAccessValues"
-          }
-        ]
-      },
-      {
-        model: models.file_audit,
-        as: "fileAudit"
-      },
-      {
-        model: models.data_change_audit,
-        as: "dataChangeAudit"
-      },
-      {
-        model: models.legacy_data_access_audit,
-        as: "legacyDataAccessAudit"
-      }
-    ]
+const getTransformedAudits = async (
+  dateRangeCondition = {},
+  queryLimit = 1000
+) => {
+  let transformedAuditsForExport = [];
+  let previouslyTransformedAudits = 0;
+  const totalAudits = await models.audit.count({
+    where: dateRangeCondition
   });
   winston.info(
-    `Database returned ${audits.length} audits that need to be transformed.`
+    `Database returned ${
+      totalAudits.length
+    } audits that need to be transformed.`
   );
 
-  return transformAuditsForExport(audits);
+  while (
+    previouslyTransformedAudits != totalAudits &&
+    previouslyTransformedAudits < totalAudits
+  ) {
+    let limitedResultSet = await models.audit.findAll({
+      where: dateRangeCondition,
+      include: [
+        {
+          model: models.export_audit,
+          as: "exportAudit"
+        },
+        {
+          model: models.data_access_audit,
+          as: "dataAccessAudit",
+          include: [
+            {
+              model: models.data_access_value,
+              as: "dataAccessValues"
+            }
+          ]
+        },
+        {
+          model: models.file_audit,
+          as: "fileAudit"
+        },
+        {
+          model: models.data_change_audit,
+          as: "dataChangeAudit"
+        },
+        {
+          model: models.legacy_data_access_audit,
+          as: "legacyDataAccessAudit"
+        }
+      ],
+      offset: previouslyTransformedAudits,
+      limit: queryLimit
+    });
+
+    previouslyTransformedAudits += limitedResultSet.length;
+
+    transformedAuditsForExport.push(transformAuditsForExport(limitedResultSet));
+  }
+
+  return _.flatten(transformedAuditsForExport);
 };
 
 export default getTransformedAudits;
