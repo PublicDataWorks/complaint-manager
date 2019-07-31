3d8c5e53055ae756f98387f8c6e85b5897efc7fa
Tyler Pearson <tyler.pearson@thoughtworks.com>
2019-07-31 11:33:16 -0500

    Wanchen/Katie/Tyler [#165432827] Create migrations for civlian title to civilian_title)id

---
+++ src/server/tasks/taskMigrationJobs/civilianTitleTransformationJobs/transformCivilianTitleToId.js
@@ -0,0 +1,74 @@
+import models from "../../../models";
+import _ from "lodash";
+
+export const transformCivilianTitleToId = async (
+  civilianTitles,
+  civilians,
+  transaction
+) => {
+  const civilianTitleDictionary = {};
+  civilianTitles.forEach(civilianTitle => {
+    civilianTitleDictionary[civilianTitle.name] = civilianTitle.id;
+  });
+
+  for (let i = 0; i < civilians.length; i++) {
+    if (!civilians[i].civilian_title_id && !_.isEmpty(civilians[i].title)) {
+      await updateDatabaseWithCorrectCivilianTitleId(
+        civilians[i],
+        civilianTitleDictionary,
+        transaction
+      );
+    }
+  }
+};
+
+const updateDatabaseWithCorrectCivilianTitleId = async (
+  civilian,
+  civilianTitleDictionary,
+  transaction
+) => {
+  const updateCivilianQuery = `UPDATE civilians SET civilian_title_id = ${
+    civilianTitleDictionary[civilian.title]
+  }, title = null WHERE id = ${civilian.id}`;
+
+  await models.sequelize.query(updateCivilianQuery, {
+    type: models.sequelize.QueryTypes.UPDATE,
+    transaction
+  });
+};
+
+export const revertTransformCivilianTitleToId = async (
+  civilianTitles,
+  civilians,
+  transaction
+) => {
+  const civilianTitleDictionary = {};
+  civilianTitles.forEach(civilianTitle => {
+    civilianTitleDictionary[civilianTitle.id] = civilianTitle.name;
+  });
+
+  for (let i = 0; i < civilians.length; i++) {
+    if (civilians[i].civilian_title_id) {
+      await updatedDatabaseWithCorrectCivilianTitleString(
+        civilians[i],
+        civilianTitleDictionary,
+        transaction
+      );
+    }
+  }
+};
+
+const updatedDatabaseWithCorrectCivilianTitleString = async (
+  civilian,
+  civilianTitleDictionary,
+  transaction
+) => {
+  const updateCivilianQuery = `UPDATE civilians SET title = '${
+    civilianTitleDictionary[civilian.civilian_title_id]
+  }', civilian_title_id = null WHERE id =${civilian.id}`;
+
+  await models.sequelize.query(updateCivilianQuery, {
+    type: models.sequelize.QueryTypes.UPDATE,
+    transaction
+  });
+};
