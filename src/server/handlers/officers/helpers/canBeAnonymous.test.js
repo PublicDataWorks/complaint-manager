c9984917510d78da11d09658399b9c88cc79b45e
jeffreymartin <jeffrey.martin@thoughtworks.com>
2019-08-20 12:03:57 -0500

    [Jeff/Brian] [#167900280] Extracted can be anonymous logic into a validator

---
+++ src/server/handlers/officers/helpers/canBeAnonymous.test.js
@@ -0,0 +1,24 @@
+import canBeAnonymous from "../helpers/canBeAnonymous";
+import { ACCUSED, WITNESS } from "../../../../sharedUtilities/constants";
+
+describe("canBeAnonymous", () => {
+  test("should return isAnonymous when supplied", () => {
+    expect(canBeAnonymous(true)).toBeTrue();
+  });
+
+  test("should return false when passed false", () => {
+    expect(canBeAnonymous(false)).toBeFalse();
+  });
+
+  test("should return false when there is no argument supplied", () => {
+    expect(canBeAnonymous(undefined)).toBeFalse();
+  });
+
+  test("should return false when officer is accused", () => {
+    expect(canBeAnonymous(true, ACCUSED)).toBeFalse();
+  });
+
+  test("should return true when officer is not accused", () => {
+      expect(canBeAnonymous(true, WITNESS)).toBeTrue();
+  })
+});
