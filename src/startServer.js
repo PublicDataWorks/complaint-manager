d8badb751121937a14dd3ad3c1810b4d4708d529
KatieMLarson <katie.larson@thoughtworks.com>
2019-08-28 15:04:40 -0500

    Revert "[Karan/Katie][#167228516] Upgrade react-scripts to 3.1.0"
    This reverts commit 2af4966fb9d0ff74251776798967e77fd5d1ba89.

--- src/startServer.js
+++ src/startServer.js
@@ -2,5 +2,5 @@ import { server } from "./server/server";
 
 server.listen(process.env.PORT || 1234, () => {
   console.info("Application is listening on port 1234");
-  console.info("Please visit http://localhost: 1234");
+  console.info("Please visit http://localhost:1234");
 });
