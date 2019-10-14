3c8f0c63146f0cc0f65f33f0b343a5cafff36a6a
Karan Krishnani <krkrishn@thoughtworks.com>
2019-10-14 11:56:53 -0400

    Katie/Karan [#168873060] Squashing the migrations into a schema file

---
+++ scripts/loadSchema.js
@@ -0,0 +1,32 @@
+const { exec } = require("child-process-promise");
+
+(async function() {
+  const env = process.env.NODE_ENV || "development";
+  console.log("Loading Schema in DB Env:", env);
+
+  const config = require(__dirname +
+    "/../src/server/config/sequelize_config.js")[env];
+  console.log(config);
+
+  const schemaFile = `20191004_schema.sql`;
+
+  const databaseName = config.database;
+  const databaseHost = config.host;
+  const databaseUser = config.username;
+  const databasePassword = config.password;
+
+  console.log("Schema import running...");
+  const loadSchemaCommand = `PGPASSWORD=${databasePassword} psql -a -U ${databaseUser} -h ${databaseHost} -d ${databaseName} < ./src/server/migrations/schema/${schemaFile}`;
+  console.log("Running the command: ", loadSchemaCommand);
+  exec(loadSchemaCommand)
+    .then(() => {
+      console.log("*************************");
+      console.log("Schema import successful");
+      process.exit(0);
+    })
+    .catch(err => {
+      console.log("*************************");
+      console.log("Schema import failed. Error:", err.message);
+      process.exit(1);
+    });
+})();
--- scripts/setup-test-db.sh
+++ scripts/setup-test-db.sh
@@ -5,6 +5,11 @@ echo Running against $DB_ENV database...
 
 yarn run sequelize db:create --env=$DB_ENV
 
+DB_CREATED=$?
+if [ $DB_CREATED -eq 0 ]; then
+  NODE_ENV=$DB_ENV node scripts/loadSchema.js
+fi
+
 NODE_ENV=$DB_ENV yarn run sequelize db:migrate --env=$DB_ENV
 
 exit $?
