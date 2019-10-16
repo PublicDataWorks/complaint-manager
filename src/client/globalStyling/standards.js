a7c9c7400943073660d8885322f27ba91ab44754
Jacob Gacek <jacob.gacek@thoughtworks.com>
2019-10-16 15:43:24 -0500

    [Katie/Wanchen/Jacob] [#168765957] Split some of styles into new standards

---
+++ src/client/globalStyling/standards.js
@@ -0,0 +1,14 @@
+const standards = {
+  //Spacing Standards
+  small: "16px",
+  medium: "24px",
+  large: "40px",
+
+  //Font Size Standards
+  fontTiny: "0.75rem",
+  fontSmall: "0.875rem",
+  fontMedium: "1rem",
+  fontLarge: "1.3rem"
+};
+
+export default standards;
