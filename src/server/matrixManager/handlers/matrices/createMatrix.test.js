215481db63de3210c5d54f9721f8587713566453
Jacob Gacek <jacob.gacek@thoughtworks.com>
2019-11-06 12:05:15 -0600

    Jacob/Wanchen [#167135360] create model and migration for matrices, create handler to create new matrix. Tests

--- src/server/matrixManager/handlers/matrices/createMatrix.test.js
+++ src/server/matrixManager/handlers/matrices/createMatrix.test.js
@@ -1,5 +1,66 @@
 import createMatrix from "./createMatrix";
+import { cleanupDatabase } from "../../../testHelpers/requestTestHelpers";
+import models from "../../models";
+import Boom from "boom";
+import { BAD_REQUEST_ERRORS } from "../../../../sharedUtilities/errorMessageConstants";
 
-test("Pass", () => {
-  expect(true).toBeTrue();
+const httpMocks = require("node-mocks-http");
+
+describe("createMatrix handler", () => {
+  let request, response, next, matrixDetails, user;
+
+  beforeEach(() => {
+    user = "TEST_USER_NICKNAME";
+
+    matrixDetails = {
+      pibControlNumber: "2019-0001-R",
+      firstReviewer: "jacob@me.com",
+      secondReviewer: "wanchen@me.com"
+    };
+
+    request = httpMocks.createRequest({
+      method: "POST",
+      headers: {
+        authorization: "Bearer SOME_MOCK_TOKEN"
+      },
+      body: {
+        ...matrixDetails
+      },
+      nickname: user
+    });
+    response = httpMocks.createResponse();
+    next = jest.fn();
+  });
+
+  afterEach(async () => {
+    await cleanupDatabase();
+  });
+
+  test("should create matrix", async () => {
+    await createMatrix(request, response, next);
+    const insertedMatrix = await models.matrices.findOne({
+      where: { pibControlNumber: matrixDetails.pibControlNumber }
+    });
+
+    expect(insertedMatrix).toEqual(
+      expect.objectContaining({
+        pibControlNumber: matrixDetails.pibControlNumber,
+        firstReviewer: matrixDetails.firstReviewer,
+        secondReviewer: matrixDetails.secondReviewer
+      })
+    );
+  });
+
+  test("should send response and 201 status with created entity", async () => {
+    await createMatrix(request, response, next);
+    console.log("mamamia", response._getData());
+
+    expect(response.statusCode).toEqual(201);
+    expect(response._getData()).toEqual(
+      expect.objectContaining({
+        ...matrixDetails
+      })
+    );
+    expect(response._isEndCalled()).toBeTruthy();
+  });
 });
