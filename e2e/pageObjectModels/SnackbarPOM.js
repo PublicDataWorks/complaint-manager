db5e719510a085e510908ef265cc06ecc5e4ceb1
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-01-16 17:00:44 -0600

    Syd/Veronica/Katie/Karan [#170439387] Fix for new material UI snackbar timeouts on e2e tests

--- e2e/pageObjectModels/SnackbarPOM.js
+++ e2e/pageObjectModels/SnackbarPOM.js
@@ -2,12 +2,13 @@ const e2e = require("./e2eUtilities.js");
 
 const snackbarCommands = {
   presentWithRegex: function(regexexpression) {
+    this.waitForElementVisible("@snackbarText", e2e.rerenderWait);
     const text = this.getText("@snackbarText");
     const regex = new RegExp(regexexpression, "i");
     return this.assert.equal(true, regex.test(regexexpression, text));
   },
   presentWithMessage: function(message) {
-    return this.waitForElementPresent(
+    return this.waitForElementVisible(
       "@snackbarText",
       e2e.rerenderWait
     ).assert.containsText("@snackbarText", message);
