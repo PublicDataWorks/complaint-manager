c07d8141df01d3f51caf5c3cee591350b8bbb2ab
sydneybotz <sydneylaurabotz@gmail.com>
2020-01-27 17:08:32 -0600

    Syd [#169957657] content of notification drawer correctly styled

--- src/client/common/globalStyling/standards.js
+++ src/client/common/globalStyling/standards.js
@@ -8,7 +8,10 @@ const standards = {
   fontTiny: "0.75rem",
   fontSmall: "0.875rem",
   fontMedium: "1rem",
-  fontLarge: "1.3rem"
+  fontLarge: "1.3rem",
+
+  //Z-Index Standards
+  maxZIndex: 10000
 };
 
 export default standards;
