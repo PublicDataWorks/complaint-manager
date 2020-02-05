2dc5d939d1046db5207adf7450330f0fc33eadb4
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-02-05 15:13:09 -0600

    Sabrina [#168211056] Adds axios request interceptor for detecting offline events

---
+++ src/client/common/axiosInterceptors/ensureOnlineOnRequestInterceptor.test.js
@@ -0,0 +1,27 @@
+import getWorkingCases from "../../complaintManager/cases/thunks/getWorkingCases";
+import nock from "nock";
+import configureInterceptors from "./interceptors";
+import { snackbarError } from "../../complaintManager/actionCreators/snackBarActionCreators";
+
+jest.mock("../auth/getAccessToken", () => jest.fn(() => "TEST_TOKEN"));
+
+describe("ensureOnlineOnRequestInterceptor", () => {
+  const dispatch = jest.fn();
+  const sortBy = "sortBy";
+  const sortDirection = "sortDirection";
+
+  beforeEach(() => {
+    configureInterceptors({ dispatch });
+    dispatch.mockClear();
+  });
+
+  test("should dispatch snackbar error on loss of connection", async () => {
+    nock("http://localhost")
+      .get(`/api/cases?sortBy=${sortBy}&sortDirection=${sortDirection}`)
+
+    await getWorkingCases(sortBy, sortDirection)(dispatch);
+
+    window.dispatchEvent(new Event("offline"));
+    expect(dispatch).toHaveBeenCalledWith(snackbarError("No Internet Connection"));
+  });
+});
