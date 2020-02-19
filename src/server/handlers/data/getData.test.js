35e4a3ac30f68cdf876ac589c12b58b06a38ffec
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-02-19 15:30:05 -0600

    [Sabrina / Veronica] [##171221336] Include switch statement to Visualization component, transfer tests from getData to countComplainantsByIntakeSource (Query) test file, trying testing getData

--- src/server/handlers/data/getData.test.js
+++ src/server/handlers/data/getData.test.js
@@ -1,102 +1,22 @@
-import {
-  buildTokenWithPermissions,
-  cleanupDatabase,
-  expectResponse
-} from "../../testHelpers/requestTestHelpers";
-import models from "../../complaintManager/models";
 import request from "supertest";
 import app from "../../server";
-import Case from "../../../client/complaintManager/testUtilities/case";
-import { BAD_REQUEST_ERRORS } from "../../../sharedUtilities/errorMessageConstants";
+import {
+  buildTokenWithPermissions,
+  expectResponse
+} from "../../testHelpers/requestTestHelpers";
+import { executeQuery } from "./queries/countComplaintsByIntakeSource";
 
 describe("getData", () => {
-  afterEach(async () => {
-    await cleanupDatabase();
-  });
-
-  test("returns count of complaints broken down by Intake Source", async done => {
-    const token = buildTokenWithPermissions("", "tuser");
-
-    const emailIntakeSource = await models.intake_source.create({
-      name: "Email"
-    });
-    const facebookIntakeSource = await models.intake_source.create({
-      name: "Facebook"
-    });
-    const otherIntakeSource = await models.intake_source.create({
-      name: "Other"
-    });
-
-    await models.cases.create(
-      new Case.Builder()
-        .defaultCase()
-        .withId(undefined)
-        .withIntakeSourceId(emailIntakeSource.id),
-      {
-        auditUser: "someone"
-      }
-    );
-
-    await models.cases.create(
-      new Case.Builder()
-        .defaultCase()
-        .withIntakeSourceId(facebookIntakeSource.id),
-      {
-        auditUser: "someone"
-      }
-    );
-
-    await models.cases.create(
-      new Case.Builder()
-        .defaultCase()
-        .withId(undefined)
-        .withIntakeSourceId(facebookIntakeSource.id),
-      {
-        auditUser: "someone"
-      }
-    );
-
-    await models.cases.create(
-      new Case.Builder()
-        .defaultCase()
-        .withId(undefined)
-        .withIntakeSourceId(otherIntakeSource.id),
-      {
-        auditUser: "someone"
-      }
-    );
-
-    const expectedData = [
-      { cases: "1", name: "Email" },
-      { cases: "2", name: "Facebook" },
-      { cases: "1", name: "Other" }
-    ];
-
-    const responsePromise = request(app)
-      .get("/api/data")
-      .set("Content-Header", "application/json")
-      .set("Authorization", `Bearer ${token}`)
-      .query({ queryType: "countComplaintsByIntakeSource" });
-
-    await expectResponse(responsePromise, 200, expectedData);
-    done();
-  });
-
-  test("throws an error when query param is not supported", async done => {
-    const unknownQueryType = "unknown";
+  test("should call countComplaintsByIntakeSource.executeQuery", () => {
     const token = buildTokenWithPermissions("", "tuser");
 
     request(app)
       .get("/api/data")
       .set("Content-Header", "application/json")
       .set("Authorization", `Bearer ${token}`)
-      .query({ queryType: unknownQueryType })
-      .expect(400)
-      .then(response => {
-        expect(response.body.message).toContain(
-          BAD_REQUEST_ERRORS.DATA_QUERY_TYPE_NOT_SUPPORTED
-        );
-        done();
-      });
+      .query({ queryType: "countComplaintsByIntakeSource" });
+
+    // expect(responsePromise).toHaveBeenCalledWith(executeQuery);
+    // done();
   });
 });
