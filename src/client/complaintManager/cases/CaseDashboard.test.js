ba42a538c3c2e681c70b15f5c3b8199c172b281f
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-02-21 14:32:52 -0600

    Sabrina/Syd [#171221336] Added feature toggle for visualizations and stopped plotly visualizations from rendering in client tests

--- src/client/complaintManager/cases/CaseDashboard.test.js
+++ src/client/complaintManager/cases/CaseDashboard.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { mount } from "enzyme";
+import React, { Component } from "react";
+import { mount, render } from "enzyme";
 import CaseDashboard from "./CaseDashboard";
 import NavBar from "../shared/components/NavBar/NavBar";
 import { Provider } from "react-redux";
@@ -12,10 +12,13 @@ import Case from "../testUtilities/case";
 import getWorkingCases from "./thunks/getWorkingCases";
 import { containsText } from "../../testHelpers";
 import { DESCENDING, SORT_CASES_BY } from "../../../sharedUtilities/constants";
+import { getFeaturesSuccess } from "../actionCreators/featureTogglesActionCreators";
 
 jest.mock("./thunks/getWorkingCases", () => () => ({
   type: "MOCK_GET_CASES_THUNK"
 }));
+
+jest.mock("../../common/components/Visualization/Visualization", () => 'Visualization');
 
 describe("CaseDashboard", () => {
   let caseDashboardWrapper, store, dispatchSpy, cases;
@@ -39,7 +42,7 @@ describe("CaseDashboard", () => {
     caseDashboardWrapper = mount(
       <Provider store={store}>
         <Router>
-          <CaseDashboard />
+          <CaseDashboard isTest={true}/>
         </Router>
       </Provider>
     );
@@ -71,5 +74,31 @@ describe("CaseDashboard", () => {
 
   test("should close snackbar when mounted", () => {
     expect(store.getState()).toHaveProperty("ui.snackbar.open", false);
+  });
+
+  describe("Data Visualization Feature Toggle", () => {
+    test("displays data visualizations when toggled on", () => {
+      store.dispatch(
+        getFeaturesSuccess({
+          dataVisualizationFeature: true
+        })
+      ); 
+      caseDashboardWrapper.update();
+      expect(
+        caseDashboardWrapper.find('[data-testid="dataVisualization"]').exists()
+      ).toBeTrue();
+    });
+
+    test("does not display data visualizations when toggled off", () => {
+      store.dispatch(
+        getFeaturesSuccess({
+          dataVisualizationFeature: false
+        })
+      );
+      caseDashboardWrapper.update();
+      expect(
+        caseDashboardWrapper.find('[data-testid="dataVisualization"]').exists()
+      ).toBeFalse();
+    });
   });
 });
