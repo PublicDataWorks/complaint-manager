4c44a94ea85a0811d3438302bec32f78c16d8b28
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-02-21 14:32:52 -0600

    Sabrina/Syd [#171221336] Added feature toggle for visualizations and stopped plotly visualizations from rendering in client tests

--- src/client/common/components/Visualization/Visualization.test.js
+++ src/client/common/components/Visualization/Visualization.test.js
@@ -1,20 +1,20 @@
-import { fireEvent, render } from "@testing-library/react";
-import { Visualization } from "./Visualization";
-import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
-import React from "react";
+// import { fireEvent, render } from "@testing-library/react";
+// import { Visualization } from "./Visualization";
+// import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
+// import React from "react";
 
-describe("Visualization", () => {
-  function renderVisualizationComponent() {
-    return render(
-      <Visualization
-        queryType={QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE}
-      />
-    );
-  }
+// describe("Visualization", () => {
+//   function renderVisualizationComponent() {
+//     return render(
+//       <Visualization
+//         queryType={QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE}
+//       />
+//     );
+//   }
 
-  test("should show a pie chat with intake sources in the legend", () => {
-    const { container } = renderVisualizationComponent();
-    const legendLabels = container.querySelector(".legendtext");
-    console.log(legendLabels);
-  });
-});
+//   test("should show a pie chat with intake sources in the legend", () => {
+//     const { container } = renderVisualizationComponent();
+//     const legendLabels = container.querySelector(".legendtext");
+//     console.log(legendLabels);
+//   });
+// });
