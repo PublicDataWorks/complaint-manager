266ba909fa000a86fa8f87ef17de2a5402e7a057
wanwanorange <wanchen.y95@gmail.com>
2020-02-21 10:38:32 -0600

    [Wanchen][#169959182] Write e2e test for user mention and case note creation

---
+++ e2e/pageObjectModels/CaseNoteDialog.js
@@ -0,0 +1,57 @@
+const e2e = require("./e2eUtilities");
+
+const caseTagDialogCommands = {
+  dialogIsOpen: function() {
+    return this.waitForElementVisible("@dialogTitle", e2e.rerenderWait);
+  },
+  setActionTaken: function() {
+    return this.waitForElementPresent("@actionTakenField", e2e.rerenderWait)
+      .click("@actionTakenField")
+      .waitForElementPresent("@actionTakenMenu", e2e.rerenderWait)
+      .moveToElement("@actionTakenToSelect", 20, 20)
+      .click("@actionTakenToSelect")
+      .waitForElementNotPresent("@actionTakenMenu", e2e.rerenderWait);
+  },
+  writeCaseNote: function(caseNote) {
+    return this.setValue("@caseNoteField", caseNote);
+  },
+  tagPersonInCaseNote: function(taggedPerson) {
+    this.setValue("@caseNoteField", "@");
+    this.waitForElementVisible("@userDropdown", e2e.rerenderWait);
+    this.setValue("@caseNoteField", [
+      taggedPerson,
+      this.api.Keys.ENTER
+    ]).api.pause(e2e.pause);
+    return this;
+  },
+  clickSubmitButton: function() {
+    return this.click("@submitCaseNoteButton");
+  }
+};
+
+module.exports = {
+  commands: [caseTagDialogCommands],
+  elements: {
+    dialogTitle: {
+      selector: "[data-testid='caseNoteDialogTitle']"
+    },
+    actionTakenField: {
+      selector: "[data-testid='actionTakenInput']+div>button"
+    },
+    actionTakenMenu: {
+      selector: ".MuiAutocomplete-popper"
+    },
+    actionTakenToSelect: {
+      selector: '[data-option-index="2"]'
+    },
+    caseNoteField: {
+      selector: "[data-testid='notesInput']"
+    },
+    userDropdown: {
+      selector: "[data-testid='user-dropdown']"
+    },
+    submitCaseNoteButton: {
+      selector: "[data-testid='submitButton']"
+    }
+  }
+};
