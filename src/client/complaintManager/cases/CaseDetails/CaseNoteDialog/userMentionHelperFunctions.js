1482db5ddfc6549dd0789dda6bd809384295b094
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-03-02 10:58:34 -0600

    [Syd/Veronica] [#170429184] sort users by label length to deal with users' names that are substrings of each other

--- src/client/complaintManager/cases/CaseDetails/CaseNoteDialog/userMentionHelperFunctions.js
+++ src/client/complaintManager/cases/CaseDetails/CaseNoteDialog/userMentionHelperFunctions.js
@@ -4,15 +4,27 @@ export const getMentionedUsers = (users, caseNoteText) => {
   let updatedMentionedUsers = [];
   caseNoteText = caseNoteText.substring(caseNoteText.indexOf("@"));
   const splitCaseNotes = caseNoteText.split("@");
+
+  const compare = (user1, user2) => {
+    if (user1.label.length > user2.label.length) return -1;
+    if (user1.label.length < user2.label.length) return 1;
+
+    return 0;
+  };
+
+  const sortedUsers = [...users];
+  sortedUsers.sort(compare);
+
   for (const subStringIndex in splitCaseNotes) {
     let subString = splitCaseNotes[subStringIndex];
-    for (const user in users) {
-      let userName = users[user].label;
+    for (const user in sortedUsers) {
+      let userName = sortedUsers[user].label;
       if (
         subString.startsWith(userName) &&
-        !updatedMentionedUsers.includes(users[user])
+        !updatedMentionedUsers.includes(sortedUsers[user])
       ) {
-        updatedMentionedUsers.push(users[user]);
+        updatedMentionedUsers.push(sortedUsers[user]);
+        break;
       }
     }
   }
