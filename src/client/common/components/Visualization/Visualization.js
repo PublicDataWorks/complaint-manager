a557d3842d98a555ffa9d7b6618a69710b90c13d
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-03-05 10:32:36 -0600

    Sabrina/Syd [#171221336] Refactored Visualization back to functional component and updated Visualization.test

--- src/client/common/components/Visualization/Visualization.js
+++ src/client/common/components/Visualization/Visualization.js
@@ -1,54 +1,49 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import axios from "axios";
 import * as countComplaintsByIntakeSource from "./Transformers/countComplaintsByIntakeSource";
 import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
 import { BAD_REQUEST_ERRORS } from "../../../../sharedUtilities/errorMessageConstants";
-import { PlotlyWrapper } from "./PlotlyWrapper";
+import {PlotlyWrapper} from "./PlotlyWrapper";
 
-class Visualization extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      isFetching: false
-    };
-  }
-
-  async componentDidMount() {
-    this.setState({ ...this.state, isFetching: true });
-    await axios
-      .get(`/api/data?queryType=${this.props.queryType}`)
-      .then(response => {
-        let transformedData;
-        switch (this.props.queryType) {
-          case QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE:
-            transformedData = countComplaintsByIntakeSource.transformData(
-              response.data
-            );
-            break;
-          default:
-            throw new Error(BAD_REQUEST_ERRORS.DATA_QUERY_TYPE_NOT_SUPPORTED);
-        }
-        this.setState({ isFetching: false, data: transformedData });
-      })
-      .catch(error => {
-        this.setState({ ...this.state, isFetching: false });
-      })
-  }
-
-  render() {
+const Visualization = (props) => {
+    const [data, setData] = useState({ data: {}, isFetching: false });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setData({ data: data.data, isFetching: true });
+                const response = await axios.get(
+                    `/api/data?queryType=${props.queryType}`
+                );
+                let transformedData;
+                switch (props.queryType) {
+                    case QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE:
+                        transformedData = countComplaintsByIntakeSource.transformData(
+                            response.data
+                        );
+                        break;
+                    default:
+                        throw new Error(BAD_REQUEST_ERRORS.DATA_QUERY_TYPE_NOT_SUPPORTED);
+                }
+                setData({ data: transformedData, isFetching: false });
+            } catch (e) {
+                console.log(e);
+                setData({ data: data.data, isFetching: false });
+            }
+        };
+        fetchData();
+    }, []);
     return (
         <PlotlyWrapper
-          data={[this.state.data]}
-          layout={{
-            width: 500,
-            height: 500,
-            title: "Complaints by Intake Source",
-            margin: 20
-          }}
+            data={[data.data]}
+            layout={{
+                width: 500,
+                height: 500,
+                title: "Complaints by Intake Source",
+                margin: 20
+            }}
         />
     );
-  }
 }
 
 export default Visualization;
