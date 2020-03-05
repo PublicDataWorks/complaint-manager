0ba2b7d23e1dfb1dac49982cb11a5c5f61bc6c5b
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-03-05 10:32:36 -0600

    Sabrina/Syd [#171221336] Refactored Visualization back to functional component and updated Visualization.test

--- src/client/common/components/Visualization/Visualization.test.js
+++ src/client/common/components/Visualization/Visualization.test.js
@@ -1,10 +1,9 @@
 import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
 import { PlotlyWrapper } from "./PlotlyWrapper";
 import React from "react";
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import Visualization from "./Visualization";
 import axios from "axios";
-
 
 jest.mock("./PlotlyWrapper", () => {
   const FakeWrapper = jest.fn(() => "PlotlyWrapper");
@@ -13,38 +12,45 @@ jest.mock("./PlotlyWrapper", () => {
 jest.mock("axios");
 
 describe("Visualization", () => {
-
   test("should pass correct data and layout options to PlotlyWrapper", async () => {
     // Arrange
-    const responseBody = { data: [
-      { cases: "2", name: "Email" },
-      { cases: "5", name: "Facebook" },
-      { cases: "3", name: "Other" }
-    ]};
-    axios.get.mockResolvedValue({ ...responseBody});
+    const responseBody = {
+      data: [
+        { cases: "2", name: "Email" },
+        { cases: "5", name: "Facebook" },
+        { cases: "3", name: "Other" }
+      ]
+    };
+
+    axios.get.mockResolvedValue({ ...responseBody });
 
     // Act
-    await render(
-      <Visualization
-        queryType={QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE}
-      />
-    );
+    await act(async () => {
+      render(
+        <Visualization
+          queryType={QUERY_TYPES.COUNT_COMPLAINTS_BY_INTAKE_SOURCE}
+        />
+      );
+    });
 
     // Assert
     expect(PlotlyWrapper).toHaveBeenCalledWith(
-            expect.objectContaining({
-                data: expect.arrayContaining([expect.objectContaining({
-                    labels: expect.arrayContaining(["Email", "Facebook", "Other"]),
-                    type: "pie",
-                    values: expect.arrayContaining([2, 5, 3])
-                })]),
-                layout: expect.objectContaining({
-                    height: 500,
-                    margin: 20,
-                    title: "Complaints by Intake Source",
-                    width: 500
-                })
-            }), {}
+      expect.objectContaining({
+        data: expect.arrayContaining([
+          expect.objectContaining({
+            labels: expect.arrayContaining(["Email", "Facebook", "Other"]),
+            type: "pie",
+            values: expect.arrayContaining([2, 5, 3])
+          })
+        ]),
+        layout: expect.objectContaining({
+          height: 500,
+          margin: 20,
+          title: "Complaints by Intake Source",
+          width: 500
+        })
+      }),
+      {}
     );
   });
 });
