2abff9bb1e7fc238cf11724a1e629768ac123560
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-03-06 13:09:33 -0600

    Syd/Sabrina [#171531805] Added onBlur to Narrative fields, removed save narrative button, and  updated tests

--- src/client/complaintManager/cases/CaseDetails/Narrative.js
+++ src/client/complaintManager/cases/CaseDetails/Narrative.js
@@ -8,97 +8,96 @@ import { NARRATIVE_FORM } from "../../../../sharedUtilities/constants";
 import { renderTextField } from "../sharedFormComponents/renderFunctions";
 
 const Narrative = props => {
+  const onBlur = () =>  {
+    if(props.dirty && !props.isArchived){
+      props.dispatch(submit(NARRATIVE_FORM));
+    }
+  };
+
   return (
-    <BaseCaseDetailsCard title="Narrative">
-      <CardContent>
-        <Typography
+      <BaseCaseDetailsCard title="Narrative">
+        <CardContent>
+          <Typography
+            data-testid={"narrativePromptDetails"}
+            style={{
+              marginBottom: "24px"
+            }}
+          >
+            Record information gained during the intake process. This
+            information will be used to populate a detailed account section of
+            the referral letter.
+          </Typography>
+          <form data-testid="createUserForm">
+            <Field
+              name="narrativeSummary"
+              label="Narrative Summary"
+              onBlur={onBlur}
+              disabled={props.isArchived}
+              component={renderTextField}
+              fullWidth
+              multiline
+              rowsMax={5}
+              placeholder={
+                props.isArchived
+                  ? ""
+                  : "Enter a brief, 2-3 sentence summary of the incident"
+              }
+              inputProps={{
+                "data-testid": "narrativeSummaryInput",
+                maxLength: 500,
+                style: {
+                  color: "black"
+                }
+              }}
+              InputLabelProps={{
+                style: {
+                  color: "black"
+                },
+                shrink: true
+              }}
+              data-testid="narrativeSummaryField"
+              style={{ marginBottom: "24px" }}
+            />
+            <Field
+              name="narrativeDetails"
+              label="Narrative Details"
+              onBlur={onBlur}
+              disabled={props.isArchived}
+              component={renderTextField}
+              fullWidth
+              multiline
+              rowsMax={20}
+              rows={5}
+              placeholder={
+                props.isArchived
+                  ? ""
+                  : "Enter a transcript or details of the incident"
+              }
+              inputProps={{
+                "data-testid": "narrativeDetailsInput",
+                style: {
+                  color: "black"
+                }
+              }}
+              InputLabelProps={{
+                style: {
+                  color: "black"
+                },
+                shrink: true
+              }}
+              data-testid="narrativeDetailsField"
+            />
+          </form>
+        </CardContent>
+        <CardActions
           style={{
-            marginBottom: "24px"
+            justifyContent: "flex-end",
+            paddingRight: "0px",
+            padding: "0px 16px 16px 0px"
           }}
         >
-          Record information gained during the intake process. This information
-          will be used to populate a detailed account section of the referral
-          letter.
-        </Typography>
-        <form data-testid="createUserForm">
-          <Field
-            name="narrativeSummary"
-            label="Narrative Summary"
-            disabled={props.isArchived}
-            component={renderTextField}
-            fullWidth
-            multiline
-            rowsMax={5}
-            placeholder={
-              props.isArchived
-                ? ""
-                : "Enter a brief, 2-3 sentence summary of the incident"
-            }
-            inputProps={{
-              "data-testid": "narrativeSummaryInput",
-              maxLength: 500,
-              style: {
-                color: "black"
-              }
-            }}
-            InputLabelProps={{
-              style: {
-                color: "black"
-              },
-              shrink: true
-            }}
-            data-testid="narrativeSummaryField"
-            style={{ marginBottom: "24px" }}
-          />
-          <Field
-            name="narrativeDetails"
-            label="Narrative Details"
-            disabled={props.isArchived}
-            component={renderTextField}
-            fullWidth
-            multiline
-            rowsMax={20}
-            rows={5}
-            placeholder={
-              props.isArchived
-                ? ""
-                : "Enter a transcript or details of the incident"
-            }
-            inputProps={{
-              "data-testid": "narrativeDetailsInput",
-              style: {
-                color: "black"
-              }
-            }}
-            InputLabelProps={{
-              style: {
-                color: "black"
-              },
-              shrink: true
-            }}
-            data-testid="narrativeDetailsField"
-          />
-        </form>
-      </CardContent>
-      <CardActions
-        style={{
-          justifyContent: "flex-end",
-          paddingRight: "0px",
-          padding: "0px 16px 16px 0px"
-        }}
-      >
-        {props.isArchived ? null : (
-          <PrimaryButton
-            data-testid="saveNarrative"
-            disabled={props.pristine}
-            onClick={() => props.dispatch(submit(NARRATIVE_FORM))}
-            style={{ margin: "0px" }}
-          >
-            Save
-          </PrimaryButton>
-        )}
-      </CardActions>
-    </BaseCaseDetailsCard>
+        </CardActions>
+      </BaseCaseDetailsCard>
   );
 };
 
