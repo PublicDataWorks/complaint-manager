cdc9378fa2b716e197e34c3f704ed7fb58ea01d8
sydneybotz <sydneylaurabotz@gmail.com>
2020-03-17 16:16:24 -0500

    Veronica/Syd [#170114736] handler to get all notifications in last 30 days for specifed user

---
+++ src/server/handlers/cases/getNotifications.test.js
@@ -0,0 +1,123 @@
+import CaseNote from "../../../client/complaintManager/testUtilities/caseNote";
+import Case from "../../../client/complaintManager/testUtilities/case";
+import Notification from "../../../client/complaintManager/testUtilities/notification";
+import { cleanupDatabase } from "../../testHelpers/requestTestHelpers";
+import getNotifications from "./getNotifications";
+import { now } from "moment";
+const models = require("../../complaintManager/models");
+const httpMocks = require("node-mocks-http");
+
+describe("getNotifications", () => {
+  let request, response, next, currentCaseNote, currentNotif, timestamp;
+
+  beforeEach(async () => {
+    timestamp = new Date(now());
+    const caseAttributes = new Case.Builder().defaultCase();
+    const currentCase = await models.cases.create(caseAttributes, {
+      auditUser: "tuser"
+    });
+
+    const caseNoteAttributes = new CaseNote.Builder()
+      .defaultCaseNote()
+      .withCaseId(currentCase.id);
+
+    currentCaseNote = await models.case_note.create(caseNoteAttributes, {
+      auditUser: "tuser"
+    });
+
+    const notificationAttributes = new Notification.Builder()
+      .defaultNotification()
+      .withCaseNoteId(currentCaseNote.id)
+      .withUser("seanrut@gmail.com");
+
+    currentNotif = await models.notification.create(notificationAttributes, {
+      auditUser: "tuser"
+    });
+
+    const otherNotificationAttributes = new Notification.Builder()
+      .defaultNotification()
+      .withCaseNoteId(currentCaseNote.id)
+      .withUser("veronica@gmail.com");
+
+    await models.notification.create(otherNotificationAttributes, {
+      auditUser: "tuser"
+    });
+
+    request = httpMocks.createRequest({
+      method: "GET",
+      headers: {
+        authorization: "Bearer SOME_MOCK_TOKEN"
+      },
+      params: { user: currentNotif.user, timestamp: timestamp },
+      nickname: "tuser"
+    });
+
+    response = httpMocks.createResponse();
+    next = jest.fn();
+  });
+
+  afterEach(async () => {
+    await cleanupDatabase();
+  });
+
+  test("should return notification to user that was mentioned", async () => {
+    await getNotifications(request, response, next);
+
+    expect(response._getData()).toEqual([
+      expect.objectContaining({
+        user: "seanrut@gmail.com"
+      })
+    ]);
+  });
+
+  test("should not return notifications that were updated or created more than 30 days ago", async () => {
+    request.params.timestamp = new Date(now());
+
+    const notificationAttributes = new Notification.Builder()
+      .defaultNotification()
+      .withCaseNoteId(currentCaseNote.id)
+      .withUser("seanrut@gmail.com");
+
+    await models.notification.create(notificationAttributes, {
+      auditUser: "tuser"
+    });
+
+    await getNotifications(request, response, next);
+
+    expect(response._getData()).toEqual([
+      expect.objectContaining({
+        user: "seanrut@gmail.com"
+      })
+    ]);
+  });
+
+  test("when user is mentioned more than once, user should receive all pertaining notifications", async () => {
+    const notificationAttributes = new Notification.Builder()
+      .defaultNotification()
+      .withCaseNoteId(currentCaseNote.id)
+      .withUser("seanrut@gmail.com");
+
+    await models.notification.create(notificationAttributes, {
+      auditUser: "tuser"
+    });
+
+    await getNotifications(request, response, next);
+
+    expect(response._getData()).toEqual([
+      expect.objectContaining({
+        user: "seanrut@gmail.com"
+      }),
+      expect.objectContaining({
+        user: "seanrut@gmail.com"
+      })
+    ]);
+  });
+
+  test("when notification is deleted, user should not receive the notification", async () => {
+    await currentNotif.destroy({ auditUser: "tuser" });
+
+    await getNotifications(request, response, next);
+
+    expect(response._getData()).toEqual([]);
+  });
+});
