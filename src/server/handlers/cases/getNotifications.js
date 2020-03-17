bc78058bd301fbff639125bcfe57ff4b67c284d6
sydneybotz <sydneylaurabotz@gmail.com>
2020-03-17 16:16:24 -0500

    Veronica/Syd [#170114736] handler to get all notifications in last 30 days for specifed user

---
+++ src/server/handlers/cases/getNotifications.js
@@ -0,0 +1,23 @@
+const asyncMiddleWare = require("../asyncMiddleware");
+const models = require("../../complaintManager/models/index");
+import sequelize from "sequelize";
+
+const getNotifications = asyncMiddleWare(async (request, response, next) => {
+  const params = {
+    where: {
+      updatedAt: { [sequelize.Op.gt]: request.params.timestamp },
+      user: request.params.user
+    }
+  };
+
+  const notifications = await models.sequelize.transaction(
+    async transaction => {
+      const allNotifications = await models.notification.findAll(params);
+      return allNotifications;
+    }
+  );
+
+  response.send(notifications);
+});
+
+module.exports = getNotifications;
