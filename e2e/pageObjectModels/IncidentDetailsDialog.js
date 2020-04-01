2c3bca5dd4d7e10cbfb139d0d6199751134a48ec
sydneybotz <sydneylaurabotz@gmail.com>
2020-04-01 09:51:35 -0500

    Syd [#171530708] update e2e tests to expect address not to fill in input when arrowing through suggestions

--- e2e/pageObjectModels/IncidentDetailsDialog.js
+++ e2e/pageObjectModels/IncidentDetailsDialog.js
@@ -1,37 +1,37 @@
 const e2e = require("./e2eUtilities.js");
 
 const incidentDetailsDialogCommands = {
-  dialogIsOpen: function() {
+  dialogIsOpen: function () {
     return this.waitForElementVisible("@dialogTitle", e2e.rerenderWait);
   },
-  typeInAddress: function(addressInput) {
+  typeInAddress: function (addressInput) {
     return this.setValue("@addressSuggestionField", [addressInput]);
   },
-  saveIncidentDetails: function() {
+  saveIncidentDetails: function () {
     return this.waitForElementVisible(
       "@saveIncidentDetailsButton",
       e2e.rerenderWait
     ).click("@saveIncidentDetailsButton");
   },
-  fillAddress: function() {
+  fillAddress: function () {
     return this.waitForElementVisible(
       "@fillAddressLink",
       e2e.rerenderWait
     ).click("@fillAddressLink");
   },
-  setIncidentDate: function() {
+  setIncidentDate: function () {
     return this.waitForElementVisible(
       "@incidentDate",
       e2e.rerenderWait
     ).setValue("@incidentDate", "12/20/2017");
   },
-  setIncidentTime: function() {
+  setIncidentTime: function () {
     return this.waitForElementVisible(
       "@incidentTime",
       e2e.rerenderWait
     ).setValue("@incidentTime", "23:111");
   },
-  setDistrict: function(districtId) {
+  setDistrict: function (districtId) {
     return this.waitForElementVisible("@districtDropdown", e2e.rerenderWait)
       .click("@districtDropdown")
       .waitForElementVisible("@districtMenu", e2e.rerenderWait)
@@ -47,7 +47,7 @@ module.exports = {
       selector: "[data-testid='editIncidentDetailsTitle']"
     },
     addressSuggestionField: {
-      selector: '[data-testid="addressSuggestionField"] > input'
+      selector: '[data-testid="addressSuggestionField"]'
     },
     saveIncidentDetailsButton: {
       selector: '[data-testid="saveIncidentDetailsButton"]'
