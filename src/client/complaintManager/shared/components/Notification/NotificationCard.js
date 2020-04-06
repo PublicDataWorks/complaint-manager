284ef29abd0d51fbdd0e378223b7a17fa7d46a45
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-04-06 16:57:42 -0500

    Syd/Sabrina [#170117762] display notifications with MUI buttons

--- src/client/complaintManager/shared/components/Notification/NotificationCard.js
+++ src/client/complaintManager/shared/components/Notification/NotificationCard.js
@@ -1,8 +1,6 @@
 import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
 import moment from "moment";
 
 const NotificationCard = props => {
@@ -13,21 +11,16 @@ const NotificationCard = props => {
     NOTIFICATION_TIME_FORMAT
   );
   return (
-    <div>
-      <ListItem style={{ backgroundColor: "white", width: "300px" }}>
-        <ListItemText
-          primary={
-            <React.Fragment>
-              <Typography component="span" style={{ fontWeight: 600 }}>
-                {title}
-              </Typography>
-            </React.Fragment>
-          }
-          secondary={timestamp}
-        />
-      </ListItem>
-      <Divider component="li" />
-    </div>
+    <ListItemText
+      primary={
+        <React.Fragment>
+          <Typography component="span" style={{ fontWeight: 600 }}>
+            {title}
+          </Typography>
+        </React.Fragment>
+      }
+      secondary={timestamp}
+    />
   );
 };
 
