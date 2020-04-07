235f145bc2ae032e3dcaed55b50b6d44f2d70ba3
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-04-07 12:02:07 -0500

    Sabrina/Syd [#170117762] clicking notification routes to case details for that notification

--- src/client/complaintManager/shared/components/Notification/NotificationList.test.js
+++ src/client/complaintManager/shared/components/Notification/NotificationList.test.js
@@ -1,5 +1,5 @@
 import createConfiguredStore from "../../../../createConfiguredStore";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { getFeaturesSuccess } from "../../../actionCreators/featureTogglesActionCreators";
@@ -10,8 +10,14 @@ import "@testing-library/jest-dom";
 import { getUsersSuccess } from "../../../../common/actionCreators/usersActionCreators";
 import { getNotificationsSuccess } from "../../../actionCreators/notificationActionCreators";
 import getUsers from "../../../../common/thunks/getUsers";
+import getCaseDetails from "../../../cases/thunks/getCaseDetails";
 
 jest.mock("../../../../common/thunks/getUsers", () => values => ({
+  type: "MOCK_THUNK",
+  values
+}));
+
+jest.mock("../../../cases/thunks/getCaseDetails", () => values => ({
   type: "MOCK_THUNK",
   values
 }));
@@ -33,10 +39,7 @@ describe("notification list", () => {
         notificationFeature: true
       })
     );
-    return wrapper;
-  };
 
-  test("should render 2 notification cards if the user has 2 notifications", async () => {
     store.dispatch(
       getUsersSuccess([
         { email: "veronicablackwel@tw.com", name: "Veronica B" },
@@ -52,17 +55,23 @@ describe("notification list", () => {
           updatedAt: "2020-03-19T18:57:31.953Z",
           caseReference: "AC2020-0004",
           mentioner: "sydbotz@tw.com",
-          id: 1
+          id: 1,
+          caseId: 4
         },
         {
           user: "veronicablackwell@tw.com",
           updatedAt: "2019-11-29T19:31:41.953Z",
           caseReference: "CC2019-0018",
           mentioner: "wanchenyao@tw.com",
-          id: 2
+          id: 2,
+          caseId: 18
         }
       ])
     );
+    return wrapper;
+  };
+
+  test("should render 2 notification cards if the user has 2 notifications", async () => {
     const { queryByText } = renderNotificationList();
 
     await wait(() => {
@@ -82,5 +91,23 @@ describe("notification list", () => {
     renderNotificationList();
 
     expect(dispatchSpy).toHaveBeenCalledWith(getUsers());
+  });
+
+  test("getCaseDetails should be dispatched when a notification card is clicked", () => {
+    const { getAllByTestId } = renderNotificationList();
+
+    const notificationCard = getAllByTestId("notificationCard")[1];
+
+    fireEvent.click(notificationCard);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(getCaseDetails(18));
+  });
+
+  test("notification card should reference correct case details link", () => {
+    const { getAllByTestId } = renderNotificationList();
+
+    const notificationCard = getAllByTestId("notificationCard")[1];
+
+    expect(notificationCard.href).toEqual(`${window.location.origin}/cases/18`);
   });
 });
