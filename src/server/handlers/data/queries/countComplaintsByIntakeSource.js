043ab4e8b05198d8e628aec502f0bf57cbce1c73
sydneybotz <sydneylaurabotz@gmail.com>
2020-04-27 15:53:04 -0500

    Sabrina/Syd [#172228008] Complaints By Intake Source query returns cases where first contact date is year to date and status is closed or forwarded to agency

--- src/server/handlers/data/queries/countComplaintsByIntakeSource.js
+++ src/server/handlers/data/queries/countComplaintsByIntakeSource.js
@@ -1,12 +1,15 @@
 import models from "../../../complaintManager/models";
 import sequelize from "sequelize";
+import { CASE_STATUS } from "../../../../sharedUtilities/constants";
 
 export const executeQuery = async () => {
   const date = new Date();
+  const yearToDate = date.setFullYear(date.getFullYear(), 0, 1);
+
   const where = {
     deletedAt: null,
-    firstContactDate: 
-    {[sequelize.Op.gt]: date.setFullYear(date.getFullYear() - 1)}
+    firstContactDate: { [sequelize.Op.gte]: yearToDate },
+    status: [CASE_STATUS.FORWARDED_TO_AGENCY, CASE_STATUS.CLOSED]
   };
 
   const queryOptions = {
--- src/server/handlers/data/queries/countComplaintsByIntakeSource.test.js
+++ src/server/handlers/data/queries/countComplaintsByIntakeSource.test.js
@@ -8,16 +8,36 @@ import request from "supertest";
 import { BAD_REQUEST_ERRORS } from "../../../../sharedUtilities/errorMessageConstants";
 import Case from "../../../../client/complaintManager/testUtilities/case";
 import app from "../../../server";
-import { response } from "express";
+import { CASE_STATUS } from "../../../../sharedUtilities/constants";
 
 describe("executeQuery", () => {
   afterEach(async () => {
     await cleanupDatabase();
   });
+  const token = buildTokenWithPermissions("", "tuser");
 
-  test("returns count of complaints broken down by Intake Source", async done => {
-    const token = buildTokenWithPermissions("", "tuser");
+  const updateCaseStatus = async (caseToUpdate, status) => {
+    const caseStatusList = [
+      CASE_STATUS.ACTIVE,
+      CASE_STATUS.LETTER_IN_PROGRESS,
+      CASE_STATUS.READY_FOR_REVIEW,
+      CASE_STATUS.FORWARDED_TO_AGENCY,
+      CASE_STATUS.CLOSED
+    ];
 
+    for (const caseStatus in caseStatusList) {
+      await caseToUpdate.update(
+        { status: caseStatusList[caseStatus] },
+        { auditUser: "someone" }
+      );
+      if (caseStatusList[caseStatus] === status) {
+        return;
+      }
+    }
+    caseToUpdate.reload();
+  };
+
+  beforeEach(async () => {
     const emailIntakeSource = await models.intake_source.create({
       name: "Email"
     });
@@ -28,7 +48,7 @@ describe("executeQuery", () => {
       name: "Other"
     });
 
-    await models.cases.create(
+    const firstCase = await models.cases.create(
       new Case.Builder()
         .defaultCase()
         .withFirstContactDate("2020-02-21")
@@ -39,7 +59,10 @@ describe("executeQuery", () => {
       }
     );
 
-    await models.cases.create(
+    await updateCaseStatus(firstCase, CASE_STATUS.FORWARDED_TO_AGENCY);
+    // firstCase.reload();
+
+    const secondCase = await models.cases.create(
       new Case.Builder()
         .defaultCase()
         .withFirstContactDate("2020-02-21")
@@ -49,7 +72,10 @@ describe("executeQuery", () => {
       }
     );
 
-    await models.cases.create(
+    await updateCaseStatus(secondCase, CASE_STATUS.FORWARDED_TO_AGENCY);
+    //secondCase.reload();
+
+    const thirdCase = await models.cases.create(
       new Case.Builder()
         .defaultCase()
         .withFirstContactDate("2020-02-21")
@@ -60,7 +86,10 @@ describe("executeQuery", () => {
       }
     );
 
-    await models.cases.create(
+    await updateCaseStatus(thirdCase, CASE_STATUS.CLOSED);
+    // thirdCase.reload();
+
+    const fourthCase = await models.cases.create(
       new Case.Builder()
         .defaultCase()
         .withFirstContactDate("2020-02-21")
@@ -70,7 +99,11 @@ describe("executeQuery", () => {
         auditUser: "someone"
       }
     );
+    await updateCaseStatus(fourthCase, CASE_STATUS.CLOSED);
+    // fourthCase.reload();
+  });
 
+  test("returns count of complaints broken down by Intake Source", async done => {
     const expectedData = [
       { cases: "1", name: "Email" },
       { cases: "2", name: "Facebook" },
@@ -107,5 +140,118 @@ describe("executeQuery", () => {
         );
         done();
       });
+  });
+
+  test("should return only cases within the current year to date", async () => {
+    const instagramIntakeSource = await models.intake_source.create({
+      name: "Instagram"
+    });
+
+    const oldCase = await models.cases.create(
+      new Case.Builder()
+        .defaultCase()
+        .withFirstContactDate("2019-12-31")
+        .withId(undefined)
+        .withIntakeSourceId(instagramIntakeSource.id),
+      {
+        auditUser: "someone"
+      }
+    );
+    await updateCaseStatus(oldCase, CASE_STATUS.FORWARDED_TO_AGENCY);
+
+    const expectedData = [
+      { cases: "1", name: "Email" },
+      { cases: "2", name: "Facebook" },
+      { cases: "1", name: "Other" }
+    ];
+
+    const responsePromise = request(app)
+      .get("/api/data")
+      .set("Content-Header", "application/json")
+      .set("Authorization", `Bearer ${token}`)
+      .query({ queryType: "countComplaintsByIntakeSource" });
+
+    await responsePromise.then(response => {
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toHaveLength(3);
+      expect(response.body).toEqual(expect.arrayContaining(expectedData));
+    });
+  });
+
+  test("should return only cases where status is forwarded to agency or closed", async () => {
+    const instagramIntakeSource = await models.intake_source.create({
+      name: "Instagram"
+    });
+
+    await models.cases.create(
+      new Case.Builder()
+        .defaultCase()
+        .withFirstContactDate("2020-02-21")
+        .withId(undefined)
+        .withIntakeSourceId(instagramIntakeSource.id),
+      {
+        auditUser: "someone"
+      }
+    );
+
+    const activeCase = await models.cases.create(
+      new Case.Builder()
+        .defaultCase()
+        .withFirstContactDate("2020-02-21")
+        .withId(undefined)
+        .withIntakeSourceId(instagramIntakeSource.id),
+      {
+        auditUser: "someone"
+      }
+    );
+
+    await updateCaseStatus(activeCase, CASE_STATUS.ACTIVE);
+
+    const letterInProgressCase = await models.cases.create(
+      new Case.Builder()
+        .defaultCase()
+        .withFirstContactDate("2020-02-21")
+        .withId(undefined)
+        .withIntakeSourceId(instagramIntakeSource.id),
+      {
+        auditUser: "someone"
+      }
+    );
+
+    await updateCaseStatus(
+      letterInProgressCase,
+      CASE_STATUS.LETTER_IN_PROGRESS
+    );
+
+    const readyForReviewCase = await models.cases.create(
+      new Case.Builder()
+        .defaultCase()
+        .withFirstContactDate("2020-02-21")
+        .withId(undefined)
+        .withIntakeSourceId(instagramIntakeSource.id),
+      {
+        auditUser: "someone"
+      }
+    );
+
+    await updateCaseStatus(readyForReviewCase, CASE_STATUS.READY_FOR_REVIEW);
+
+    const expectedData = [
+      { cases: "1", name: "Email" },
+      { cases: "2", name: "Facebook" },
+      { cases: "1", name: "Other" }
+    ];
+
+    const responsePromise = request(app)
+      .get("/api/data")
+      .set("Content-Header", "application/json")
+      .set("Authorization", `Bearer ${token}`)
+      .query({ queryType: "countComplaintsByIntakeSource" });
+
+    await responsePromise.then(response => {
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toHaveLength(3);
+      expect(response.body).toEqual(expect.arrayContaining(expectedData));
+    });
   });
 });
