887be09e211d0a09c8892ca90d2235b36397b06a
sydneybotz <sydneylaurabotz@gmail.com>
2020-05-12 10:10:50 -0500

    Sabrina/Syd [#172678797] updated data viz font to Roboto and add additional colors

--- src/client/common/components/Visualization/dataVizStyling.js
+++ src/client/common/components/Visualization/dataVizStyling.js
@@ -2,21 +2,25 @@ export const COLORS = [
   "#002171",
   "#5fad56",
   "#9d5d9b",
-  "#ffd100",
-  "#3D6094",
-  "#578585",
-  "#c3837d",
-  "#e3a957"
+  "#f0e443",
+  "#0072B2",
+  "#E69F01",
+  "#5B4C9C",
+  "#117733",
+  "#CD78A7",
+  "#89CCEE",
+  "#44AA99",
+  "#DDDDDD"
 ];
 
 export const TITLE_FONT = {
-  family: "Open Sans",
+  family: '"Roboto", "Helvetica", "Arial", sans-serif',
   size: 30,
   color: "#555555"
 };
 
 export const LABEL_FONT = {
-  family: "Open Sans",
+  family: '"Roboto", "Helvetica", "Arial", sans-serif',
   size: 12,
   color: "#777777"
 };
