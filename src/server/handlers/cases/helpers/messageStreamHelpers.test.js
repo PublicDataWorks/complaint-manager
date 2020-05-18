48329c881463a314165188c96d8fe56031581186
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-05-18 12:00:05 -0700

    Syd/Veronica [#172684161] refactor of sending messages through getMessageStream

---
+++ src/server/handlers/cases/helpers/messageStreamHelpers.test.js
@@ -0,0 +1,18 @@
+import { sendMessage } from "./messageStreamHelpers";
+
+const httpMocks = require("node-mocks-http");
+
+describe("message stream helpers", () => {
+  test("sendMessage should write message to response", () => {
+    const response = httpMocks.createResponse();
+    const user = { id: "test@test.com", response: response };
+    const type = "mockType";
+    const message = "we testing out here!";
+
+    sendMessage(type, user, message);
+
+    expect(user.response._getData()).toEqual(
+      'data: {"type":"mockType","message":"we testing out here!"}\n\n'
+    );
+  });
+});
