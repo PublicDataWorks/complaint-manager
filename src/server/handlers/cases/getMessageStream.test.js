33fdde26cc8e2bae3936be15c0a8fefa68a6faf9
sydneybotz <sydneylaurabotz@gmail.com>
2020-05-19 16:41:37 -0500

    Veronica/Syd [#172684161] all tabs will recieve notification updates (user can have multiple client connections for SSE)

--- src/server/handlers/cases/getMessageStream.test.js
+++ src/server/handlers/cases/getMessageStream.test.js
@@ -1,7 +1,7 @@
 import {
   getClients,
   getMessageStream,
-  isActiveClient,
+  getActiveClients,
   sendNotification
 } from "./getMessageStream";
 import mockFflipObject from "../../testHelpers/mockFflipObject";
@@ -70,50 +70,70 @@ describe("get message stream", () => {
     expect(response._getData()).toEqual(expect.stringContaining("connection"));
   });
 
-  test("server should maintain correct list of clients", async () => {
+  test("should add new client regardless of whether email already has a connection", async () => {
     const clients = getClients();
 
     expect(clients).toEqual([
-      expect.objectContaining({ id: "test@test.com" }),
-      expect.objectContaining({ id: "test1@test.com" }),
-      expect.objectContaining({ id: "test2@test.com" })
+      expect.objectContaining({ email: "test@test.com" }),
+      expect.objectContaining({ email: "test1@test.com" }),
+      expect.objectContaining({ email: "test2@test.com" })
     ]);
 
     request.nickname = "test@test.com";
 
     await getMessageStream(request, response, next);
 
-    const sameClients = getClients();
+    const newClients = getClients();
 
-    expect(sameClients).toEqual([
-      expect.objectContaining({ id: "test@test.com" }),
-      expect.objectContaining({ id: "test1@test.com" }),
-      expect.objectContaining({ id: "test2@test.com" })
+    expect(newClients).toEqual([
+      expect.objectContaining({ email: "test@test.com" }),
+      expect.objectContaining({ email: "test1@test.com" }),
+      expect.objectContaining({ email: "test2@test.com" }),
+      expect.objectContaining({ email: "test@test.com" })
     ]);
   });
 
-  test("should return only active clients", () => {
-    const activeClient = isActiveClient("test@test.com");
+  test("should return all active clients for each email", async () => {
+    request.nickname = "test@test.com";
 
-    expect(activeClient).toEqual(
-      expect.objectContaining({ id: "test@test.com" })
+    await getMessageStream(request, response, next);
+
+    const activeClients = getActiveClients("test@test.com");
+
+    expect(activeClients).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ email: "test@test.com" }),
+        expect.objectContaining({ email: "test@test.com" })
+      ])
     );
 
-    const undefindClient = isActiveClient("test3@test.com");
+    const undefinedClients = getActiveClients("test3@test.com");
 
-    expect(undefindClient).toEqual(undefined);
+    expect(undefinedClients).toEqual([]);
   });
 
   test("should call sendMessage and getNotifications from sendNotifications", async () => {
     await sendNotification("test@test.com");
-
     expect(sendMessage).toHaveBeenCalledWith(
       "notifications",
-      isActiveClient("test@test.com"),
+      getActiveClients("test@test.com")[0],
       [
         { user: "wancheny@gmail.com", hasBeenRead: true },
         { user: "random@gmail.com", hasBeenRead: false }
       ]
+    );
+  });
+
+  test("should set access-control-allow-origin response header if only in the development env for e2e testing", async () => {
+    process.env.NODE_ENV = "development_e2e";
+    request.nickname = "test1@test.com";
+
+    await getMessageStream(request, response, next);
+
+    expect(response._headers).toEqual(
+      expect.objectContaining({
+        "access-control-allow-origin": "https://app-e2e"
+      })
     );
   });
 });
