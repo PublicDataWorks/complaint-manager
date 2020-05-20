3b0bd02c7aa1d912e36b5bc306bf47579fe7a017
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-05-20 11:56:01 -0700

    Syd/Sabrina/Veronica [#172768736] update imports in getUsers.test

--- src/server/common/handlers/users/getUsers.test.js
+++ src/server/common/handlers/users/getUsers.test.js
@@ -1,100 +1,94 @@
 import httpMocks from "node-mocks-http";
-import getUsers from "./getUsers";
 import {
-    AUDIT_SUBJECT,
-    MANAGER_TYPE
+  AUDIT_SUBJECT,
+  MANAGER_TYPE
 } from "../../../../sharedUtilities/constants";
 import auditDataAccess from "../../../handlers/audits/auditDataAccess";
-import {getUsers} from "../../../services/auth0UserServices";
-
-import Boom from "boom";
-import {suppressWinstonLogs} from "../../../testHelpers/requestTestHelpers";
+import { getUsers } from "../../../services/auth0UserServices";
+import { suppressWinstonLogs } from "../../../testHelpers/requestTestHelpers";
 
 jest.mock("../../../handlers/audits/auditDataAccess");
 jest.mock("../../../services/auth0UserServices");
 let auth0Users = {
-    "name": "john doe",
-    "email": "john.doe@thoughtworks.com",
+  name: "john doe",
+  email: "john.doe@thoughtworks.com"
 };
 
-
 describe("getUsers tests", () => {
-    let mockGetUserRequest, mockGetUserResponse, next;
+  let mockGetUserRequest, mockGetUserResponse, next;
 
-    beforeEach(() => {
-        mockGetUserRequest = httpMocks.createRequest({
-            method: "GET",
-            headers: {
-                authorization: "Bearer token"
-            },
-            nickname: "nickname"
+  beforeEach(() => {
+    mockGetUserRequest = httpMocks.createRequest({
+      method: "GET",
+      headers: {
+        authorization: "Bearer token"
+      },
+      nickname: "nickname"
+    });
+    mockGetUserResponse = httpMocks.createResponse();
+
+    next = jest.fn();
+    process.env.NODE_ENV = "development";
+    getUsers.mockClear();
+  });
+
+  describe("Successful path", () => {
+    test("Should Call getUsersFromAuth0", async () => {
+      getUsers.mockImplementation(() => auth0Users);
+
+      await getUsers(mockGetUserRequest, mockGetUserResponse, next);
+
+      expect(getUsers).toBeCalledTimes(1);
+      expect(mockGetUserResponse.statusCode).toEqual(200);
+      expect(mockGetUserResponse._getData()).toEqual(auth0Users);
+    });
+  });
+
+  describe("Error Handling", () => {
+    test(
+      "Should throw error if getUsersFromAuth0 fails",
+      suppressWinstonLogs(async () => {
+        getUsers.mockImplementationOnce(() => {
+          throw new Error("I am failing!");
         });
-        mockGetUserResponse = httpMocks.createResponse();
 
-        next = jest.fn();
-        process.env.NODE_ENV = "development";
-        getUsers.mockClear();
+        await getUsers(mockGetUserRequest, mockGetUserResponse, next);
+        expect(next).toHaveBeenCalledWith(new Error("I am failing!"));
+      })
+    );
+  });
+
+  describe("Auditing", () => {
+    beforeAll(() => {
+      getUsers.mockImplementation(() => auth0Users);
     });
 
-    describe("Successful path", () => {
+    test(
+      "Should audit when accessing user data",
+      suppressWinstonLogs(async () => {
+        await getUsers(mockGetUserRequest, mockGetUserResponse, next);
 
-        test("Should Call getUsersFromAuth0", async () => {
-            getUsers.mockImplementation(() => auth0Users)
-
-            await getUsers(mockGetUserRequest, mockGetUserResponse, next);
-
-            expect(getUsers).toBeCalledTimes(1);
-            expect(mockGetUserResponse.statusCode).toEqual(200);
-            expect(mockGetUserResponse._getData()).toEqual(auth0Users)
-        })
-    });
-
-    describe("Error Handling", () => {
-        test(
-            "Should throw error if getUsersFromAuth0 fails",
-            suppressWinstonLogs(async () => {
-
-                getUsers.mockImplementationOnce(() => {
-                    throw new Error("I am failing!")
-                });
-
-                await getUsers(mockGetUserRequest, mockGetUserResponse, next);
-                expect(next).toHaveBeenCalledWith(new Error("I am failing!"));
-            })
+        expect(auditDataAccess).toHaveBeenCalledWith(
+          mockGetUserRequest.nickname,
+          null,
+          MANAGER_TYPE.COMPLAINT,
+          AUDIT_SUBJECT.ALL_USER_DATA,
+          { users: { attributes: ["name", "email"] } },
+          expect.anything()
         );
-    });
+      })
+    );
 
-    describe("Auditing", () => {
-        beforeAll(() => {
-            getUsers.mockImplementation(() => auth0Users)
-        })
+    test(
+      "Should throw error if audit fails",
+      suppressWinstonLogs(async () => {
+        auditDataAccess.mockImplementation(() => {
+          throw new Error("I am failing!");
+        });
 
-        test(
-            "Should audit when accessing user data",
-            suppressWinstonLogs(async () => {
-                await getUsers(mockGetUserRequest, mockGetUserResponse, next);
-
-                expect(auditDataAccess).toHaveBeenCalledWith(
-                    mockGetUserRequest.nickname,
-                    null,
-                    MANAGER_TYPE.COMPLAINT,
-                    AUDIT_SUBJECT.ALL_USER_DATA,
-                    {users: {attributes: ["name", "email"]}},
-                    expect.anything()
-                );
-            })
-        );
-
-        test(
-            "Should throw error if audit fails",
-            suppressWinstonLogs(async () => {
-                auditDataAccess.mockImplementation(() => {
-                    throw new Error("I am failing!");
-                });
-
-                await getUsers(mockGetUserRequest, mockGetUserResponse, next);
-                expect(next).toHaveBeenCalledWith(new Error("I am failing!"));
-            })
-        );
-    });
+        await getUsers(mockGetUserRequest, mockGetUserResponse, next);
+        expect(next).toHaveBeenCalledWith(new Error("I am failing!"));
+      })
+    );
+  });
 });
