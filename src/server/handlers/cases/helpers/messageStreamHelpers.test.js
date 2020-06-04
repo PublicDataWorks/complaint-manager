5cfc8305d4d3b3ec2b6fdfd7b21b1740ea14555f
Karan Krishnani <krkrishn@thoughtworks.com>
2020-06-04 16:18:05 -0400

    Syd/Karan [#000] Add logging around platform errors on messageStream

--- src/server/handlers/cases/helpers/messageStreamHelpers.test.js
+++ src/server/handlers/cases/helpers/messageStreamHelpers.test.js
@@ -1,8 +1,26 @@
 import { sendMessage } from "./messageStreamHelpers";
+import { suppressWinstonLogs } from "../../../testHelpers/requestTestHelpers";
 
 const httpMocks = require("node-mocks-http");
 
 describe("message stream helpers", () => {
+  test("sendMessage throws error on serialization failrues", () => {
+    suppressWinstonLogs(() => {
+      const response = httpMocks.createResponse();
+      const user = {
+        id: Date.now(),
+        email: "test@test.com",
+        response: response
+      };
+      const type = "mockType";
+      const message = { x: 2n };
+
+      expect(() => {
+        sendMessage(type, user, message);
+      }).toThrow("Do not know how to serialize a BigInt");
+    });
+  });
+
   test("sendMessage should write message to response", () => {
     const response = httpMocks.createResponse();
     const user = { id: Date.now(), email: "test@test.com", response: response };
