af389459d51c6a4bd6d263f85a30a1d8afd4dbab
sydneybotz <sydneylaurabotz@gmail.com>
2020-06-18 12:21:52 -0700

    Syd/Veronica [#173311297] Migration to rename referral letters for anon complainants from their last name to 'Anonymous' in db and s3 buckets
    Syd/Veronica [#173311297] Migration to rename referral letters for anon complainants from their last name to 'Anonymous' in db and s3 buckets

---
+++ src/server/migrations/20200617205929-transform-referral-filenames-for-anonymous-complainants.js
@@ -0,0 +1,38 @@
+"use strict";
+import models from "../complaintManager/models";
+import {
+  reverseTransformReferralLetterIfComplainantAnonymous,
+  transformReferralLetterIfComplainantAnonymous
+} from "../migrationJobs/transformReferralLetterIfComplainantAnonymous";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async transaction => {
+      const referralLetters = await models.referral_letter.findAll({});
+
+      try {
+        await transformReferralLetterIfComplainantAnonymous(
+          referralLetters,
+          transaction
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async transaction => {
+      const referralLetters = await models.referral_letter.findAll({});
+
+      try {
+        await reverseTransformReferralLetterIfComplainantAnonymous(
+          referralLetters,
+          transaction
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }
+};
