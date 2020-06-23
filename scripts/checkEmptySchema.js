e31f1439b8a936cf984d7bc25b8986b570638d68
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-06-23 14:21:57 -0500

    Karan/Sabrina [#173315817] Creates script to check for empty schema during setup:db

---
+++ scripts/checkEmptySchema.js
@@ -0,0 +1,39 @@
+const { exec } = require("child-process-promise");
+
+(async function () {
+  const env = process.env.NODE_ENV || "development";
+  console.log("Checking for Schema in DB Env:", env);
+
+  const config = require(__dirname +
+    "/../src/server/config/sequelize_config.js")[env];
+
+  const schemaQuery =
+    "select exists(select * from information_schema.tables where table_schema = 'public')::int;";
+
+  const databaseName = config.database;
+  const databaseHost = config.host;
+  const databaseUser = config.username;
+  const databasePassword = config.password;
+
+  console.log("Schema check running...");
+  const checkSchemaCommand = `PGPASSWORD=${databasePassword} psql -t -h ${databaseHost} -U ${databaseUser} -d ${databaseName} -c "${schemaQuery}"`;
+  exec(checkSchemaCommand)
+    .then(result => {
+      console.log("*************************");
+      const tablesInSchema = parseInt(result.stdout);
+      switch (tablesInSchema) {
+        case 0:
+          console.log("Schema is empty, will load initial schema...");
+          break;
+        case 1:
+          console.log("Schema contains tables, skipping initial schema load.");
+          break;
+      }
+      process.exit(tablesInSchema);
+    })
+    .catch(err => {
+      console.log("*************************");
+      console.log("Schema check failed. Error:", err.message);
+      process.exit(2);
+    });
+})();
