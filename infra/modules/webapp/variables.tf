5b52e3a9ed5b70bb736f618240d97bcb33f5dd94
Karan Krishnani <krkrishn@thoughtworks.com>
2020-07-01 16:31:11 -0400

    Karan [#173227091] Creates webapp module and moves Playground State to module

---
+++ infra/modules/webapp/variables.tf
@@ -0,0 +1,35 @@
+variable "heroku_email" {
+  description = "Email account used for provisioning against Heroku"
+}
+
+variable "heroku_api_key" {
+  description = "API key for Heroku account associated with email"
+}
+
+variable "team_name" {
+  description = "Name of the Heroku team under which the app should be provisioned"
+}
+
+variable "app_name" {
+  description = "Name of the Heroku app to be provisioned"
+}
+
+variable "env_policy" {
+  description = "Policy for buckets across the environment"
+}
+
+variable "env_name" {
+  description = "Name of the environment"
+}
+
+variable "bucket_names" {
+  description = "Names of the buckets in this env"
+}
+
+variable "postgres_plan" {
+  description = "Heroku Postgres Plan Name"
+}
+
+variable "papertrail_plan" {
+  default = "Papertrail Plan Name"
+}
