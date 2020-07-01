0a9ef9dd3d9d8deead867555d6775080c22039f2
Karan Krishnani <krkrishn@thoughtworks.com>
2020-07-01 15:14:08 -0400

    Karan [#173227091] Brings CI into Terraform using independent tf state on S3

--- infra/backend/main.tf
+++ infra/backend/main.tf
@@ -29,33 +29,32 @@ variable "bucket_names" {
 terraform {
   backend "s3" {
     bucket = "noipm-terraform"
-    key = "tfstate-playground"
     region = "us-east-1"
 
     encrypt = true
     shared_credentials_file = "~/.aws/credentials"
-    profile                 = "noipm-terraform"
+    profile = "noipm-terraform"
   }
 }
 
 provider "heroku" {
   version = "~> 2.0"
-  email   = var.heroku_email
+  email = var.heroku_email
   api_key = var.heroku_api_key
 }
 
 provider "aws" {
   version = "~> 2.0"
-  region  = "us-east-1"
+  region = "us-east-1"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "noipm-terraform"
+  profile = "noipm-terraform"
 }
 
 data "aws_secretsmanager_secret_version" "env_secrets" {
-  secret_id     = "${var.env_name}/Env/Config"
+  secret_id = "${var.env_name}/Env/Config"
 }
 resource "heroku_app" "app" {
-  name   = var.app_name
+  name = var.app_name
   region = "us"
   organization {
     name = var.team_name
@@ -67,26 +66,68 @@ resource "heroku_app" "app" {
 }
 
 resource "heroku_addon" "redis_addon" {
-  app  = heroku_app.app.name
+  app = heroku_app.app.name
   plan = "rediscloud:30"
 }
 
+variable "postgres_plan" {
+  description = "Heroku Postgres Plan Name"
+}
 resource "heroku_addon" "postgres_addon" {
-  app  = heroku_app.app.name
-  plan = "heroku-postgresql:hobby-dev"
+  app = heroku_app.app.name
+  plan = "heroku-postgresql:${var.postgres_plan}"
 }
 
+variable "papertrail_plan" {
+  default = "Papertrail Plan Name"
+}
 resource "heroku_addon" "papertrail_addon" {
-  app  = heroku_app.app.name
-  plan = "papertrail:choklad"
+  app = heroku_app.app.name
+  plan = "papertrail:${var.papertrail_plan}"
 }
 
 resource "aws_s3_bucket" "env_bucket" {
   for_each = toset(var.bucket_names)
 
   bucket = each.value
-  acl    = "private"
-  force_destroy = true
+  acl = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "exports_bucket" {
+  bucket = "noipm-exports-${var.env_name}"
+  acl = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    abort_incomplete_multipart_upload_days = 0
+    enabled = true
+    id = "Delete old exports"
+    tags = {}
+
+    expiration {
+      days = 1
+      expired_object_delete_marker = false
+    }
+
+    noncurrent_version_expiration {
+      days = 1
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -141,14 +182,66 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 POLICY
 }
 
+resource "aws_s3_bucket_policy" "exports_bucket_policy" {
+  bucket = aws_s3_bucket.exports_bucket.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DenyIncorrectEncryptionHeader",
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.exports_bucket.id}/*",
+            "Condition": {
+                "StringNotEquals": {
+                    "s3:x-amz-server-side-encryption": "AES256"
+                }
+            }
+        },
+        {
+            "Sid": "DenyUnEncryptedObjectUploads",
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.exports_bucket.id}/*",
+            "Condition": {
+                "Null": {
+                    "s3:x-amz-server-side-encryption": "true"
+                }
+            }
+        },
+        {
+            "Sid": "DenyUnSecureCommunications",
+            "Effect": "Deny",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.exports_bucket.id}/*",
+            "Condition": {
+                "Bool": {
+                    "aws:SecureTransport": "false"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
 resource "aws_iam_policy" "env_policy" {
-  name        = "noipm-${var.env_name}-bucket-access"
+  name = "noipm-${var.env_name}-bucket-access"
   description = "A policy to allow bucket listing and CRUD on bucket contents"
   policy = var.env_policy
 }
 
 resource "aws_iam_policy_attachment" "attach_policy" {
-  name       = "${var.env_name}-policy-group-attachment"
-  groups     = ["developer", "contributor"]
+  name = "${var.env_name}-policy-group-attachment"
+  groups = [
+    "developer",
+    "contributor"]
   policy_arn = aws_iam_policy.env_policy.arn
 }
