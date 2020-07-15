f79a2dff8af1f45f6f87b5675037efd89864a7cc
Karan Krishnani <krkrishn@thoughtworks.com>
2020-07-15 11:13:52 -0400

    Ray/Karan [#173227091] Split connection url into User/Pass/Host/Database

--- infra/modules/webapp/outputs.tf
+++ infra/modules/webapp/outputs.tf
@@ -2,6 +2,6 @@ output "api_gateway_endpoint" {
   value = aws_apigatewayv2_api.api_gateway.api_endpoint
 }
 
-output "database_connection_url" {
-  value = heroku_app.app.all_config_vars["DATABASE_URL"]
+output "database_connection_details" {
+  value = regex("://(?P<username>\\w+):(?P<password>\\w+)@(?P<host>[\\w-.]+):\\d+/(?P<database>\\w+)", heroku_app.app.all_config_vars["DATABASE_URL"])
 }
