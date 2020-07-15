479384832a2ba5cd9719d99b86bc8d065e1aec0a
Karan Krishnani <krkrishn@thoughtworks.com>
2020-07-15 11:13:52 -0400

    Ray/Karan [#173227091] Split connection url into User/Pass/Host/Database

--- infra/playground/outputs.tf
+++ infra/playground/outputs.tf
@@ -2,6 +2,6 @@ output "api_gateway_endpoint" {
   value = module.webapp.api_gateway_endpoint
 }
 
-output "database_connection_url" {
-  value = module.webapp.database_connection_url
+output "database_connection_details" {
+  value = module.webapp.database_connection_details
 }
