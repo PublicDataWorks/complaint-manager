f42ed064ef4b3b880f8dc0ee184c028e4ecf32ac
Karan Krishnani <krkrishn@thoughtworks.com>
2020-07-15 10:59:29 -0400

    Ray/Karan [#173227091] Output Database URL upon provisioning environment

--- infra/modules/webapp/outputs.tf
+++ infra/modules/webapp/outputs.tf
@@ -1,3 +1,7 @@
 output "api_gateway_endpoint" {
   value = aws_apigatewayv2_api.api_gateway.api_endpoint
 }
+
+output "database_connection_url" {
+  value = heroku_app.app.all_config_vars["DATABASE_URL"]
+}
