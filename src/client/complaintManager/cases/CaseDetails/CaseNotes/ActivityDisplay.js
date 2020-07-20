7bd98d22cc444033851d6ec52d843bdd426489d4
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-07-20 11:50:50 -0500

    Veronica/Sabrina [#171340460] Adds bolding to mentioned users in case notes and associated tests

--- src/client/complaintManager/cases/CaseDetails/CaseNotes/ActivityDisplay.js
+++ src/client/complaintManager/cases/CaseDetails/CaseNotes/ActivityDisplay.js
@@ -3,18 +3,80 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 import moment from "moment";
 import ActivityMenu from "./ActivityMenu";
 import TextTruncate from "../../../shared/components/TextTruncate";
+import _ from "lodash";
+import { getMentionedUsers } from "../CaseNoteDialog/userMentionHelperFunctions";
 
 const ActivityDisplay = ({
   caseId,
   activity,
   highlightedCaseNote,
-  shouldTruncate = true
+  allUsers
 }) => {
   const author = activity.author.name
     ? activity.author.name
     : "[" + activity.author.email + "]";
 
   const TIMESTAMP_FORMAT = "MMM D, YYYY h:mm A";
+
+  const splitTextToDict = (mentionedUsers, activityNotes) => {
+    let notesDict = {};
+
+    if (_.isEmpty(mentionedUsers)) {
+      notesDict["t0"] = `${activityNotes}`;
+      return notesDict;
+    } else {
+      for (let i = 0; i < mentionedUsers.length; i++) {
+        let lowerCaseActivityNotes = activityNotes.toLowerCase();
+        let lowerCaseMentionedUser = mentionedUsers[i].label.toLowerCase();
+
+        if (lowerCaseActivityNotes.includes(`@${lowerCaseMentionedUser}`)) {
+          let startIndex = lowerCaseActivityNotes.indexOf(
+            `@${lowerCaseMentionedUser}`
+          );
+
+          const beforeMention = activityNotes.substring(0, startIndex);
+          notesDict[`t${i}`] = `${beforeMention}`;
+
+          const actualMention = activityNotes.substring(
+            startIndex,
+            startIndex + `@${lowerCaseMentionedUser}`.length
+          );
+          notesDict[`m${i}`] = `${actualMention}`;
+
+          activityNotes = activityNotes.substring(
+            startIndex + `@${mentionedUsers[i].label}`.length
+          );
+        }
+      }
+      notesDict["tEnd"] = activityNotes;
+      return notesDict;
+    }
+  };
+
+  const notesWithBoldMentions = notesDict => {
+    return (
+      <Typography
+        variant="body2"
+        style={{ whiteSpace: "pre-wrap" }}
+        data-testid={"notesText"}
+      >
+        {Object.keys(notesDict).map(key => {
+          return key.includes("m") ? (
+            <b key={key}>{notesDict[key]}</b>
+          ) : (
+            <span key={key}>{notesDict[key]}</span>
+          );
+        })}
+      </Typography>
+    );
+  };
+
+  const getActivityNotes = notes => {
+    const mentionedUsers = getMentionedUsers(allUsers, notes);
+    const notesDict = splitTextToDict(mentionedUsers, notes);
+
+    return notesWithBoldMentions(notesDict);
+  };
 
   return (
     <Card
@@ -61,16 +123,11 @@ const ActivityDisplay = ({
               }}
             >
               {activity.notes ? (
-                shouldTruncate ? (
-                  <TextTruncate
-                    testLabel="notesText"
-                    message={activity.notes ? activity.notes : "N/A"}
-                  />
-                ) : (
-                  <Typography data-testid="notesText">
-                    {activity.notes}
-                  </Typography>
-                )
+                <TextTruncate
+                  testLabel="notesText"
+                  message={activity.notes ? activity.notes : "N/A"}
+                  getActivityNotes={getActivityNotes}
+                />
               ) : (
                 ""
               )}
