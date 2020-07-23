fc2ac6c4c36caab5753a879525fbd643af24a5da
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-07-23 08:36:09 -0700

    Syd/Veronica [#172229631] add testing to getData query handler for complainant type over the past 12 months query

--- src/server/handlers/data/getData.test.js
+++ src/server/handlers/data/getData.test.js
@@ -20,6 +20,33 @@ const MOCK_COMPLAINANT_TYPE_DATA_VALUES = [
   { complainantType: "Civilian Within NOPD (CN)" }
 ];
 
+const MOCK_COMPLAINANT_TYPE_PAST_12_MONTHS_DATA_VALUES = {
+  CC: [
+    {
+      date: "Jun 19",
+      count: 1
+    }
+  ],
+  PO: [
+    {
+      date: "Jun 19",
+      count: 8
+    }
+  ],
+  CN: [
+    {
+      date: "Jun 19",
+      count: 3
+    }
+  ],
+  AC: [
+    {
+      date: "Jun 19",
+      count: 0
+    }
+  ]
+};
+
 jest.mock("../../handlers/data/queries/countComplaintsByIntakeSource", () => ({
   executeQuery: jest.fn(() => {
     return MOCK_INTAKE_SOURCE_DATA_VALUES;
@@ -37,6 +64,15 @@ jest.mock(
   () => ({
     executeQuery: jest.fn(() => {
       return MOCK_COMPLAINANT_TYPE_DATA_VALUES;
+    })
+  })
+);
+
+jest.mock(
+  "../../handlers/data/queries/countComplaintsByComplainantTypePast12Months",
+  () => ({
+    executeQuery: jest.fn(() => {
+      return MOCK_COMPLAINANT_TYPE_PAST_12_MONTHS_DATA_VALUES;
     })
   })
 );
@@ -101,6 +137,25 @@ describe("getData", () => {
     await getData(request, response, next);
 
     expect(response._getData()).toEqual(MOCK_COMPLAINANT_TYPE_DATA_VALUES);
+  });
+
+  test("should call getData when countComplaintsByComplainantTypePast12Months query called", async () => {
+    const request = httpMocks.createRequest({
+      method: "GET",
+      headers: {
+        authorization: "Bearer SOME_MOCK_TOKEN"
+      },
+      query: {
+        queryType: "countComplaintsByComplainantTypePast12Months"
+      },
+      nickname: "tuser"
+    });
+
+    await getData(request, response, next);
+
+    expect(response._getData()).toEqual(
+      MOCK_COMPLAINANT_TYPE_PAST_12_MONTHS_DATA_VALUES
+    );
   });
 
   test("throws an error when query param is not supported", async () => {
