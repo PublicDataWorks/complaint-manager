2a47b796ebdf9a26ed19f77a07abd4a87e21f43d
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-07-30 13:51:27 -0700

    Ray/Veronica [#172057941] Resolves breaking changes for higher environments

--- src/server/createConfiguredS3Instance.js
+++ src/server/createConfiguredS3Instance.js
@@ -3,7 +3,9 @@ const path = require("path");
 const config = require("./config/config");
 
 const createConfiguredS3Instance = () => {
-  let credentials;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  let credentials = { accessKeyId, secretAccessKey };
 
   if (["development", "test"].includes(process.env.NODE_ENV)) {
     const localConfig = require(path.join(__dirname, "./awsConfig.json"));
