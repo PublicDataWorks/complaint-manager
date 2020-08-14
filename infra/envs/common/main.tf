5b342f22548802e4f67f9381b81e9c98878b021b
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-08-14 11:00:16 -0500

    Sabrina/Sean [#174072015] Provisions private images bucket in S3 and adds common bucket inline policy

--- infra/envs/common/main.tf
+++ infra/envs/common/main.tf
@@ -5,6 +5,11 @@ provider "aws" {
   profile = "noipm-terraform"
 }
 
+variable "s3_bucket_names" {
+  type    = list
+  default = ["noipm-seed-files","noipm-private-images"]
+}
+
 resource "aws_s3_bucket" "seed_files" {
   bucket = "${var.organization_name}-seed-files"
   acl = "private"
@@ -20,6 +25,75 @@ resource "aws_s3_bucket" "seed_files" {
       }
     }
   }
+}
+
+resource "aws_s3_bucket" "private_images" {
+  bucket = "${var.organization_name}-private-images"
+  acl = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  for_each = toset(var.s3_bucket_names)
+
+  bucket = each.key
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DenyIncorrectEncryptionHeader",
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${each.value}/*",
+            "Condition": {
+                "StringNotEquals": {
+                    "s3:x-amz-server-side-encryption": "AES256"
+                }
+            }
+        },
+        {
+            "Sid": "DenyUnEncryptedObjectUploads",
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${each.value}/*",
+            "Condition": {
+                "Null": {
+                    "s3:x-amz-server-side-encryption": "true"
+                }
+            }
+        },
+        {
+            "Sid": "DenyUnSecureCommunications",
+            "Effect": "Deny",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::${each.value}/*",
+            "Condition": {
+                "Bool": {
+                    "aws:SecureTransport": "false"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }
 
 resource "aws_iam_policy" "env_policy" {
@@ -53,6 +127,49 @@ resource "aws_iam_policy" "env_policy" {
     ]
 }
 POLICY
+}
+
+resource "aws_iam_policy" "instance_policy" {
+  name = "${var.organization_name}-instance-bucket-access"
+  description = "A policy to allow bucket listing and CRUD on bucket contents"
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowListingBucket",
+            "Effect": "Allow",
+            "Action": [
+              "s3:ListBucket",
+              "s3:GetBucketLocation"
+             ],
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.private_images.id}"
+       },
+       {
+           "Sid": "AllowBucketContentCRUD",
+           "Effect": "Allow",
+           "Action": [
+               "s3:PutObject",
+               "s3:GetObjectAcl",
+               "s3:GetObject",
+               "s3:DeleteObject",
+               "s3:PutObjectAcl"
+           ],
+           "Resource": "arn:aws:s3:::${aws_s3_bucket.private_images.id}/*"
+       }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "attach_instance_policy" {
+  name = "instance-policy-group-attachment"
+  groups = [
+    "developer",
+    "contributor",
+    "production"
+  ]
+  policy_arn = aws_iam_policy.instance_policy.arn
 }
 
 resource "aws_iam_policy_attachment" "attach_policy" {
