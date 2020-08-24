782bc3ca94f2d8647207d17408949ec95ebc90a8
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-08-24 08:31:17 -0500

    Sabrina [#170384419] Conditionally restarts app container after cm e2e script run

--- scripts/run-e2e-cm.sh
+++ scripts/run-e2e-cm.sh
@@ -2,6 +2,7 @@
 
 echo "Stopping app container..."
 docker-compose stop app
+CONTAINER_ID=$(docker-compose ps -q app)
 
 # In case you want to test a single test case
 # nightwatch --env local --test tests/complaintManager/complaintManagerUserJourney.js --testcase "should navigate to all exports page and export all cases"
@@ -11,3 +12,13 @@ docker-compose run e2e nightwatch --env local tests/complaintManager
 
 echo "Stopping app-e2e container..."
 docker-compose stop app-e2e
+
+if [[ -n $CONTAINER_ID ]]
+then
+  RESTART_APP_CHECK=$(docker inspect --format "{{.State.ExitCode}}" $CONTAINER_ID)
+  if [[ 1 -eq $RESTART_APP_CHECK ]];
+  then
+    echo "Restarting app container..."
+    docker-compose start app
+  fi
+fi
