9cb29b04a1e757231700ec9e48f268ed613fab20
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-08-25 10:56:43 -0500

    Team [#000] Refactors add and display case tag e2e test

--- e2e/pageObjectModels/SnackbarPOM.js
+++ e2e/pageObjectModels/SnackbarPOM.js
@@ -1,19 +1,18 @@
 const e2e = require("./e2eUtilities.js");
 
 const snackbarCommands = {
-  presentWithRegex: function(regexexpression) {
+  presentWithRegex: function (regexexpression) {
     this.waitForElementVisible("@snackbarText", e2e.rerenderWait);
     const text = this.getText("@snackbarText");
     const regex = new RegExp(regexexpression, "i");
     return this.assert.equal(true, regex.test(regexexpression, text));
   },
-  presentWithMessage: function(message) {
-    return this.waitForElementVisible(
-      "@snackbarText",
-      e2e.rerenderWait
-    ).assert.containsText("@snackbarText", message);
+  presentWithMessage: function (message) {
+    return this.waitForElementVisible("@snackbarText", e2e.rerenderWait)
+      .moveToElement("@snackbarText", "", 5, 5)
+      .assert.containsText("@snackbarText", message);
   },
-  close: function() {
+  close: function () {
     return this.click("@closeButton").waitForElementNotPresent(
       "@snackbarText",
       e2e.rerenderWait
