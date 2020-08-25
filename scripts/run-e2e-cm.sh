087f1a568fe8578571bb6fef2d15de6b4fb7ea9a
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2020-08-25 10:55:12 -0500

    Sabrina [#170384419] Refactors cm e2e script to restart app container without using exit code conditional

--- scripts/run-e2e-cm.sh
+++ scripts/run-e2e-cm.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+CONTAINER_ID=$(docker-compose ps -q app)
+if [[ -n $CONTAINER_ID ]];
+then
+  RESTART_APP_CHECK=$(docker inspect --format "{{.State.Running}}" $CONTAINER_ID)
+fi
+
 echo "Stopping app container..."
 docker-compose stop app
-CONTAINER_ID=$(docker-compose ps -q app)
 
 # In case you want to test a single test case
 # nightwatch --env local --test tests/complaintManager/complaintManagerUserJourney.js --testcase "should navigate to all exports page and export all cases"
@@ -13,12 +18,8 @@ docker-compose run e2e nightwatch --env local tests/complaintManager
 echo "Stopping app-e2e container..."
 docker-compose stop app-e2e
 
-if [[ -n $CONTAINER_ID ]]
+if [[ $RESTART_APP_CHECK = true ]];
 then
-  RESTART_APP_CHECK=$(docker inspect --format "{{.State.ExitCode}}" $CONTAINER_ID)
-  if [[ 1 -eq $RESTART_APP_CHECK ]];
-  then
-    echo "Restarting app container..."
-    docker-compose start app
-  fi
+  echo "Restarting app container..."
+  docker-compose start app
 fi
