c88c677ecae2393cdc942c8aae5ffdc792299a2e
Ray Perry <ray.perry@thoughtworks.com>
2020-09-14 15:33:42 -0500

    Veronica/Ray [#174713262] Adding unit tests for the custom logger.

--- src/sharedUtilities/logger.js
+++ src/sharedUtilities/logger.js
@@ -2,21 +2,48 @@ import winston from "winston";
 import Transport from "winston-transport";
 import axios from 'axios';
 
+class MessageBatch {
+  constructor(messageLimit = 1) {
+    this.messages = [];
+    this.messageLimit = messageLimit;
+  }
+
+  queue(message) {
+    this.messages.push(message);
+    if (this.messages.length < this.messageLimit) return null;
+    return this.flush();
+  }
+
+  flush() {
+    const oldMessages = [...this.messages];
+    this.messages = [];
+    return oldMessages;
+  }
+}
+
 class BackendTransport extends Transport {
   constructor(transportOptions) {
     super(transportOptions);
 
     this.endpoint = transportOptions.endpoint || null;
+    this.messageBatch = new MessageBatch();
   }
 
-  log(message, callback) {
-    axios.post(this.endpoint, message)
-      .then(response => {
+  async log(message, callback) {
+    const payload = { messages: this.messageBatch.queue(message) } 
+
+    if (!payload.messages) {
+      this.emit(message);
+    } else {
+      if (!this.endpoint) return console.warn("Endpoint is null. Aborting.");
+
+      try {
+        await axios.post(this.endpoint, payload);
         this.emit(message);
-      })
-      .catch(error => {
+      } catch (error) {
         this.emit(error);
-      });
+      }
+    }
     
     if (callback) callback();
   }
