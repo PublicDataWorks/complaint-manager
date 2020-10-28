a9d15d73fe0f13276dc1674f295ee5b4dbe844c2
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-10-28 09:12:52 -0700

    Sabrina/Veronica [#175064772] update 'last updated' date to be dynamic and update snapshot with mock of date

--- src/client/publicDataDashboard/PublicDataDashboard.test.js
+++ src/client/publicDataDashboard/PublicDataDashboard.test.js
@@ -10,6 +10,7 @@ jest.mock("./DashboardDataSection", () => () => "DashboardDataSection");
 
 describe("Public Data Dashboard", () => {
   test("should render public data dashboard with correct styling", () => {
+    Date.now = jest.fn(() => 1403900852194);
     const wrapper = mount(<PublicDataDashboard />);
 
     expect(wrapper).toMatchSnapshot();
