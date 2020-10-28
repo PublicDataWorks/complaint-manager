adf303f4605fd5734ba0d7de3ce18f5753832a87
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-10-28 09:38:19 -0700

    Sabrina/Veronica [#174830754] change instances of Complaint Manager to Police Data Manager

--- scripts/run-e2e-cm.sh
+++ scripts/run-e2e-cm.sh
@@ -12,7 +12,7 @@ docker-compose stop app
 # In case you want to test a single test case
 # nightwatch --env local --test tests/complaintManager/complaintManagerUserJourney.js --testcase "should navigate to all exports page and export all cases"
 
-echo "Running E2E test suite for Complaint Manager..."
+echo "Running E2E test suite for Police Data Manager..."
 docker-compose run e2e nightwatch --env local tests/complaintManager
 
 echo "Stopping app-e2e container..."
