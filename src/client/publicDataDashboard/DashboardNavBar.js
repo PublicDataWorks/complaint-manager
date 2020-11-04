4e110cf9c359dc4a85c78e9b122872210f31eca9
Karan Krishnani <krkrishn@thoughtworks.com>
2020-11-04 16:50:22 -0500

    Veronica/Karan [#175539782] Makes the logo responsive

--- src/client/publicDataDashboard/DashboardNavBar.js
+++ src/client/publicDataDashboard/DashboardNavBar.js
@@ -1,48 +1,55 @@
 import { Container, Grid, Typography, Link } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import styles from "./dashboardStyling/styles";
 import React, { Component } from "react";
+import logoSmall from "./assets/logo_sm.svg";
 
-class DashboardNavBar extends Component {
-  render() {
-    return (
-      <Grid container>
-        <Grid item xs={8} style={{ marginBottom: "22px" }}>
-          <img src="/favicon.ico" style={{ width: "132px", height: "120px" }} />
-        </Grid>
-        <Grid item xs={4} style={{ padding: 0 }}>
-          <Container
+const DashboardNavBar = () => {
+  const isMobile = useMediaQuery("(max-width:768px)");
+  return (
+    <Grid container>
+      <Grid item xs={8} style={{ marginBottom: "22px" }}>
+        <img
+          src={isMobile ? logoSmall : "/favicon.ico"}
+          style={{
+            width: isMobile ? "54px" : "132px",
+            height: isMobile ? "66px" : "120px"
+          }}
+        />
+      </Grid>
+      <Grid item xs={4} style={{ padding: 0 }}>
+        <Container
+          style={{
+            display: "flex",
+            padding: "48px 0px",
+            alignItems: "center",
+            justifyContent: "flex-end"
+          }}
+        >
+          <Typography style={styles.navBarLink}>
+            <Link
+              href="/data/about"
+              underline="none"
+              style={{ color: styles.navBarLink.color }}
+              data-testid="aboutLink"
+            >
+              About
+            </Link>
+          </Typography>
+          <Typography
             style={{
-              display: "flex",
-              padding: "48px 0px",
-              alignItems: "center",
-              justifyContent: "flex-end"
+              paddingLeft: "32px",
+              color: styles.navBarLink.color,
+              fontSize: styles.navBarLink.fontSize,
+              fontWeight: styles.navBarLink.fontWeight
             }}
           >
-            <Typography style={styles.navBarLink}>
-              <Link
-                href="/data/about"
-                underline="none"
-                style={{ color: styles.navBarLink.color }}
-                data-testid="aboutLink"
-              >
-                About
-              </Link>
-            </Typography>
-            <Typography
-              style={{
-                paddingLeft: "32px",
-                color: styles.navBarLink.color,
-                fontSize: styles.navBarLink.fontSize,
-                fontWeight: styles.navBarLink.fontWeight
-              }}
-            >
-              Glossary
-            </Typography>
-          </Container>
-        </Grid>
+            Glossary
+          </Typography>
+        </Container>
       </Grid>
-    );
-  }
-}
+    </Grid>
+  );
+};
 
 export default DashboardNavBar;
