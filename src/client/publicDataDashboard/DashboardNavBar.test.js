f6e1aafcadd4aa104417d7a5db9d3fdadd4eea57
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-11-05 13:58:16 -0800

    Karan/Veronica [#175539782] add custom breakpoints to dashboard theme for responsive grids

--- src/client/publicDataDashboard/DashboardNavBar.test.js
+++ src/client/publicDataDashboard/DashboardNavBar.test.js
@@ -1,16 +1,26 @@
 import { mount } from "enzyme";
 import DashboardNavBar from "./DashboardNavBar";
 import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import dashboardStylingDesktop from "./dashboardStyling/dashboardStylingDesktop";
 
 describe("Dashboard NavBar", () => {
   test("should render the navbar in the public data dashboard with correct styling", () => {
-    const wrapper = mount(<DashboardNavBar />);
+    const wrapper = mount(
+      <MuiThemeProvider theme={dashboardStylingDesktop}>
+        <DashboardNavBar />
+      </MuiThemeProvider>
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
 
   test("should navigate to About page when About button is clicked on", async () => {
-    const wrapper = mount(<DashboardNavBar />);
+    const wrapper = mount(
+      <MuiThemeProvider theme={dashboardStylingDesktop}>
+        <DashboardNavBar />
+      </MuiThemeProvider>
+    );
     const aboutLink = wrapper.find('[data-testid="aboutLink"]').first();
 
     aboutLink.simulate("click");
