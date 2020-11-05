085cb7ece3dd2d611cb5f7f1a46734406287b308
Veronica Blackwell <veronica.blackwell@thoughtworks.com>
2020-11-05 13:28:47 -0800

    Karan/Veronica [#175539782] move custom view port style to mui themes

---
+++ src/client/publicDataDashboard/dashboardStyling/dashboardStylingMobile.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const muiTheme = createMuiTheme({
+  dashboard: {
+    padding: "20px",
+    about: {
+      padding: "20px"
+    },
+    navBar: {
+      padding: "24px 0px",
+      logoWidth: "54px",
+      logoHeight: "66px"
+    }
+  }
+});
+
+export default muiTheme;
