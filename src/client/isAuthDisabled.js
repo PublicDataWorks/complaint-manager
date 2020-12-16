89a84e2bba1721b9dd8fa68e29f3a794fd2e5028
Karan Krishnani <krkrishn@thoughtworks.com>
2020-12-16 16:22:22 -0500

    Sabrina/Karan [#174478158] Links disabling of Auth to Cloud Services variable

--- src/client/isAuthDisabled.js
+++ src/client/isAuthDisabled.js
@@ -1,7 +1,6 @@
 export const isAuthDisabled = () => {
   return (
-    process.env.REACT_APP_AUTH_DISABLED === true ||
-      process.env.REACT_APP_AUTH_DISABLED === "true" ||
-      localStorage.getItem('REACT_APP_AUTH_DISABLED')
+    process.env.REACT_APP_CLOUD_SERVICES_DISABLED === true ||
+    process.env.REACT_APP_CLOUD_SERVICES_DISABLED === "true"
   );
 };
