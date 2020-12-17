b61faba623a5b2a336ad61334bda362fb00fbb80
Karan Krishnani <krkrishn@thoughtworks.com>
2020-12-17 16:11:48 -0500

    Sabrina/Karan [#174478158] Mirgrates secretsManager to use credentials from Environment

--- src/server/createConfiguredSecretsManagerInstance.js
+++ src/server/createConfiguredSecretsManagerInstance.js
@@ -1,12 +1,26 @@
+const set = require("lodash/set");
 const AWS = require("aws-sdk");
 const path = require("path");
 
 const createConfiguredSecretsManagerInstance = () => {
-  const secretsManager = new AWS.SecretsManager({
-    region: "us-east-1"
-  });
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  const isLowerEnv = ["development", "test"].includes(process.env.NODE_ENV);
+  const areCloudServicesDisabled =
+    process.env.CLOUD_SERVICES_DISABLED == "true";
 
-  secretsManager.config.loadFromPath(path.join(__dirname, "./awsConfig.json"));
+  let credentials = { accessKeyId, secretAccessKey };
+  if (isLowerEnv && areCloudServicesDisabled) {
+    console.log("Overriding AWS config for Localstack");
+    credentials = { accessKeyId: "test", secretAccessKey: "test" };
+    const localConfig = {
+      endpoint: "host.docker.internal:4566"
+    };
+    set(AWS, ["config", "secretsmanager"], localConfig);
+  }
+
+  AWS.config.update({ region: "us-east-1" });
+  const secretsManager = new AWS.SecretsManager(credentials);
 
   return secretsManager;
 };
