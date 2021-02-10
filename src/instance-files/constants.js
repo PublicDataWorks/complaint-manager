a456db77388d86ce41b3fc33ccc29769227b61fe
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2021-02-10 16:12:12 -0600

    Sabrina/Lex [#176856096] Moves generateSubjectLine helper function to instance-files

--- src/instance-files/constants.js
+++ src/instance-files/constants.js
@@ -3,3 +3,10 @@ export const INSTANCE_CIVILIAN_WITHIN_PD_INITIATED =
   "Civilian Within NOPD Initiated";
 export const INSTANCE_EMPLOYEE_TYPE_CIVILIAN_WITHIN_PD = "Civilian Within NOPD";
 export const INSTANCE_PERSON_TYPE_CIVILIAN_WITHIN_PD = "Civilian (NOPD)";
+
+export const generateSubjectLine = (caseReference, pibCaseNumber) => {
+  if (pibCaseNumber) {
+    return `Supplemental Referral; OIPM Complaint ${caseReference}; PIB Case ${pibCaseNumber}`;
+  }
+  return `Complaint Referral; OIPM Complaint ${caseReference}`;
+};
