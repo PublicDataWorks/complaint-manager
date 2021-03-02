820db0474afee5d105666037cb3fe5910bb70499
Karan Krishnani <krkrishn@thoughtworks.com>
2021-03-02 12:25:26 -0500

    Lex/Karan [#176647233] Adds auth and better error handling to scripts

--- scripts/search/index-config.js
+++ scripts/search/index-config.js
@@ -1,37 +1,42 @@
-'use strict';
+"use strict";
 
-const INDEX_NAME = 'cases';
+const INDEX_NAME = "cases";
 
-const DEVELOPMENT = 'development';
-const TEST = 'test';
-const CI = 'ci';
-const STAGING = 'staging';
-const PRODUCTION = 'production';
+const DEVELOPMENT = "development";
+const TEST = "test";
+const CI = "ci";
+const STAGING = "staging";
+const PRODUCTION = "production";
 
 const config = {
   [DEVELOPMENT]: {
-    host: 'http://elasticsearch',
-    port: '9200',
+    protocol: "http://",
+    host: "elasticsearch",
+    port: "9200",
     indexName: `${DEVELOPMENT}_${INDEX_NAME}`
   },
   [TEST]: {
-    host: 'http://localhost',
-    port: '9200',
+    protocol: "http://",
+    host: "localhost",
+    port: "9200",
     indexName: `${TEST}_${INDEX_NAME}`
   },
   [CI]: {
-    host: 'http://localhost',
-    port: '9200',
+    protocol: "https://",
+    host: "405d72828d1c44fca5344807ef8256ba.us-east-1.aws.found.io",
+    port: "9243",
     indexName: `${CI}_${INDEX_NAME}`
   },
   [STAGING]: {
-    host: 'http://localhost',
-    port: '9200',
+    protocol: "https://",
+    host: "405d72828d1c44fca5344807ef8256ba.us-east-1.aws.found.io",
+    port: "9243",
     indexName: `${STAGING}_${INDEX_NAME}`
   },
   [PRODUCTION]: {
-    host: 'http://localhost',
-    port: '9200',
+    protocol: "https://",
+    host: "localhost",
+    port: "9200",
     indexName: `${PRODUCTION}_${INDEX_NAME}`
   }
 };
