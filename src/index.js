dc0c6a1c78c6539da7fcef525d29db0148bd7e96
Ray Perry <ray.perry@thoughtworks.com>
2021-03-03 14:50:41 -0600

    Ray [#175102366] Adding client-side error reporting.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import RootContainer from "./client/RootContainer";
+import logger from "./client/logger";
+
+window.addEventListener("error", ({ message }) => logger.error(message));
 
 render(<RootContainer />, document.getElementById("root"));
+
+setTimeout(() => {
+  throw new Error("OH GOD EVERYTHING IS ON FIRE");
+}, 3000);
