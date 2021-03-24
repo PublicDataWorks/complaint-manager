c65c1add882af6b703608aca7e4e98291e27c11a
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2021-03-24 15:10:48 -0500

    Lex/Sabrina [#175781316] updates search scripts to index civilians and case officers and query more args

--- scripts/search/create-search-index.js
+++ scripts/search/create-search-index.js
@@ -45,6 +45,8 @@
           mappings: {
             properties: {
               tag_name: { type: "text" },
+              first_name: { type: "text" },
+              last_name: { type: "text" },
               case_id: { type: "integer" }
             }
           }
@@ -57,34 +59,70 @@
   console.log(`Index ${index} created.`);
 
   const models = require("../../src/server/policeDataManager/models/index");
-  const tagsResult = await models.case_tag.findAll({
-    include: [
-      {
-        model: models.tag,
-        as: "tag"
-      }
-    ]
-  });
+  let tagsResults, accusedOfficersResults, civiliansResults;
 
-  if (!tagsResult) {
-    console.error("Something went wrong while fetching tags.");
-    return 1;
-  }
+  await models.sequelize
+    .transaction(async transaction => {
+      tagsResults = await models.case_tag.findAll({
+        include: [
+          {
+            model: models.tag,
+            as: "tag"
+          }
+        ]
+      });
+      accusedOfficersResults = await models.case_officer.findAll();
+      civiliansResults = await models.civilian.findAll();
+    })
+    .catch(handleError);
 
-  if (!tagsResult.length) {
-    console.log("No tags were found. Ending script.");
+  if (
+    !tagsResults.length &&
+    !accusedOfficersResults.length &&
+    !civiliansResults.length
+  ) {
+    console.log("No results were found. Ending script.");
     return 0;
   }
 
-  const bulkOperations = tagsResult.flatMap(tagResult => {
-    const operation = { index: { _index: index } };
+  const operation = { index: { _index: index } };
 
-    const { caseId: case_id } = tagResult.dataValues;
-    const { name: tag_name } = tagResult.tag.dataValues;
+  const caseOfficerBulkOperations = accusedOfficersResults.flatMap(
+    accusedOfficersResult => {
+      const {
+        caseId: case_id,
+        firstName: first_name,
+        lastName: last_name
+      } = accusedOfficersResult.dataValues;
+      const document = { case_id, first_name, last_name };
+
+      return [operation, document];
+    }
+  );
+
+  const civilianBulkOperations = civiliansResults.flatMap(civiliansResult => {
+    const {
+      caseId: case_id,
+      firstName: first_name,
+      lastName: last_name
+    } = civiliansResult.dataValues;
+    const document = { case_id, first_name, last_name };
+
+    return [operation, document];
+  });
+
+  const tagBulkOperations = tagsResults.flatMap(tagsResult => {
+    const { caseId: case_id } = tagsResult.dataValues;
+    const { name: tag_name } = tagsResult.tag.dataValues;
     const document = { case_id, tag_name };
 
     return [operation, document];
   });
+
+  const bulkOperations = tagBulkOperations.concat(
+    caseOfficerBulkOperations,
+    civilianBulkOperations
+  );
 
   await elasticClient
     .bulk({ refresh: true, body: bulkOperations })
