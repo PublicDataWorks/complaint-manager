48ae48e544d36ac328842d45bdcd8dd46152cbcb
Karan Krishnani <krkrishn@thoughtworks.com>
2021-04-08 17:00:34 -0400

    Sabrina/Karan [#177211961] Removes instance-files from PDM repo

--- src/instance-files/helpers.js
+++
@@ -1,6 +0,0 @@
-export const generateSubjectLine = (caseReference, pibCaseNumber) => {
-  if (pibCaseNumber) {
-    return `Supplemental Referral; OIPM Complaint ${caseReference}; PIB Case ${pibCaseNumber}`;
-  }
-  return `Complaint Referral; OIPM Complaint ${caseReference}`;
-};
