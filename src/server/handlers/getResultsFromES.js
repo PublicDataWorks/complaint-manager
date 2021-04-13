d763516dac088d19e6d4880009ac10a2f43171bb
Ray Perry <ray.perry@thoughtworks.com>
2021-04-13 10:34:26 -0500

    Sean/Ray [#175781316] More modifications for the Elasticsearch...
    client.

--- src/server/handlers/getResultsFromES.js
+++ src/server/handlers/getResultsFromES.js
@@ -18,7 +18,7 @@ export const getResultsFromES = async (queryString, currentPage = 1) => {
     node: `${protocol}${host}${port ? ':' + port : ''}`,
     auth: { username, password },
     ssl: {
-      rejectUnauthorized: false
+      ca: "../../server.crt"
     }
   });
 
