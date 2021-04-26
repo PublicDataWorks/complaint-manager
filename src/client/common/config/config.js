8416b8dfe7b174ea489a4a27896aecb51a0857f9
sabrinasanchez6 <sabrinasanchez@thoughtworks.com>
2021-04-26 15:27:42 -0500

    Sabrina/Lex/Isabel [#177211961] Provisioning playground

--- src/client/common/config/config.js
+++ src/client/common/config/config.js
@@ -63,7 +63,7 @@ export default {
       scope: "openid profile",
       nicknameKey: "https://noipm-ci.herokuapp.com/nickname"
     },
-    hostname: "https://ssganz3zfl.execute-api.us-east-1.amazonaws.com",
+    hostname: "https://rmwftjcbxh.execute-api.us-east-1.amazonaws.com",
     backendUrl: "https://noipm-playground.herokuapp.com"
   },
   ci: {
