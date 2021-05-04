38efa6e0a7243b5247668d9c147079eb992fd18d
Isabel Olson <isabel.olson@thoughtworks.com>
2021-05-04 10:41:57 -0500

    mob-session [#177817110] extracted sorting query generation into helper

--- src/server/handlers/cases/getCases/getCases.js
+++ src/server/handlers/cases/getCases/getCases.js
@@ -1,12 +1,12 @@
 import models from "../../../policeDataManager/models";
 import sequelize from "sequelize";
+import getSortingOrderForQuery from "../helpers/getSortingOrderForQuery";
 import {
   ASCENDING,
   DEFAULT_PAGINATION_LIMIT,
   DESCENDING,
   SORT_CASES_BY
 } from "../../../../sharedUtilities/constants";
-import { caseInsensitiveSort } from "../../sequelizeHelpers";
 
 const Op = sequelize.Op;
 
@@ -73,96 +73,6 @@ const getCases = async (
   };
 
   return await models.sortable_cases_view.findAndCountAll(queryOptions);
-};
-
-const getSortingOrderForQuery = (sortBy, sortDirection) => {
-  const model = models.sortable_cases_view;
-  switch (sortBy) {
-    case SORT_CASES_BY.PRIMARY_COMPLAINANT:
-      if (sortDirection === ASCENDING) {
-        return [
-          ["complainantPersonType", DESCENDING],
-          [
-            caseInsensitiveSort("complainantLastName", model),
-            `${ASCENDING} NULLS FIRST`
-          ],
-          [
-            caseInsensitiveSort("complainantFirstName", model),
-            `${ASCENDING} NULLS FIRST`
-          ],
-          [
-            caseInsensitiveSort("complainantMiddleName", model),
-            `${ASCENDING} NULLS FIRST`
-          ]
-        ];
-      } else {
-        return [
-          ["complainantPersonType", ASCENDING],
-          [
-            caseInsensitiveSort("complainantLastName", model),
-            `${DESCENDING} NULLS LAST`
-          ],
-          [
-            caseInsensitiveSort("complainantFirstName", model),
-            `${DESCENDING} NULLS LAST`
-          ],
-          [
-            caseInsensitiveSort("complainantMiddleName", model),
-            `${DESCENDING} NULLS LAST`
-          ]
-        ];
-      }
-    case SORT_CASES_BY.PRIMARY_ACCUSED_OFFICER:
-      if (sortDirection === ASCENDING) {
-        return [
-          ["accusedPersonType", DESCENDING],
-          [
-            caseInsensitiveSort("accusedLastName", model),
-            `${ASCENDING} NULLS FIRST`
-          ],
-          [
-            caseInsensitiveSort("accusedFirstName", model),
-            `${ASCENDING} NULLS FIRST`
-          ],
-          [
-            caseInsensitiveSort("accusedMiddleName", model),
-            `${ASCENDING} NULLS FIRST`
-          ]
-        ];
-      } else {
-        return [
-          ["accusedPersonType", ASCENDING],
-          [
-            caseInsensitiveSort("accusedLastName", model),
-            `${DESCENDING} NULLS LAST`
-          ],
-          [
-            caseInsensitiveSort("accusedFirstName", model),
-            `${DESCENDING} NULLS LAST`
-          ],
-          [
-            caseInsensitiveSort("accusedMiddleName", model),
-            `${DESCENDING} NULLS LAST`
-          ]
-        ];
-      }
-
-    case SORT_CASES_BY.FIRST_CONTACT_DATE:
-    case SORT_CASES_BY.STATUS:
-    case SORT_CASES_BY.TAGS:
-    case SORT_CASES_BY.ASSIGNED_TO:
-      return [[sortBy, sortDirection]];
-    case SORT_CASES_BY.CASE_REFERENCE:
-      return [
-        ["year", sortDirection],
-        ["caseNumber", sortDirection]
-      ];
-    default:
-      return [
-        ["year", DESCENDING],
-        ["caseNumber", DESCENDING]
-      ];
-  }
 };
 
 export default getCases;
