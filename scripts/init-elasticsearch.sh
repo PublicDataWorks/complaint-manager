ff626dbaca93dc4254d01b73c43e2e9e0458c778
Karan Krishnani <krkrishn@thoughtworks.com>
2021-05-06 11:31:37 -0400

    Lex/Karan [#177817110] Fixes search index creation on local env

---
+++ scripts/init-elasticsearch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Waiting for ElasticSearch to become available..."
+./wait-for-it.sh elasticsearch:9200
+
+# Creates the index
+npx babel-node ./scripts/search/create-search-index.js
