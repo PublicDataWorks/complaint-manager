3d4c4a6812b467e2cd2d75085eca4f278d341cda
Andrew Rist <andrew.rist@thoughtworks.com>
2021-06-14 15:29:40 -0500

    updated assert script per code review comments

--- scripts/assertOnTestLatency.js
+++ scripts/assertOnTestLatency.js
@@ -1,36 +1,45 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const THRESHOLDS = { // set a specific threshold (in ms) for an operation, otherwise DEFAULT will be used
-    ['Retrieve Final Referral Letter URL']: 60,
-    DEFAULT: 500
+const THRESHOLDS = {
+  // set a specific threshold (in ms) for an operation, otherwise DEFAULT will be used
+  ["Retrieve Final Referral Letter URL"]: 60,
+  DEFAULT: 500
 };
 
-fs.readFile('output/letter-generation-test-stats.csv', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return 1;
-    }
+fs.readFile("output/letter-generation-test-stats.csv", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-    let failure = false;
-    let result = data.split('\n').map(row => {
-        let [operation, status, latency] = row.split(',');
-        return { operation, status, latency };
-    }).filter(res => res.operation && res.operation !== 'operation')
-        .forEach(res => {
-            let threshold = THRESHOLDS.DEFAULT;
-            if (THRESHOLDS[res.operation]) {
-                threshold = THRESHOLDS[res.operation];
-            }
+  let failure = false;
+  data
+    .split("\n")
+    .map(row => {
+      let [operation, status, latency] = row.split(",");
+      return { operation, status, latency };
+    })
+    .filter(res => res.operation && res.operation !== "operation")
+    .forEach(res => {
+      let threshold = THRESHOLDS.DEFAULT;
+      if (THRESHOLDS[res.operation]) {
+        threshold = THRESHOLDS[res.operation];
+      }
 
-            if (res.latency > threshold) {
-                failure = true;
-                console.error(`ERROR - operation: ${res.operation} took ${res.latency}ms, which is above its threshold of ${threshold}ms`);
-            } else {
-                console.log(`operation: ${res.operation} passed its threshold (${res.latency}ms < ${threshold}ms)`)
-            }
-        });
+      if (res.latency > threshold) {
+        failure = true;
+        console.error(
+          `ERROR - operation: ${res.operation} took ${res.latency}ms, which is above its threshold of ${threshold}ms`
+        );
+      } else {
+        console.log(
+          `operation: ${res.operation} passed its threshold (${res.latency}ms < ${threshold}ms)`
+        );
+      }
+    });
 
-    if (failure) {
-        throw new Error('latency validation failed (see above)');
-    }
+  if (failure) {
+    console.error("latency validation failed (see above)");
+    process.exit(1);
+  }
 });
