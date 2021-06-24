4580295cd5ab9f1e1c695f7ce73809abca6a05a7
Andrew Rist <andrew.rist@thoughtworks.com>
2021-06-24 14:31:47 -0500

    Andrew/Isabel [#177935455] Added search field tests for unmounting and negative scenario

--- src/client/policeDataManager/cases/SearchCases/SearchCasesForm.test.js
+++ src/client/policeDataManager/cases/SearchCases/SearchCasesForm.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { push } from "connected-react-router";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { waitFor } from "@testing-library/dom";
 import SearchCasesForm from "./SearchCasesForm";
 import createConfiguredStore from "../../../createConfiguredStore";
@@ -25,10 +25,14 @@ describe("SearchCasesForm", () => {
   });
 
   describe("onSubmit", () => {
+    let getByTestId, textField, submitButton;
+    beforeEach(() => {
+      getByTestId = searchCasesFormWrapper.getByTestId;
+      textField = getByTestId("searchField");
+      submitButton = getByTestId("searchButton");
+    });
+
     test("should redirect to results page with query string", async () => {
-      const { getByTestId } = searchCasesFormWrapper;
-      const textField = getByTestId("searchField");
-      const submitButton = getByTestId("searchButton");
       fireEvent.change(textField, { target: { value: searchQuery } });
       fireEvent.click(submitButton);
 
@@ -38,6 +42,38 @@ describe("SearchCasesForm", () => {
           push(`/search?queryString=${correctSearchQuery}`)
         );
       });
+    });
+
+    test("should not redirect if the query string is blank", async () => {
+      fireEvent.change(textField, { target: { value: "   " } });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => {
+        // Assert
+        expect(dispatchSpy).not.toHaveBeenCalledWith({
+          payload: {
+            args: expect.anything(),
+            method: "push"
+          },
+          type: expect.anything()
+        });
+      });
+    });
+
+    test("should display query string after unmounting and remounting component", async () => {
+      fireEvent.change(textField, { target: { value: searchQuery } });
+      fireEvent.click(submitButton);
+      searchCasesFormWrapper.unmount();
+      searchCasesFormWrapper = render(
+        <Provider store={store}>
+          <Router>
+            <SearchCasesForm />
+          </Router>
+        </Provider>
+      );
+
+      console.log(screen.queryByTestId("searchField"));
+      expect(screen.queryByTestId("searchField").value).toEqual(searchQuery);
     });
   });
 });
