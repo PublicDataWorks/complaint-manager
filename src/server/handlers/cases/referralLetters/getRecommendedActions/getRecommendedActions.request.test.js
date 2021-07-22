2c2ce2f0b72c0e65282fdce5178f672143f6b750
Isabel Olson <isabel.olson@thoughtworks.com>
2021-07-22 09:55:26 -0500

    Isabel [#178117617] Moved Insight out of repo

--- src/server/handlers/cases/referralLetters/getRecommendedActions/getRecommendedActions.request.test.js
+++ src/server/handlers/cases/referralLetters/getRecommendedActions/getRecommendedActions.request.test.js
@@ -17,8 +17,6 @@ describe("getRecommendedActions", function () {
     const reassignedDecription =
       "Be temporarily or permanently reassigned from his/her current assignment";
     const trainingDecription = "Receive training regarding any issues noted";
-    const insightDescription =
-      "Receive supervisory interventions and monitoring - INSIGHT";
 
     const reassigned = await models.recommended_action.create({
       description: reassignedDecription
@@ -26,14 +24,10 @@ describe("getRecommendedActions", function () {
     const training = await models.recommended_action.create({
       description: trainingDecription
     });
-    const insight = await models.recommended_action.create({
-      description: insightDescription
-    });
 
     const expectedResponseBody = [
       { id: reassigned.id, description: reassigned.description },
       { id: training.id, description: training.description },
-      { id: insight.id, description: insight.description }
     ];
 
     const responsePromise = request(app)
