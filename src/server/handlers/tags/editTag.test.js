a129fa7e94dd16b214d8f310731c113fbe847f04
Andrew Rist <andrew.rist@thoughtworks.com>
2021-08-03 16:26:41 -0500

    Lex/Andrew [#178523791] created editTagHelper to make db queries to edit a tag's name

---
+++ src/server/handlers/tags/editTag.test.js
@@ -0,0 +1,7 @@
+describe("editTag", () => {
+  test.todo("should return a 400 if the updated tag name already exists");
+
+  test.todo(
+    "should update the name of the tag if that name does not already exist"
+  );
+});
