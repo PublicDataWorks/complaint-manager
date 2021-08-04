d081b6204d638293414a0f9caebe4734b856d7ea
Andrew Rist <andrew.rist@thoughtworks.com>
2021-08-04 16:33:52 -0500

    Lex/Andrew [#178523791] calling editTag handler from front-end

--- src/server/handlers/tags/editTag.js
+++ src/server/handlers/tags/editTag.js
@@ -1,15 +1,16 @@
+import Boom from "boom";
 import asyncMiddleware from "../asyncMiddleware";
 import editTagAndAuditDetails from "./editTagHelper";
 
 const editTag = asyncMiddleware(async (request, response, next) => {
   try {
-    let result = editTagAndAuditDetails(
+    let result = await editTagAndAuditDetails(
       request,
       request.params.id,
       request.body
     );
 
-    response.status(200).send(result);
+    response.status(200).json(result);
   } catch (error) {
     next(error);
   }
