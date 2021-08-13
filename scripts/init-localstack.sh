6628bc72a172da8d78ed362bb849f36650ac3216
Karan Krishnani <krkrishn@thoughtworks.com>
2021-08-13 16:03:25 -0400

    Lex/Karan [#178490618] Creates buckets once on localstack init

--- scripts/init-localstack.sh
+++ scripts/init-localstack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-BUCKETS=(noipm-local noipm-referral-letters-local noipm-complainant-letters-local noipm-exports-local nopd-officers-local noipm-seed-files noipm-exports-local)
+BUCKETS=(noipm-local noipm-referral-letters-local noipm-complainant-letters-local noipm-exports-local nopd-officers-local noipm-seed-files)
 SEED_BUCKET_NAME=noipm-seed-files
-SEED_FILE_DIRECTORY="/app/src/instance-files/localstack-seed-files"
-FILES=($(ls $SEED_FILE_DIRECTORY))
+OFFICER_DATA_BUCKET=nopd-officers-local
+SEED_FILE_SRC_DIR="/app/src/instance-files/localstack-seed-files"
+FILES=($(ls $SEED_FILE_SRC_DIR))
 
 if [ "$USE_CLOUD_SERVICES" = "true" ]; then
     echo "Cloud services are enabled. Skipping Localstack setup."
@@ -18,16 +19,38 @@ export AWS_SECRET_ACCESS_KEY=test
 echo "Waiting for Localstack to become available..."
 ./wait-for-it.sh localstack:4566
 
-# Make the buckets
+# Make the buckets if they don't exist (and copy any seeded files into them)
 for BUCKET in "${BUCKETS[@]}"; do
-    aws --endpoint-url=http://host.docker.internal:4566 s3 mb "s3://$BUCKET" --region=us-east-1
+    # if the bucket doesn't exist
+    if aws --endpoint-url=http://host.docker.internal:4566 s3 ls "s3://$BUCKET" 2>&1 | grep -q 'NoSuchBucket'
+    then
+        # create the bucket
+        aws --endpoint-url=http://host.docker.internal:4566 s3 mb "s3://$BUCKET" --region=us-east-1
+
+        # run any file copy jobs for the bucket which was created
+        case $BUCKET in
+            
+            $SEED_BUCKET_NAME)
+                # Place DB seeder files into the noipm-seed-files bucket
+                for FILE in "${FILES[@]}"; do
+                    aws --endpoint-url=http://host.docker.internal:4566 s3 cp "$SEED_FILE_SRC_DIR/$FILE" "s3://$SEED_BUCKET_NAME"
+                done
+                ;;
+
+            $OFFICER_DATA_BUCKET)
+                # Place officer seed data in nopd-officers-local (from config) bucket
+                aws --endpoint-url=http://host.docker.internal:4566 s3 cp "$SEED_FILE_SRC_DIR/officerSeedData.csv" "s3://$OFFICER_DATA_BUCKET"
+                ;;
+            *)
+                echo "No files to be copied for $BUCKET bucket"
+                ;;
+        esac
+    else
+        echo Skip creating $BUCKET bucket since it already exists
+    fi
 done
 
-# Seed the buckets
-for FILE in "${FILES[@]}"; do
-    aws --endpoint-url=http://host.docker.internal:4566 s3 cp "$SEED_FILE_DIRECTORY/$FILE" "s3://$SEED_BUCKET_NAME"
-done
 
-# Officer Seed Data exception because REASONS
-aws --endpoint-url=http://host.docker.internal:4566 s3 cp "$SEED_FILE_DIRECTORY/officerSeedData.csv" "s3://nopd-officers-local"
+
+
 
