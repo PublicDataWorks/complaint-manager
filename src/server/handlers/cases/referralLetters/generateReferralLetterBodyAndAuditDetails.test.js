a0d545c5316e6b56d3a6a0a1a535bcc99bbee375
Andrew Rist <andrew.rist@thoughtworks.com>
2021-09-15 15:38:38 -0500

    Revert "Lex/Andrew [#178952455] fixed case reference prefix determination in cases model"
    This reverts commit 6c8b1b51d8002061fe9e2dd5b89bda542c5a5d2e.

--- src/server/handlers/cases/referralLetters/generateReferralLetterBodyAndAuditDetails.test.js
+++ src/server/handlers/cases/referralLetters/generateReferralLetterBodyAndAuditDetails.test.js
@@ -5,18 +5,6 @@ import {
   getReferralLetterCaseDataAndAuditDetails
 } from "./generateReferralLetterBodyAndAuditDetails";
 import { cleanupDatabase } from "../../../testHelpers/requestTestHelpers";
-import Case from "../../../../sharedTestHelpers/case";
-import CaseOfficer from "../../../../sharedTestHelpers/caseOfficer";
-import {
-  COMPLAINANT,
-  RANK_INITIATED
-} from "../../../../sharedUtilities/constants";
-import Officer from "../../../../sharedTestHelpers/Officer";
-
-const {
-  CIVILIAN_WITHIN_PD_INITIATED,
-  PERSON_TYPE
-} = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/constants`);
 
 jest.mock("handlebars", () => ({
   compile: jest.fn(() => {
@@ -27,14 +15,10 @@ jest.mock("handlebars", () => ({
 }));
 
 describe("generateReferralLetterBodyAndAuditDetails", () => {
-  let existingCase, officer;
+  let existingCase;
 
   beforeEach(async () => {
     existingCase = await createTestCaseWithCivilian();
-    officer = await models.officer.create(
-      new Officer.Builder().defaultOfficer(),
-      { auditUser: "auditUser" }
-    );
   });
 
   afterEach(async () => {
@@ -43,132 +27,18 @@ describe("generateReferralLetterBodyAndAuditDetails", () => {
 
   describe("getReferralLetterCaseDataAndAuditDetails", () => {
     test("should return caseData and auditDetails", async () => {
-      const referralLetterCaseDataAndAuditDetails =
-        await models.sequelize.transaction(async transaction => {
+      const referralLetterCaseDataAndAuditDetails = await models.sequelize.transaction(
+        async transaction => {
           return await getReferralLetterCaseDataAndAuditDetails(
             existingCase.id,
             transaction
           );
-        });
+        }
+      );
 
-      let caseReferenceSuffix = existingCase.caseNumber + "";
-      caseReferenceSuffix = caseReferenceSuffix.padStart(4, "0");
       expect(referralLetterCaseDataAndAuditDetails).toEqual({
         caseData: expect.objectContaining({
-          id: existingCase.id,
-          caseReference: `${PERSON_TYPE.CIVILIAN.abbreviation}2017-${caseReferenceSuffix}`,
-          caseReferencePrefix: PERSON_TYPE.CIVILIAN.abbreviation
-        }),
-        auditDetails: expectedReferralLetterCaseAuditDetails
-      });
-    });
-
-    test("should return caseData and auditDetails for Civilian Within PD", async () => {
-      let cpdCase = await models.cases.create(
-        new Case.Builder()
-          .defaultCase()
-          .withComplaintType(CIVILIAN_WITHIN_PD_INITIATED),
-        { auditUser: "nobody" }
-      );
-
-      await models.case_officer.create(
-        new CaseOfficer.Builder()
-          .defaultCaseOfficer()
-          .withCaseEmployeeType(
-            PERSON_TYPE.CIVILIAN_WITHIN_PD.employeeDescription
-          )
-          .withCaseId(cpdCase.id)
-          .withRoleOnCase(COMPLAINANT)
-          .withOfficerId(officer.id),
-        { auditUser: "auditUser" }
-      );
-
-      const referralLetterCaseDataAndAuditDetails =
-        await models.sequelize.transaction(async transaction => {
-          return await getReferralLetterCaseDataAndAuditDetails(
-            cpdCase.id,
-            transaction
-          );
-        });
-
-      let caseReferenceSuffix = cpdCase.caseNumber + "";
-      caseReferenceSuffix = caseReferenceSuffix.padStart(4, "0");
-      expect(referralLetterCaseDataAndAuditDetails).toEqual({
-        caseData: expect.objectContaining({
-          id: cpdCase.id,
-          caseReference: `${PERSON_TYPE.CIVILIAN_WITHIN_PD.abbreviation}2017-${caseReferenceSuffix}`,
-          caseReferencePrefix: PERSON_TYPE.CIVILIAN_WITHIN_PD.abbreviation
-        }),
-        auditDetails: expectedReferralLetterCaseAuditDetails
-      });
-    });
-
-    test("should return caseData and auditDetails for Police Officer", async () => {
-      let poCase = await models.cases.create(
-        new Case.Builder().defaultCase().withComplaintType(RANK_INITIATED),
-        { auditUser: "nobody" }
-      );
-
-      await models.case_officer.create(
-        new CaseOfficer.Builder()
-          .defaultCaseOfficer()
-          .withCaseId(poCase.id)
-          .withRoleOnCase(COMPLAINANT)
-          .withOfficerId(officer.id),
-        { auditUser: "auditUser" }
-      );
-
-      const referralLetterCaseDataAndAuditDetails =
-        await models.sequelize.transaction(async transaction => {
-          return await getReferralLetterCaseDataAndAuditDetails(
-            poCase.id,
-            transaction
-          );
-        });
-
-      let caseReferenceSuffix = poCase.caseNumber + "";
-      caseReferenceSuffix = caseReferenceSuffix.padStart(4, "0");
-      expect(referralLetterCaseDataAndAuditDetails).toEqual({
-        caseData: expect.objectContaining({
-          id: poCase.id,
-          caseReference: `${PERSON_TYPE.KNOWN_OFFICER.abbreviation}2017-${caseReferenceSuffix}`,
-          caseReferencePrefix: PERSON_TYPE.KNOWN_OFFICER.abbreviation
-        }),
-        auditDetails: expectedReferralLetterCaseAuditDetails
-      });
-    });
-
-    test("should return caseData and auditDetails for Anonymous", async () => {
-      let acCase = await models.cases.create(
-        new Case.Builder().defaultCase().withComplaintType(RANK_INITIATED),
-        { auditUser: "nobody" }
-      );
-
-      await models.case_officer.create(
-        new CaseOfficer.Builder()
-          .defaultCaseOfficer()
-          .withIsAnonymous(true)
-          .withRoleOnCase(COMPLAINANT)
-          .withCaseId(acCase.id)
-          .withOfficerId(officer.id),
-        { auditUser: "auditUser" }
-      );
-
-      const referralLetterCaseDataAndAuditDetails =
-        await models.sequelize.transaction(async transaction => {
-          return await getReferralLetterCaseDataAndAuditDetails(
-            acCase.id,
-            transaction
-          );
-        });
-
-      let caseReferenceSuffix = acCase.caseNumber + "";
-      caseReferenceSuffix = caseReferenceSuffix.padStart(4, "0");
-      expect(referralLetterCaseDataAndAuditDetails).toEqual({
-        caseData: expect.objectContaining({
-          id: acCase.id,
-          caseReference: `AC2017-${caseReferenceSuffix}`,
-          caseReferencePrefix: "AC"
+          id: existingCase.id
         }),
         auditDetails: expectedReferralLetterCaseAuditDetails
       });
@@ -177,13 +47,14 @@ describe("generateReferralLetterBodyAndAuditDetails", () => {
 
   describe("generateReferralLetterBodyAndAuditDetails", () => {
     test("should return an object with referralLetterBody and auditDetails", async () => {
-      const referralLetterBodyAndAuditDetails =
-        await models.sequelize.transaction(async transaction => {
+      const referralLetterBodyAndAuditDetails = await models.sequelize.transaction(
+        async transaction => {
           return await generateReferralLetterBodyAndAuditDetails(
             existingCase.id,
             transaction
           );
-        });
+        }
+      );
 
       expect(referralLetterBodyAndAuditDetails).toEqual({
         referralLetterBody: expect.anything(),
