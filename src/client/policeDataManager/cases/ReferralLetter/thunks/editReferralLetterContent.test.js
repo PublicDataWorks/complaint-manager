006260f8f494f15a9a36988e2debf41d2fcdebad
Andrew Rist <andrew.rist@thoughtworks.com>
2021-10-11 17:23:08 -0500

    Andrew [#179701966] using regex for body matcher where axios is randomly adding quotes

--- src/client/policeDataManager/cases/ReferralLetter/thunks/editReferralLetterContent.test.js
+++ src/client/policeDataManager/cases/ReferralLetter/thunks/editReferralLetterContent.test.js
@@ -21,7 +21,10 @@ describe("editReferralLetterContent" + "", () => {
         "content-type": "application/json"
       }
     })
-      .put(`/api/cases/${caseId}/referral-letter/content`, referralLetterHtml)
+      .put(
+        `/api/cases/${caseId}/referral-letter/content`,
+        new RegExp(`.*${referralLetterHtml}.*`)
+      )
       .reply(200);
     await editReferralLetterContent(
       caseId,
