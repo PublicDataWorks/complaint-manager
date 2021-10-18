5fce131313798aa8ec2511154464bee61aa59554
Andrew Rist <andrew.rist@thoughtworks.com>
2021-10-18 11:04:45 -0500

    Lex/Andrew [#179773093] created districts layer on the map visualization using choroplethmapbox

--- src/client/common/components/Visualization/Transformers/locationDataTransformer.js
+++ src/client/common/components/Visualization/Transformers/locationDataTransformer.js
@@ -1,3 +1,5 @@
+import districts from "../layers/NOPD_Police_Districts.json";
+
 export const transformData = rawData => ({
   data: [
     {
@@ -12,6 +14,43 @@ export const transformData = rawData => ({
       colorscale: "Cividis",
       showscale: false,
       showlegend: false
+    },
+    {
+      type: "choroplethmapbox",
+      geojson: districts,
+      locationmode: "geojson-id",
+      featureidkey: "id",
+      locations: [
+        "District1",
+        "District2",
+        "District3",
+        "District4",
+        "District5",
+        "District6",
+        "District7",
+        "District8"
+      ],
+      text: [
+        "Police District 1",
+        "Police District 2",
+        "Police District 3",
+        "Police District 4",
+        "Police District 5",
+        "Police District 6",
+        "Police District 7",
+        "Police District 8"
+      ],
+      hoverinfo: "text",
+      showlegend: false,
+      showscale: false,
+      autocolorscale: false,
+      colorscale: "Jet",
+      z: [1, 2, 3, 4, 5, 6, 7, 8],
+      zmin: 1,
+      zmax: 8,
+      marker: {
+        opacity: 0.3
+      }
     }
   ]
 });
