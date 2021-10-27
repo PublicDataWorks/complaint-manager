a936200e2819047c4047c4ad5f894385f4c18458
midenlexjoseph <midenlex.joseph@gmail.com>
2021-10-27 15:33:20 -0400

    Andrew/Lex [#179954445] Move map constants to instance files

--- src/client/common/components/Visualization/MapVisualization.js
+++ src/client/common/components/Visualization/MapVisualization.js
@@ -4,134 +4,17 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { getVisualizationData } from "./getVisualizationData";
 import { PlotlyWrapper } from "./PlotlyWrapper";
-import districts from "./layers/NOPD_Police_Districts.json";
-import schools from "./layers/Schools.json";
-import parks from "./layers/parks.json";
-import libraries from "./layers/Public_Libraries.json";
 import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
 
+const { MAP_CONFIG
+} = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/constants`);
+
 // TODO move these three consts to instance-files
-const NOLA_CENTER = { lat: 29.947, lon: -90.07 };
-const DEFAULT_ZOOM = 10;
-const MAP_LAYERS = [
-  {
-    text: "NOPD Districts",
-    layers: [
-      {
-        sourcetype: "geojson",
-        source: districts,
-        type: "line"
-      }
-    ],
-    data: [
-      {
-        type: "choroplethmapbox",
-        geojson: districts,
-        locationmode: "geojson-id",
-        featureidkey: "id",
-        locations: [
-          "District1",
-          "District2",
-          "District3",
-          "District4",
-          "District5",
-          "District6",
-          "District7",
-          "District8"
-        ],
-        text: [
-          "Police District 1",
-          "Police District 2",
-          "Police District 3",
-          "Police District 4",
-          "Police District 5",
-          "Police District 6",
-          "Police District 7",
-          "Police District 8"
-        ],
-        hoverinfo: "text",
-        showlegend: false,
-        showscale: false,
-        autocolorscale: false,
-        colorscale: "Jet",
-        z: [1, 2, 3, 4, 5, 6, 7, 8],
-        zmin: 1,
-        zmax: 8,
-        marker: {
-          opacity: 0.3
-        }
-      }
-    ]
-  },
-  {
-    text: "Schools",
-    checkboxColor: "red",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: schools.features.map(school => school.geometry.coordinates[1]),
-        lon: schools.features.map(school => school.geometry.coordinates[0]),
-        text: schools.features.map(
-          school => `School Name: ${school.properties.NAME}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "red",
-          size: 9
-        }
-      }
-    ]
-  },
-  {
-    text: "Parks",
-    checkboxColor: "green",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: parks.features.map(school => school.geometry.coordinates[1]),
-        lon: parks.features.map(school => school.geometry.coordinates[0]),
-        text: parks.features.map(
-          school => `Park Name: ${school.properties.name}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "green",
-          size: 9
-        }
-      }
-    ]
-  },
-  {
-    text: "Public Libraries",
-    checkboxColor: "#00BFFF",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: libraries.features.map(school => school.geometry.coordinates[1]),
-        lon: libraries.features.map(school => school.geometry.coordinates[0]),
-        text: libraries.features.map(
-          school => `Library Name: ${school.properties.FacilityName}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "#00BFFF",
-          size: 9
-        }
-      }
-    ]
-  }
-];
 
 const MapVisualization = props => {
   const [location, setLocation] = useState({ lat: [], lon: [], z: [] });
   const [visibleLayers, setVisibleLayers] = useState(
-    MAP_LAYERS.map(() => false)
+    MAP_CONFIG.LAYERS.map(() => false)
   );
 
   useEffect(() => {
@@ -165,10 +48,10 @@ const MapVisualization = props => {
     }
   ];
   let layers = [];
-  for (let i in MAP_LAYERS) {
+  for (let i in MAP_CONFIG.LAYERS) {
     if (visibleLayers[i]) {
-      data.push(...MAP_LAYERS[i].data);
-      layers.push(...MAP_LAYERS[i].layers);
+      data.push(...MAP_CONFIG.LAYERS[i].data);
+      layers.push(...MAP_CONFIG.LAYERS[i].layers);
     }
   }
 
@@ -193,8 +76,8 @@ const MapVisualization = props => {
           dragmode: "zoom",
           mapbox: {
             style: "open-street-map",
-            center: NOLA_CENTER,
-            zoom: DEFAULT_ZOOM,
+            center: MAP_CONFIG.CENTER,
+            zoom: MAP_CONFIG.DEFAULT_ZOOM,
             opacity: 0.9,
             autocolorscale: false,
             layers: layers
@@ -205,7 +88,7 @@ const MapVisualization = props => {
       <section>
         <h3>Map Layers</h3>
         <section style={{ display: "flex", flexDirection: "column" }}>
-          {MAP_LAYERS.map((layer, idx) => (
+          {MAP_CONFIG.LAYERS.map((layer, idx) => (
             <FormControlLabel
               key={layer.text}
               control={
