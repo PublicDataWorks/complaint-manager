1d44b2864739c057bd6f3b9708df655d7fab7125
Andrew Rist <andrew.rist@thoughtworks.com>
2021-10-29 21:37:00 -0500

    Revert "Andrew [#179775104] refactor: extracted layer data to constant outside the component"
    This reverts commit 89a83a5b2333f732736f0d7fa58b421012746187.

--- src/client/common/components/Visualization/MapVisualization.js
+++ src/client/common/components/Visualization/MapVisualization.js
@@ -10,123 +10,99 @@ import libraries from "./layers/Public_Libraries.json";
 import { QUERY_TYPES } from "../../../../sharedUtilities/constants";
 import moment from "moment";
 
-// TODO move these three consts to instance-files
 const NOLA_CENTER = { lat: 29.947, lon: -90.07 };
 const DEFAULT_ZOOM = 10;
-const MAP_LAYERS = [
-  {
-    text: "NOPD Districts",
-    layers: [
-      {
-        sourcetype: "geojson",
-        source: districts,
-        type: "line"
-      }
-    ],
-    data: [
-      {
-        type: "choroplethmapbox",
-        geojson: districts,
-        locationmode: "geojson-id",
-        featureidkey: "id",
-        locations: [
-          "District1",
-          "District2",
-          "District3",
-          "District4",
-          "District5",
-          "District6",
-          "District7",
-          "District8"
-        ],
-        text: [
-          "Police District 1",
-          "Police District 2",
-          "Police District 3",
-          "Police District 4",
-          "Police District 5",
-          "Police District 6",
-          "Police District 7",
-          "Police District 8"
-        ],
-        hoverinfo: "text",
-        showlegend: false,
-        showscale: false,
-        autocolorscale: false,
-        colorscale: "Jet",
-        z: [1, 2, 3, 4, 5, 6, 7, 8],
-        zmin: 1,
-        zmax: 8,
-        marker: {
-          opacity: 0.3
-        }
-      }
-    ]
-  },
-  {
-    text: "Schools",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: schools.features.map(school => school.geometry.coordinates[1]),
-        lon: schools.features.map(school => school.geometry.coordinates[0]),
-        text: schools.features.map(
-          school => `School Name: ${school.properties.NAME}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "red"
-        }
-      }
-    ]
-  },
-  {
-    text: "Parks",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: parks.features.map(school => school.geometry.coordinates[1]),
-        lon: parks.features.map(school => school.geometry.coordinates[0]),
-        text: parks.features.map(
-          school => `Park Name: ${school.properties.name}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "green"
-        }
-      }
-    ]
-  },
-  {
-    text: "Public Libraries",
-    layers: [],
-    data: [
-      {
-        type: "scattermapbox",
-        lat: libraries.features.map(school => school.geometry.coordinates[1]),
-        lon: libraries.features.map(school => school.geometry.coordinates[0]),
-        text: libraries.features.map(
-          school => `Library Name: ${school.properties.FacilityName}`
-        ),
-        hoverinfo: "text",
-        showlegend: false,
-        marker: {
-          color: "#00BFFF"
-        }
-      }
-    ]
+
+const DISTRICT_DATA = {
+  type: "choroplethmapbox",
+  geojson: districts,
+  locationmode: "geojson-id",
+  featureidkey: "id",
+  locations: [
+    "District1",
+    "District2",
+    "District3",
+    "District4",
+    "District5",
+    "District6",
+    "District7",
+    "District8"
+  ],
+  text: [
+    "Police District 1",
+    "Police District 2",
+    "Police District 3",
+    "Police District 4",
+    "Police District 5",
+    "Police District 6",
+    "Police District 7",
+    "Police District 8"
+  ],
+  hoverinfo: "text",
+  showlegend: false,
+  showscale: false,
+  autocolorscale: false,
+  colorscale: "Jet",
+  z: [1, 2, 3, 4, 5, 6, 7, 8],
+  zmin: 1,
+  zmax: 8,
+  marker: {
+    opacity: 0.3
   }
-];
+};
+
+const DISTRICT_OUTLINE = {
+  sourcetype: "geojson",
+  source: districts,
+  type: "line"
+};
+
+const SCHOOLS_DATA = {
+  type: "scattermapbox",
+  lat: schools.features.map(school => school.geometry.coordinates[1]),
+  lon: schools.features.map(school => school.geometry.coordinates[0]),
+  text: schools.features.map(
+    school => `School Name: ${school.properties.NAME}`
+  ),
+  hoverinfo: "text",
+  showlegend: false,
+  marker: {
+    color: "red"
+  }
+};
+
+const PARKS_DATA = {
+  type: "scattermapbox",
+  lat: parks.features.map(school => school.geometry.coordinates[1]),
+  lon: parks.features.map(school => school.geometry.coordinates[0]),
+  text: parks.features.map(school => `Park Name: ${school.properties.name}`),
+  hoverinfo: "text",
+  showlegend: false,
+  marker: {
+    color: "green"
+  }
+};
+
+const LIBRARIES_DATA = {
+  type: "scattermapbox",
+  lat: libraries.features.map(school => school.geometry.coordinates[1]),
+  lon: libraries.features.map(school => school.geometry.coordinates[0]),
+  text: libraries.features.map(
+    school => `Library Name: ${school.properties.FacilityName}`
+  ),
+  hoverinfo: "text",
+  showlegend: false,
+  marker: {
+    color: "#00BFFF"
+  }
+};
 
 const MapVisualization = props => {
   const [location, setLocation] = useState({ lat: [], lon: [], z: [] });
-  const [visibleLayers, setVisibleLayers] = useState(
-    MAP_LAYERS.map(() => false)
-  );
+  const [showDistrict, setShowDistrict] = useState(false);
+  const [showSchools, setShowSchools] = useState(false);
+  const [showParks, setShowParks] = useState(false);
+  const [showLibraries, setShowLibraries] = useState(false);
 
   useEffect(() => {
     const load = async () =>
@@ -142,7 +118,6 @@ const MapVisualization = props => {
 
     load();
   }, []);
-
   let data = [
     {
       type: "densitymapbox",
@@ -159,11 +134,22 @@ const MapVisualization = props => {
     }
   ];
   let layers = [];
-  for (let i in MAP_LAYERS) {
-    if (visibleLayers[i]) {
-      data.push(...MAP_LAYERS[i].data);
-      layers.push(...MAP_LAYERS[i].layers);
-    }
+
+  if (showDistrict) {
+    data.push(DISTRICT_DATA);
+    layers.push(DISTRICT_OUTLINE);
+  }
+
+  if (showSchools) {
+    data.push(SCHOOLS_DATA);
+  }
+
+  if (showParks) {
+    data.push(PARKS_DATA);
+  }
+
+  if (showLibraries) {
+    data.push(LIBRARIES_DATA);
   }
 
   return (
@@ -199,23 +185,46 @@ const MapVisualization = props => {
       <section>
         <h3>Map Layers</h3>
         <section style={{ display: "flex", flexDirection: "column" }}>
-          {MAP_LAYERS.map((layer, idx) => (
-            <FormControlLabel
-              key={layer.text}
-              control={
-                <Checkbox
-                  checked={visibleLayers[idx]}
-                  onChange={event => {
-                    let newVisibleLayers = [...visibleLayers];
-                    newVisibleLayers[idx] = event.target.checked;
-                    setVisibleLayers(newVisibleLayers);
-                  }}
-                  color="default"
-                />
-              }
-              label={layer.text}
-            />
-          ))}
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={showDistrict}
+                onChange={event => setShowDistrict(event.target.checked)}
+                color="default"
+              />
+            }
+            label="NOPD Districts"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={showSchools}
+                onChange={event => setShowSchools(event.target.checked)}
+                color="default"
+              />
+            }
+            label="Schools"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={showParks}
+                onChange={event => setShowParks(event.target.checked)}
+                color="default"
+              />
+            }
+            label="Parks"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={showLibraries}
+                onChange={event => setShowLibraries(event.target.checked)}
+                color="default"
+              />
+            }
+            label="Public Libraries"
+          />
         </section>
       </section>
     </section>
