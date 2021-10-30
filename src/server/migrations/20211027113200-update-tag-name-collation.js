a75a9be37ccd2e27b09a41ce637be6770e9201f4
Andrew Rist <andrew.rist@thoughtworks.com>
2021-10-29 21:36:39 -0500

    Revert "Andrew [#179789124] updated collation migration to include both sortable_cases_views"
    This reverts commit 594a344d37829f81b595678cb94937437c887b43.

--- src/server/migrations/20211027113200-update-tag-name-collation.js
+++ src/server/migrations/20211027113200-update-tag-name-collation.js
@@ -157,20 +157,17 @@ const reverseCollationQuery =
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const viewName = "sortable_cases_view";
-    const viewName2 = "sortable_cases_view_2"
 
     try {
       await queryInterface.sequelize.transaction(async transaction => {
         await queryInterface.sequelize.query(`DROP VIEW ${viewName}`, {
           transaction
         });
-        await queryInterface.sequelize.query(`DROP VIEW ${viewName2}`)
         await queryInterface.sequelize.query(collationQuery, { transaction });
         await queryInterface.sequelize.query(
           sortableCasesViewWithMultipleAccused,
           { transaction }
         );
-        await queryInterface.sequelize.query(sortableCasesViewWithMultipleAccused.replace(viewName, viewName2), { transaction });
       });
     } catch (error) {
       throw new Error(
@@ -181,20 +178,19 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     const viewName = "sortable_cases_view";
-    const viewName2 = "sortable_cases_view_2"
 
     try {
       await queryInterface.sequelize.transaction(async transaction => {
         await queryInterface.sequelize.query(`DROP VIEW ${viewName}`, {
           transaction
         });
-        await queryInterface.sequelize.query(`DROP VIEW ${viewName2}`)
-        await queryInterface.sequelize.query(reverseCollationQuery, { transaction });
+        await queryInterface.sequelize.query(reverseCollationQuery, {
+          transaction
+        });
         await queryInterface.sequelize.query(
           sortableCasesViewWithMultipleAccused,
           { transaction }
         );
-        await queryInterface.sequelize.query(sortableCasesViewWithMultipleAccused.replace(viewName, viewName2), { transaction });
       });
     } catch (error) {
       throw new Error(
