e0092ad3ecbdea9ae91bdff5a235b610dda19758
Andrew Rist <andrew.rist@thoughtworks.com>
2021-10-29 21:43:55 -0500

    Revert "Lex/Andrew [#179648493] resets current page on cases table when it is remounted"
    This reverts commit cdfa29b4b01050ce1f736f1085d5f500b1c268a3.

--- src/client/policeDataManager/cases/CasesTable/CasesTable.js
+++ src/client/policeDataManager/cases/CasesTable/CasesTable.js
@@ -181,7 +181,6 @@ class CasesTable extends React.Component {
   }
 
   componentDidMount() {
-    this.currentPage = 1;
     this.getCases(SORT_CASES_BY.CASE_REFERENCE, DESCENDING, 1);
     this.updateSort(SORT_CASES_BY.CASE_REFERENCE, DESCENDING);
   }
