f09555d210633c5239b6271209c5dacdf61f575d
Karan Krishnani <krkrishn@thoughtworks.com>
2021-12-16 12:40:04 -0500

    chore:Karan [#73] moves noipm infra scripts to instance files noipm

--- infra/envs/common/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "elasticsearch_https_endpoint" { 
-  value = ec_deployment.ec-deployment.elasticsearch[0].https_endpoint 
-} 
-output "elasticsearch_username" { 
-  value = ec_deployment.ec-deployment.elasticsearch_username 
-}
-output "elasticsearch_password" { 
-  value = ec_deployment.ec-deployment.elasticsearch_password 
-}
