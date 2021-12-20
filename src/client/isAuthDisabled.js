a77b2497639cbaa910a2e984b8bc687dc6276c4a
Karan Krishnani <krkrishn@thoughtworks.com>
2021-12-20 13:09:20 -0500

    chore: Andrew/Karan [#180034570] Disables authentication on Demo env

--- src/client/isAuthDisabled.js
+++ src/client/isAuthDisabled.js
@@ -1,6 +1,7 @@
 export const isAuthDisabled = () => {
   return (
     process.env.REACT_APP_USE_CLOUD_SERVICES === false ||
-    process.env.REACT_APP_USE_CLOUD_SERVICES === "false"
+    process.env.REACT_APP_USE_CLOUD_SERVICES === "false" ||
+    process.env.REACT_APP_ENV === "demo"
   );
 };
--- src/server/isAuthDisabled.js
+++ src/server/isAuthDisabled.js
@@ -1,6 +1,7 @@
 export const isAuthDisabled = () => {
   return (
     process.env.USE_CLOUD_SERVICES === false ||
-    process.env.USE_CLOUD_SERVICES === "false"
+    process.env.USE_CLOUD_SERVICES === "false" ||
+    process.env.NODE_ENV === "demo"
   );
 };
