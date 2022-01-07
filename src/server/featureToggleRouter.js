0bc47b34fd8792626b9d5cb35ead56091d832dec
Karan Krishnani <krkrishn@thoughtworks.com>
2022-01-07 11:26:31 -0500

    feat: Andrew/Karan [#80] Makes feature flags available within instance files

--- src/server/featureToggleRouter.js
+++ src/server/featureToggleRouter.js
@@ -5,7 +5,7 @@ const express = require("express");
 const router = express.Router();
 const asyncMiddleware = require("./handlers/asyncMiddleware");
 
-const features = require("./config/features");
+const features = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/features`);
 const criteria = [
   {
     id: "isPreProd",
