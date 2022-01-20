e352af59f485779ab9acd9993df8c20ed0177866
midenlexjoseph <midenlex.joseph@gmail.com>
2022-01-20 16:00:37 -0500

    chore: Cat/Andrew/Lex [#180351221] addresses the remaining accessibility serious/critical issues

--- src/client/policeDataManager/shared/components/ExpansionPanelIconButton.js
+++ src/client/policeDataManager/shared/components/ExpansionPanelIconButton.js
@@ -3,14 +3,15 @@ import React from "react";
 
 const ExpansionPanelIconButton = ({ disabled }) => (
   <div style={{ width: "36px", marginRight: 16 }}>
-    <IconButton
+    <Icon
       color="secondary"
       className="chevron-right"
+      title="expansion-button"
       style={{ height: "36px", width: "36px" }}
       disabled={disabled}
     >
-      <Icon>unfold_more</Icon>
-    </IconButton>
+      unfold_more
+    </Icon>
   </div>
 );
 
