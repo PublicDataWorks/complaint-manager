3d5008de02f0a02c1f356a368c91805c53541491
Andrew Rist <andrew.rist@thoughtworks.com>
2022-03-24 09:33:42 -0500

    chore: Andrew [#116] sets feature flags to behave like prod in staging

--- src/server/featureToggleRouter.js
+++ src/server/featureToggleRouter.js
@@ -11,7 +11,7 @@ const criteria = [
     id: "isPreProd",
     check: user =>
       process.env.NODE_ENV !== "production" &&
-      process.env.NODE_ENV !== "staging"
+      process.env.NODE_ENV !== "development"
   },
   {
     id: "off",
