7cf94bef7035f68b98857cea8ca111d96d6a2860
Andrew Rist <andrew.rist@thoughtworks.com>
2022-05-06 14:38:56 -0500

    chore: Andrew [#000] extends the time waiting for deploy to succeed

--- scripts/ensure-deploy-succeeded.sh
+++ scripts/ensure-deploy-succeeded.sh
@@ -8,7 +8,7 @@ URL=$1
 # This script needs to run inside an Alpine container
 apk --no-cache add curl
 
-for i in `seq 1 10`; do
+for i in `seq 1 20`; do
     curl -f $URL
     if [ 0 -eq $? ]; then
         echo "App is up!"
