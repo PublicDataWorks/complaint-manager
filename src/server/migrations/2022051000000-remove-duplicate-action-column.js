17e7ba8248cd1a8ba69b3b79eb55e40029477fa5
Maria DelSignore <maria.delsignore@thoughtworks.com>
2022-05-11 10:19:50 -0600

    refactor: Maria/Andrew [#109] remove duplicate action column from case notes table

---
+++ src/server/migrations/2022051000000-remove-duplicate-action-column.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const ADD_COLUMN_QUERY = `ALTER TABLE case_notes
+  ADD COLUMN IF NOT EXISTS action`;
+
+const REMOVE_COLUMN_QUERY = `ALTER TABLE case_notes 
+  DROP COLUMN IF EXISTS action`;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(REMOVE_COLUMN_QUERY, { 
+          transaction 
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while restructuring case notes table to remove duplicate action column. Internal Error: ${error}`
+      );
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(ADD_COLUMN_QUERY, {
+          transaction
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while rolling back duplicate action column update. Internal Error: ${error}`
+      );
+    }
+  }
+};
