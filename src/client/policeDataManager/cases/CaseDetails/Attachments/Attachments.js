58f7bf72dda8311017dbda09b327eea7c30d1f79
Maria DelSignore <maria.delsignore@thoughtworks.com>
2022-05-19 10:44:04 -0600

    feature: Maria [#136] add edit case permission to upload an attachment

--- src/client/policeDataManager/cases/CaseDetails/Attachments/Attachments.js
+++ src/client/policeDataManager/cases/CaseDetails/Attachments/Attachments.js
@@ -4,6 +4,9 @@ import { CardContent, Typography } from "@material-ui/core";
 import styles from "../../../../common/globalStyling/styles";
 import DropzoneContainer from "./DropzoneContainer";
 import AttachmentsList from "./AttachmentsList";
+import { connect } from "react-redux";
+import { property } from "lodash";
+import { USER_PERMISSIONS } from "../../../../../sharedUtilities/constants";
 
 const Attachments = props => {
   return (
@@ -25,8 +28,12 @@ const Attachments = props => {
           <div style={{ flex: 2 }} />
         </div>
         <AttachmentsList />
-        {props.isArchived ? null : (
-          <div style={{ marginTop: "48px" }}>
+        {props.isArchived ||
+        !props.permissions?.includes(USER_PERMISSIONS.EDIT_CASE) ? null : (
+          <div
+            style={{ marginTop: "48px" }}
+            data-testid="file-upload-container"
+          >
             <div style={{ marginBottom: "8px" }}>
               <Typography style={styles.section}>UPLOAD A FILE</Typography>
             </div>
@@ -41,4 +48,6 @@ const Attachments = props => {
   );
 };
 
-export default Attachments;
+export default connect(state => ({
+  permissions: state?.users?.current?.userInfo?.permissions
+}))(Attachments);
--- src/client/policeDataManager/cases/CaseDetails/Attachments/Attachments.test.js
+++ src/client/policeDataManager/cases/CaseDetails/Attachments/Attachments.test.js
@@ -2,10 +2,14 @@ import React from "react";
 import Attachments from "./Attachments";
 import { mount } from "enzyme";
 import Case from "../../../../../sharedTestHelpers/case";
-import { containsText } from "../../../../testHelpers";
+import {
+  containsText,
+  expectEventuallyNotToExist
+} from "../../../../testHelpers";
 import createConfiguredStore from "../../../../createConfiguredStore";
 import { Provider } from "react-redux";
 import { getCaseDetailsSuccess } from "../../../actionCreators/casesActionCreators";
+import { USER_PERMISSIONS } from "../../../../../sharedUtilities/constants";
 
 jest.mock("../../../../common/auth/getAccessToken", () =>
   jest.fn(() => "TEST_TOKEN")
@@ -14,68 +18,95 @@ jest.mock("../../../../common/auth/getAccessToken", () =>
 describe("Attachments card", () => {
   let store;
 
-  beforeEach(() => {
-    store = createConfiguredStore();
+  describe("without permissions", () => {
+    beforeEach(() => {
+      store = createConfiguredStore();
+      store.dispatch({
+        type: "AUTH_SUCCESS",
+        userInfo: { permissions: [USER_PERMISSIONS.MANAGE_TAGS] }
+      });
+    });
+
+    test("should not display UPLOAD A FILE", () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <Attachments />
+        </Provider>
+      );
+
+      expect(
+        wrapper.find('[data-testid="file-upload-container"]')
+      ).toHaveLength(0);
+    });
   });
 
-  test("should display attachments if they exist", () => {
-    const caseDetail = new Case.Builder().defaultCase().build();
-    const attachmentFileName = caseDetail.attachments[0].fileName;
+  describe("with permissions", () => {
+    beforeEach(() => {
+      store = createConfiguredStore();
+      store.dispatch({
+        type: "AUTH_SUCCESS",
+        userInfo: { permissions: [USER_PERMISSIONS.EDIT_CASE] }
+      });
+    });
+    test("should display attachments if they exist", () => {
+      const caseDetail = new Case.Builder().defaultCase().build();
+      const attachmentFileName = caseDetail.attachments[0].fileName;
 
-    store.dispatch(getCaseDetailsSuccess(caseDetail));
+      store.dispatch(getCaseDetailsSuccess(caseDetail));
 
-    const wrapper = mount(
-      <Provider store={store}>
-        <Attachments />
-      </Provider>
-    );
+      const wrapper = mount(
+        <Provider store={store}>
+          <Attachments />
+        </Provider>
+      );
 
-    containsText(
-      wrapper,
-      'div[data-testid="attachmentsField"]',
-      attachmentFileName
-    );
-  });
+      containsText(
+        wrapper,
+        'div[data-testid="attachmentsField"]',
+        attachmentFileName
+      );
+    });
 
-  test("should display no attachments is null", () => {
-    const caseDetail = new Case.Builder()
-      .defaultCase()
-      .withAttachments(null)
-      .build();
+    test("should display no attachments is null", () => {
+      const caseDetail = new Case.Builder()
+        .defaultCase()
+        .withAttachments(null)
+        .build();
 
-    store.dispatch(getCaseDetailsSuccess(caseDetail));
+      store.dispatch(getCaseDetailsSuccess(caseDetail));
 
-    const wrapper = mount(
-      <Provider store={store}>
-        <Attachments />
-      </Provider>
-    );
+      const wrapper = mount(
+        <Provider store={store}>
+          <Attachments />
+        </Provider>
+      );
 
-    containsText(
-      wrapper,
-      'div[data-testid="attachmentsField"]',
-      "No files are attached"
-    );
-  });
+      containsText(
+        wrapper,
+        'div[data-testid="attachmentsField"]',
+        "No files are attached"
+      );
+    });
 
-  test("should display no attachments if attachments is empty list", () => {
-    const caseDetail = new Case.Builder()
-      .defaultCase()
-      .withAttachments([])
-      .build();
+    test("should display no attachments if attachments is empty list", () => {
+      const caseDetail = new Case.Builder()
+        .defaultCase()
+        .withAttachments([])
+        .build();
 
-    store.dispatch(getCaseDetailsSuccess(caseDetail));
+      store.dispatch(getCaseDetailsSuccess(caseDetail));
 
-    const wrapper = mount(
-      <Provider store={store}>
-        <Attachments />
-      </Provider>
-    );
+      const wrapper = mount(
+        <Provider store={store}>
+          <Attachments />
+        </Provider>
+      );
 
-    containsText(
-      wrapper,
-      'div[data-testid="attachmentsField"]',
-      "No files are attached"
-    );
+      containsText(
+        wrapper,
+        'div[data-testid="attachmentsField"]',
+        "No files are attached"
+      );
+    });
   });
 });
