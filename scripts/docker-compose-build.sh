7a97b6c4972a013c938f5b32da0e7a73c53b2af8
Isabel Olson <isabel.olson@thoughtworks.com>
2022-07-20 14:29:07 -0500

    chore: Isabel [#000] updates the docker image name

--- scripts/docker-compose-build.sh
+++ scripts/docker-compose-build.sh
@@ -26,6 +26,6 @@ echo "Building..."
 INSTANCE_IMAGE=${INSTANCE_IMAGE} INSTANCE_VERSION=${INSTANCE_VERSION} E2E_IMAGE=${E2E_IMAGE} docker-compose build
 
 echo "Copying updated yarn.lock from container"
-docker run --rm --entrypoint cat police_data_manager_app:latest /app/yarn.lock > yarn.lock
+docker run --rm --entrypoint cat complaint-manager_app:latest /app/yarn.lock > yarn.lock
 
 echo "Build complete"
