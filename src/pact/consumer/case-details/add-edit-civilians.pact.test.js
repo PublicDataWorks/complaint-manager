59f3c190988d802ec912c2b72029dfc02fba9818
Andrew Rist <andrew.rist@thoughtworks.com>
2022-10-03 16:49:34 -0500

    test: Andrew/Syamala [#174] added pact test that adds civilian witness

--- src/pact/consumer/case-details/add-edit-civilians.pact.test.js
+++ src/pact/consumer/case-details/add-edit-civilians.pact.test.js
@@ -2,7 +2,7 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { pactWith } from "jest-pact";
-import { like, eachLike } from "@pact-foundation/pact/src/dsl/matchers";
+import { like } from "@pact-foundation/pact/src/dsl/matchers";
 import { setUpCaseDetailsPage } from "./case-details-helper";
 
 jest.mock(
@@ -16,162 +16,149 @@ jest.mock(
   }
 );
 
-pactWith(
-  {
-    consumer: "complaint-manager.client",
-    provider: "complaint-manager.server",
-    logLevel: "ERROR",
-    timeout: 500000
-  },
-  provider => {
-    beforeAll(async () => {
-      axios.defaults.baseURL = provider.mockService.baseUrl;
-    });
+jest.setTimeout(500000);
+const scenarios = [
+  { role: "Complainant", addButtonIndex: 0 },
+  { role: "Witness", addButtonIndex: 1 }
+];
 
-    describe("add/edit civilians complainant/witnesses", () => {
-      beforeEach(async () => await setUpCaseDetailsPage(provider));
-      test("should add a civiliant complainant", async () => {
-        await provider.addInteraction({
-          state:
-            "Case exists: race ethnicities exist: gender identities exist: civilian titles exist",
-          uponReceiving: "add complainant",
-          withRequest: {
-            method: "POST",
-            path: "/api/cases/1/civilians",
-            headers: {
-              "Content-Type": "application/json"
+scenarios.forEach(({ role, addButtonIndex }) => {
+  pactWith(
+    {
+      consumer: "complaint-manager.client",
+      provider: "complaint-manager.server",
+      logLevel: "ERROR",
+      timeout: 500000
+    },
+    provider => {
+      beforeAll(async () => {
+        axios.defaults.baseURL = provider.mockService.baseUrl;
+      });
+
+      describe(`add/edit civilian ${role}`, () => {
+        beforeEach(async () => await setUpCaseDetailsPage(provider));
+
+        test(`should add a civiliant ${role}`, async () => {
+          const civilian = {
+            fullName: "Andrew Rist",
+            id: 3944,
+            firstName: "Andrew",
+            lastName: "Rist",
+            roleOnCase: "Complainant",
+            email: "fakeemail@email.com",
+            isAnonymous: false,
+            createdAt: "2022-09-29T18:24:28.907Z",
+            updatedAt: "2022-09-29T18:24:28.907Z",
+            caseId: 1,
+            raceEthnicityId: 17,
+            genderIdentityId: 1,
+            civilianTitleId: 2,
+            raceEthnicity: {
+              id: 17,
+              name: "White",
+              createdAt: "2019-01-07T20:08:30.512Z",
+              updatedAt: "2019-01-07T20:08:30.512Z"
             },
-            body: {
-              roleOnCase: "Complainant",
-              caseId: 1,
-              civilianTitleId: 2,
-              firstName: "Andrew",
-              lastName: "Rist",
-              genderIdentityId: 1,
-              raceEthnicityId: 2,
-              email: "fakeemail@email.com"
+            genderIdentity: {
+              id: 1,
+              name: "Male",
+              createdAt: "2019-04-19T21:31:23.009Z",
+              updatedAt: "2019-04-19T21:31:23.009Z"
             }
-          },
-          willRespondWith: {
-            status: 201,
-            body: like({
-              primaryComplainant: {
-                fullName: "Andrew Rist",
-                id: 3944,
+          };
+
+          await provider.addInteraction({
+            state:
+              "Case exists: race ethnicities exist: gender identities exist: civilian titles exist",
+            uponReceiving: `add ${role}`,
+            withRequest: {
+              method: "POST",
+              path: "/api/cases/1/civilians",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: {
+                roleOnCase: role,
+                caseId: 1,
+                civilianTitleId: 2,
                 firstName: "Andrew",
                 lastName: "Rist",
-                roleOnCase: "Complainant",
-                email: "fakeemail@email.com",
-                isAnonymous: false,
-                createdAt: "2022-09-29T18:24:28.907Z",
-                updatedAt: "2022-09-29T18:24:28.907Z",
-                caseId: 1,
-                raceEthnicityId: 17,
                 genderIdentityId: 1,
-                civilianTitleId: 2,
-                raceEthnicity: {
-                  id: 17,
-                  name: "White",
-                  createdAt: "2019-01-07T20:08:30.512Z",
-                  updatedAt: "2019-01-07T20:08:30.512Z"
+                raceEthnicityId: 2,
+                email: "fakeemail@email.com"
+              }
+            },
+            willRespondWith: {
+              status: 201,
+              body: like({
+                primaryComplainant:
+                  role === "Complainant" ? civilian : undefined,
+                caseReferencePrefix: "CC",
+                caseReference: "CC2020-0482",
+                id: 1,
+                complaintType: "Civilian Initiated",
+                statusId: 4,
+                year: 2020,
+                caseNumber: 482,
+                firstContactDate: "2020-08-14",
+                incidentDate: "2017-12-20",
+                intakeSourceId: 3,
+                incidentTime: "02:31:00",
+                incidentTimezone: "CST",
+                narrativeSummary: "test summary data",
+                narrativeDetails: "<p>test details data</p>",
+                createdBy: "noipm.infrastructure@gmail.com",
+                assignedTo: "noipm.infrastructure@gmail.com",
+                createdAt: "2020-08-14T14:30:31.302Z",
+                updatedAt: "2020-08-14T15:37:23.668Z",
+                intakeSource: {
+                  id: 3,
+                  name: "In Person",
+                  createdAt: "2018-12-21T02:07:39.872Z",
+                  updatedAt: "2018-12-21T02:07:39.872Z"
                 },
-                genderIdentity: {
-                  id: 1,
-                  name: "Male",
-                  createdAt: "2019-04-19T21:31:23.009Z",
-                  updatedAt: "2019-04-19T21:31:23.009Z"
-                }
-              },
-              caseReferencePrefix: "CC",
-              caseReference: "CC2020-0482",
-              id: 1,
-              complaintType: "Civilian Initiated",
-              statusId: 4,
-              year: 2020,
-              caseNumber: 482,
-              firstContactDate: "2020-08-14",
-              incidentDate: "2017-12-20",
-              intakeSourceId: 3,
-              incidentTime: "02:31:00",
-              incidentTimezone: "CST",
-              narrativeSummary: "test summary data",
-              narrativeDetails: "<p>test details data</p>",
-              createdBy: "noipm.infrastructure@gmail.com",
-              assignedTo: "noipm.infrastructure@gmail.com",
-              createdAt: "2020-08-14T14:30:31.302Z",
-              updatedAt: "2020-08-14T15:37:23.668Z",
-              intakeSource: {
-                id: 3,
-                name: "In Person",
-                createdAt: "2018-12-21T02:07:39.872Z",
-                updatedAt: "2018-12-21T02:07:39.872Z"
-              },
-              complainantCivilians: [
-                {
-                  fullName: "Andrew Rist",
-                  id: 3944,
-                  firstName: "Andrew",
-                  lastName: "Rist",
-                  roleOnCase: "Complainant",
-                  email: "fakeemail@email.com",
-                  isAnonymous: false,
-                  createdAt: "2022-09-29T18:24:28.907Z",
-                  updatedAt: "2022-09-29T18:24:28.907Z",
-                  caseId: 1,
-                  raceEthnicityId: 17,
-                  genderIdentityId: 1,
-                  civilianTitleId: 2,
-                  raceEthnicity: {
-                    id: 17,
-                    name: "White",
-                    createdAt: "2019-01-07T20:08:30.512Z",
-                    updatedAt: "2019-01-07T20:08:30.512Z"
-                  },
-                  genderIdentity: {
-                    id: 1,
-                    name: "Male",
-                    createdAt: "2019-04-19T21:31:23.009Z",
-                    updatedAt: "2019-04-19T21:31:23.009Z"
-                  }
-                }
-              ],
-              witnessCivilians: [],
-              attachments: [],
-              accusedOfficers: [],
-              complainantOfficers: [],
-              witnessOfficers: [],
-              status: "Ready for Review",
-              pdfAvailable: false,
-              isArchived: false,
-              nextStatus: "Forwarded to Agency"
-            })
-          }
-        });
+                complainantCivilians: role === "Complainant" ? [civilian] : [],
+                witnessCivilians: role === "Witness" ? [civilian] : [],
+                attachments: [],
+                accusedOfficers: [],
+                complainantOfficers: [],
+                witnessOfficers: [],
+                status: "Ready for Review",
+                pdfAvailable: false,
+                isArchived: false,
+                nextStatus: "Forwarded to Agency"
+              })
+            }
+          });
 
-        const addButtons = await screen.findAllByTestId(
-          "addComplainantWitness"
-        );
+          const addButtons = await screen.findAllByTestId(
+            "addComplainantWitness"
+          );
 
-        userEvent.click(addButtons[0]);
-        userEvent.click(await screen.findByText("Civilian Complainant"));
+          userEvent.click(addButtons[addButtonIndex]);
+          userEvent.click(await screen.findByText(`Civilian ${role}`));
 
-        userEvent.click(await screen.findByTestId("firstNameInput"));
-        userEvent.type(screen.getByTestId("firstNameInput"), "Andrew");
-        userEvent.click(screen.getByTestId("titleInput"));
-        userEvent.click(await screen.findByText("Dr."));
-        userEvent.click(screen.getByTestId("lastNameInput"));
-        userEvent.type(screen.getByTestId("lastNameInput"), "Rist");
-        userEvent.click(screen.getByTestId("genderInput"));
-        userEvent.click(screen.getByText("Male"));
-        userEvent.click(screen.getByTestId("raceEthnicityInput"));
-        userEvent.click(screen.getByText("American Indian or Alaska Native"));
-        userEvent.click(screen.getByTestId("emailInput"));
-        userEvent.type(screen.getByTestId("emailInput"), "fakeemail@email.com");
-        userEvent.click(screen.getByTestId("submitEditCivilian"));
+          userEvent.click(await screen.findByTestId("firstNameInput"));
+          userEvent.type(screen.getByTestId("firstNameInput"), "Andrew");
+          userEvent.click(screen.getByTestId("titleInput"));
+          userEvent.click(await screen.findByText("Dr."));
+          userEvent.click(screen.getByTestId("lastNameInput"));
+          userEvent.type(screen.getByTestId("lastNameInput"), "Rist");
+          userEvent.click(screen.getByTestId("genderInput"));
+          userEvent.click(screen.getByText("Male"));
+          userEvent.click(screen.getByTestId("raceEthnicityInput"));
+          userEvent.click(screen.getByText("American Indian or Alaska Native"));
+          userEvent.click(screen.getByTestId("emailInput"));
+          userEvent.type(
+            screen.getByTestId("emailInput"),
+            "fakeemail@email.com"
+          );
+          userEvent.click(screen.getByTestId("submitEditCivilian"));
 
-        expect(await screen.findByText("Civilian was successfully created"))
-          .toBeInTheDocument;
+          expect(await screen.findByText("Civilian was successfully created"))
+            .toBeInTheDocument;
+        }, 200000);
       });
-    });
-  }
-);
+    }
+  );
+});
