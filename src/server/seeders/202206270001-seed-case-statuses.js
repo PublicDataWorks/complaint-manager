8739ecba13ab7be296f657676af1da68f75908de
Claire Holt <claire.holt@thoughtworks.com>
2022-12-06 15:35:31 -0700

    chore: Mob [#223] moved statuses to instance files for case status seeder

--- src/server/seeders/202206270001-seed-case-statuses.js
+++ src/server/seeders/202206270001-seed-case-statuses.js
@@ -1,21 +1,19 @@
 "use strict";
-
+const statuses = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/statuses`);
 const TABLE = "case_statuses";
 
 const INSERT_CASE_STATUSES = `INSERT INTO ${TABLE}(name, order_key) 
-  VALUES ('Initial', 0), 
-  ('Active', 1), 
-  ('Letter in Progress', 2), 
-  ('Ready for Review', 3), 
-  ('Forwarded to Agency', 4), 
-  ('Closed', 5)
-`;
+  VALUES `;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    let query = statuses.reduce((acc, elem, index) => {
+      return `${acc} ('${elem}', ${index}),`;
+    }, INSERT_CASE_STATUSES);
+    query = query.slice(0, -1);
     try {
       await queryInterface.sequelize.transaction(async transaction => {
-        await queryInterface.sequelize.query(INSERT_CASE_STATUSES, {
+        await queryInterface.sequelize.query(query, {
           transaction
         });
       });
