f65c1a118862b3f43f1b419685a375c650ace581
Syamala Neeraja <syamalaneeraja@Syamalas-MacBook-Pro.local>
2022-12-07 14:27:20 -0600

    chore: MOB [#223] seed feature toggles

--- src/server/seeders/202209220000-seed-feature-toggles.js
+++ src/server/seeders/202209220000-seed-feature-toggles.js
@@ -1,23 +1,21 @@
 "use strict";
 
+const features = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/features.json`);
+
 const INSERT_FEATURES = `INSERT INTO feature_toggles (name, description, enabled, is_dev)
-    VALUES ('allowAccusedOfficersToBeBlankFeature', 'Accused can be blank', FALSE, FALSE),
-    ('styleGuideFeature', 'Style Guide Feature', FALSE, FALSE),
-    ('removeCaseNotesFeature', 'Case notes can be removed', FALSE, FALSE),
-    ('realtimeNotificationsFeature', 'Realtime Notifications using Server-Sent Events', TRUE, FALSE),
-    ('nonUserAuthenticationFeature', 'Authenticate through Auth0 without user credentials', FALSE, FALSE),
-    ('searchCasesFeature', 'Search for cases using Elasticsearch', TRUE, FALSE),
-    ('mapVisualizationFeature', 'Show maps on the private dashboard page', TRUE, FALSE),
-    ('publicMapVisualizationFeature', 'Show maps on the public dashboard page', TRUE, FALSE),
-    ('countByDistrictVisualizationFeature', 'show a bar-graph of the number of complaints per district', TRUE, FALSE),
-    ('topAllegationsVisualizationFeature', 'show a bar-graph of the top 20 allegations', TRUE, FALSE)
-    `;
+    VALUES `;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    let query = features.reduce((acc, elem) => {
+      return `${acc} ('${elem.name}', '${
+        elem.description
+      }', ${!!elem.enabled}, FALSE),`;
+    }, INSERT_FEATURES);
+    query = query.slice(0, -1);
     try {
       await queryInterface.sequelize.transaction(async transaction => {
-        await queryInterface.sequelize.query(INSERT_FEATURES, { transaction });
+        await queryInterface.sequelize.query(query, { transaction });
       });
     } catch (error) {
       throw new Error(
