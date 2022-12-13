9cb2e96b9d124a14310f1029199842db2e23c04c
Maria DelSignore <maria.delsignore@thoughtworks.com>
2022-12-13 11:46:07 -0700

    chore: Maria/Syamala [#231] add seeder for generate letter dropdown button

---
+++ src/server/seeders/2022121300000-seed-hawaii-feature-toggle.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const FEATURE_TOGGLES_TABLE = "feature_toggles";
+
+const ENABLED = process.env.ORG === "HAWAII" ? true : false;
+
+const INSERT_FEATURE_TOGGLE = `INSERT INTO ${FEATURE_TOGGLES_TABLE}(name, description, enabled, is_dev) 
+  VALUES ('chooseGenerateLetterButton', 'Allows you to choose between generate letter button dropdown and status button', ${ENABLED}, false)
+`;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(INSERT_FEATURE_TOGGLE, {
+          transaction
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while seeding data into ${FEATURE_TOGGLES_TABLE} table. Internal Error: ${error}`
+      );
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      `DELETE FROM ${FEATURE_TOGGLES_TABLE} 
+        WHERE name = 'chooseGenerateLetterButton';`
+    );
+  }
+};
