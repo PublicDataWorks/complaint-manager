07b795b0b5c40741b425fea3051f7ef21c1b8c1f
Andrew Rist <andrew.rist@thoughtworks.com>
2022-12-21 12:32:53 -0600

    chore: Jean/Andrew [#230] resets recommended actions counter on unseed

--- src/server/seeders/20181017163038-seed-recommended-actions.js
+++ src/server/seeders/20181017163038-seed-recommended-actions.js
@@ -12,5 +12,8 @@ module.exports = {
       "referral_letter_officer_recommended_actions"
     );
     await queryInterface.bulkDelete("recommended_actions");
+    await queryInterface.sequelize.query(
+      "ALTER SEQUENCE IF EXISTS recommended_actions_id_seq START 1 RESTART 1 MINVALUE 1"
+    );
   }
 };
