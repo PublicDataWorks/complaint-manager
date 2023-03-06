44d7cd3ec1a9725d4559065a20178d2c96f9d3e4
Andrew Rist <andrew.rist@thoughtworks.com>
2023-03-06 16:41:51 -0600

    chore: Andrew [#249] adds letter and letter image seeding for Hawaii

--- src/server/seeders/2022092100000-seed-letter-images-and-letter-types-letter-images.js
+++ src/server/seeders/2022092100000-seed-letter-images-and-letter-types-letter-images.js
@@ -4,66 +4,81 @@ const LETTER_IMAGES_TABLE = "letter_images";
 const LETTER_TYPES_LETTER_IMAGES_TABLE = "letter_types_letter_images";
 
 const INSERT_LETTER_IMAGES = `INSERT INTO ${LETTER_IMAGES_TABLE}(image) 
-  VALUES ('header_text.png'), ('icon.png')
+  VALUES ${
+    process.env.ORG === "HAWAII"
+      ? "('hawaii_header_text.jpeg'), ('hawaii_icon.jpeg')"
+      : "('header_text.png'), ('icon.png')"
+  }
 `;
 
 const INSERT_LETTER_TYPES_LETTER_IMAGES = `INSERT INTO ${LETTER_TYPES_LETTER_IMAGES_TABLE}(image_id, letter_id, max_width, name) 
-  VALUES 
-    ({headerImageId}, {referralLetterId}, '450px', 'header'), 
-    ({iconImageId}, {referralLetterId}, '60px', 'smallIcon'), 
-    ({headerImageId}, {complainantLetterId}, '450px', 'header'), 
-    ({iconImageId}, {complainantLetterId}, '60px', 'smallIcon')
+  VALUES ${
+    process.env.ORG === "HAWAII"
+      ? `({headerImageId}, {cannotHelpLetterId}, '450px', 'header'),
+        ({iconImageId}, {cannotHelpLetterId}, '60px', 'smallIcon')`
+      : `({headerImageId}, {referralLetterId}, '450px', 'header'), 
+        ({iconImageId}, {referralLetterId}, '60px', 'smallIcon'), 
+        ({headerImageId}, {complainantLetterId}, '450px', 'header'), 
+        ({iconImageId}, {complainantLetterId}, '60px', 'smallIcon')`
+  }
 `;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    if (process.env.ORG === "NOIPM") {
-      try {
-        await queryInterface.sequelize.transaction(async transaction => {
-          await queryInterface.sequelize.query(INSERT_LETTER_IMAGES, {
-            transaction
-          });
-
-          const imageId = await queryInterface.sequelize.query(
-            `SELECT id, image FROM ${LETTER_IMAGES_TABLE}`,
-            {
-              transaction
-            }
-          );
-          const letterId = await queryInterface.sequelize.query(
-            "SELECT id, type FROM letter_types",
-            {
-              transaction
-            }
-          );
-
-          let query = INSERT_LETTER_TYPES_LETTER_IMAGES;
-          imageId[0].forEach(element => {
-            query = query.replaceAll(
-              element.image === "header_text.png"
-                ? "{headerImageId}"
-                : "{iconImageId}",
-              element.id
-            );
-          });
-          letterId[0].forEach(element => {
-            query = query.replaceAll(
-              element.type === "REFERRAL"
-                ? "{referralLetterId}"
-                : "{complainantLetterId}",
-              element.id
-            );
-          });
-
-          await queryInterface.sequelize.query(query, {
-            transaction
-          });
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(INSERT_LETTER_IMAGES, {
+          transaction
         });
-      } catch (error) {
-        throw new Error(
-          `Error while seeding data into ${LETTER_IMAGES_TABLE} and ${LETTER_TYPES_LETTER_IMAGES_TABLE} tables. Internal Error: ${error}`
+
+        const imageId = await queryInterface.sequelize.query(
+          `SELECT id, image FROM ${LETTER_IMAGES_TABLE}`,
+          {
+            transaction
+          }
         );
-      }
+        const letterId = await queryInterface.sequelize.query(
+          "SELECT id, type FROM letter_types",
+          {
+            transaction
+          }
+        );
+
+        let query = INSERT_LETTER_TYPES_LETTER_IMAGES;
+        imageId[0].forEach(element => {
+          query = query.replaceAll(
+            element.image.includes("header")
+              ? "{headerImageId}"
+              : "{iconImageId}",
+            element.id
+          );
+        });
+        letterId[0].forEach(element => {
+          let templateVar;
+          switch (element.type) {
+            case "REFERRAL":
+              templateVar = "{referralLetterId}";
+              break;
+            case "COMPLAINANT":
+              templateVar = "{complainantLetterId}";
+              break;
+            case "CAN'T HELP":
+              templateVar = "{cannotHelpLetterId}";
+          }
+
+          if (templateVar) {
+            query = query.replaceAll(templateVar, element.id);
+          }
+        });
+
+        await queryInterface.sequelize.query(query, {
+          transaction
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while seeding data into ${LETTER_IMAGES_TABLE} and ${LETTER_TYPES_LETTER_IMAGES_TABLE} tables. Internal Error: ${error}`
+      );
     }
   },
 
