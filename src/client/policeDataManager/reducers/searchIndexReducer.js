ad98c3a53575dd5fd003cc4f9efd09dadeb35833
Maria DelSignore <maria.delsignore@thoughtworks.com>
2023-05-08 14:26:48 -0600

    chore: Maria/Andrew [#244] set searchable data as dirty when inmate is manually entered or case is created

--- src/client/policeDataManager/reducers/searchIndexReducer.js
+++ src/client/policeDataManager/reducers/searchIndexReducer.js
@@ -1,11 +1,13 @@
-import { submit, startSubmit } from "redux-form";
+import { submit, startSubmit, setSubmitSucceeded } from "redux-form";
 import { isEqual } from "lodash";
 import {
   ADD_OFFICER_TO_CASE_SUCCEEDED,
+  CASE_CREATED_SUCCESS,
   CIVILIAN_CREATION_SUCCEEDED,
   CREATE_CASE_TAG_SUCCESS,
   EDIT_CASE_OFFICER_SUCCEEDED,
   EDIT_CIVILIAN_SUCCESS,
+  MANUALLY_ENTER_INMATE_FORM,
   NARRATIVE_FORM,
   REMOVE_CASE_TAG_SUCCESS,
   SEARCHABLE_DATA_IS_CLEAN_AGAIN,
@@ -25,6 +27,13 @@ const searchIndexReducer = (state = initialState, action = {}) => {
     return true;
   }
 
+  const manuallyEnterInmateFormStartSubmitAction = setSubmitSucceeded(
+    MANUALLY_ENTER_INMATE_FORM
+  );
+  if (isEqual(action, manuallyEnterInmateFormStartSubmitAction)) {
+    return true;
+  }
+
   switch (action.type) {
     case SEARCHABLE_DATA_IS_CLEAN_AGAIN:
       return initialState;
@@ -34,6 +43,7 @@ const searchIndexReducer = (state = initialState, action = {}) => {
     case EDIT_CASE_OFFICER_SUCCEEDED:
     case CREATE_CASE_TAG_SUCCESS:
     case REMOVE_CASE_TAG_SUCCESS:
+    case CASE_CREATED_SUCCESS:
       return true;
     default:
       return state;
