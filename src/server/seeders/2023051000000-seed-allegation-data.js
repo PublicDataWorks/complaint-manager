036997d2ddd40b7b5f2f546ae86ed3d365b16be4
gumidyal <syamala.gumidyala@thoughtworks.com>
2023-05-11 16:00:21 -0500

    chore: Maria/Syamala [#270] updated allegations seeder

--- src/server/seeders/2023051000000-seed-allegation-data.js
+++ src/server/seeders/2023051000000-seed-allegation-data.js
@@ -4,6 +4,7 @@ const models = require("../policeDataManager/models/index");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("allegations");
     await loadCsvFromS3("allegations.csv", models.allegation);
   },
   down: async (queryInterface, Sequelize) => {
