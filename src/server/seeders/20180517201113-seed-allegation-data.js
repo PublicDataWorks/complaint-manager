746bda3dece7b6fe7a2a5886be757192bd1e0080
gumidyal <syamala.gumidyala@thoughtworks.com>
2023-05-11 17:13:35 -0500

    Revert "chore: Maria/Syamala [#270] updated allegations seeder"
    This reverts commit 036997d2ddd40b7b5f2f546ae86ed3d365b16be4.

--- src/server/seeders/20180517201113-seed-allegation-data.js
+++ src/server/seeders/20180517201113-seed-allegation-data.js
@@ -4,7 +4,6 @@ const models = require("../policeDataManager/models/index");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("allegations");
     await loadCsvFromS3("allegations.csv", models.allegation);
   },
   down: async (queryInterface, Sequelize) => {
