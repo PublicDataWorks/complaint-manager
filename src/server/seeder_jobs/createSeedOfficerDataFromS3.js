dd1ad51086566a7dfd13e544cdcab13d53c2b3ca
Jean Watts <jean.watts@thoughtworks.com>
2023-05-19 14:30:33 -0400

    chore: Andrew/Jean [#262] created seeder for updating roster data

--- src/server/seeder_jobs/createSeedOfficerDataFromS3.js
+++ src/server/seeder_jobs/createSeedOfficerDataFromS3.js
@@ -8,16 +8,105 @@ const createConfiguredS3Instance = require("../createConfiguredS3Instance");
 const config = require(`${process.env.REACT_APP_INSTANCE_FILES_DIR}/serverConfig`);
 const winston = require("winston");
 
-const promises = [];
-let counter = 0;
-const officersToUpdate = [];
-const officersToCreate = [];
-
 const createSeedOfficerDataFromS3 = async (
   officerFileName = "officerSeedData.csv",
   shouldCloseConnections = false
 ) => {
-  winston.info("File name", officerFileName);
+  const promises = [];
+  let counter = 0;
+  const officersToUpdate = [];
+  const officersToCreate = [];
+
+  const parseNullValues = value => {
+    if (value === "" || value === "NULL") return null;
+    return value;
+  };
+
+  const isEqualIgnoringType = (value1, value2) => {
+    if (value1 == value2) {
+      return true;
+    }
+  };
+
+  const rowDataIsOfficerToBeUpdated = (seedDataRow, existingOfficer) => {
+    const officerValuesToCompare = _.omit(existingOfficer.dataValues, [
+      "id",
+      "createdAt",
+      "updatedAt",
+      "districtId",
+      "dob"
+    ]);
+
+    const keysToPick = Object.keys(officerValuesToCompare);
+    const seedDataToCompare = _.pick(seedDataRow, keysToPick);
+
+    return !_.isEqualWith(
+      officerValuesToCompare,
+      seedDataToCompare,
+      isEqualIgnoringType
+    );
+  };
+
+  const transformDistrictToDistrictId = async seedDataRow => {
+    const ordinalDistrict = getOrdinalDistrict(seedDataRow.district);
+    if (ordinalDistrict) {
+      const foundDistrict = await models.district.findOne({
+        where: { name: ordinalDistrict }
+      });
+      if (foundDistrict) {
+        seedDataRow.districtId = foundDistrict.id;
+        seedDataRow.district = null;
+      }
+    }
+  };
+
+  const determineWhetherToCreateOrUpdateOfficer = async seedDataRow => {
+    const officerNumber = seedDataRow.officerNumber;
+    const existingOfficer = await models.officer.findOne({
+      where: { officerNumber }
+    });
+
+    if (existingOfficer) {
+      if (rowDataIsOfficerToBeUpdated(seedDataRow, existingOfficer)) {
+        await transformDistrictToDistrictId(seedDataRow);
+        officersToUpdate.push(seedDataRow);
+      }
+    } else {
+      await transformDistrictToDistrictId(seedDataRow);
+      officersToCreate.push(seedDataRow);
+    }
+  };
+
+  const updateOfficers = async officersToUpdate => {
+    if (officersToUpdate.length === 0) {
+      winston.info("No officers to update");
+      return;
+    }
+    let totalCount = 0;
+    let errorCount = 0;
+    for (let officerData of officersToUpdate) {
+      await models.officer
+        .update(officerData, {
+          where: { officerNumber: officerData.officerNumber }
+        })
+        .catch(error => {
+          winston.error(
+            `Error updating officer number ${officerData.officerNumber}; `,
+            error.message
+          );
+          errorCount++;
+        })
+        .then(() => {
+          totalCount++;
+        });
+    }
+    winston.info(
+      `Finished updating ${totalCount - errorCount} officers successfully`
+    );
+    winston.error(`${errorCount} officer updates failed`);
+  };
+
+  winston.info("File name: " + officerFileName);
   try {
     const s3 = createConfiguredS3Instance();
     const officerBucketName = config[process.env.NODE_ENV].officerBucket;
@@ -73,98 +162,10 @@ const createSeedOfficerDataFromS3 = async (
   } catch (error) {
     winston.error(`Officer Update Error: ${error}`);
     console.dir(error);
+    throw error;
   } finally {
     if (shouldCloseConnections) models.sequelize.close();
   }
-};
-
-const determineWhetherToCreateOrUpdateOfficer = async seedDataRow => {
-  const officerNumber = seedDataRow.officerNumber;
-  const existingOfficer = await models.officer.findOne({
-    where: { officerNumber }
-  });
-
-  if (existingOfficer) {
-    if (rowDataIsOfficerToBeUpdated(seedDataRow, existingOfficer)) {
-      await transformDistrictToDistrictId(seedDataRow);
-      officersToUpdate.push(seedDataRow);
-    }
-  } else {
-    await transformDistrictToDistrictId(seedDataRow);
-    officersToCreate.push(seedDataRow);
-  }
-};
-
-const transformDistrictToDistrictId = async seedDataRow => {
-  const ordinalDistrict = getOrdinalDistrict(seedDataRow.district);
-  if (ordinalDistrict) {
-    const foundDistrict = await models.district.findOne({
-      where: { name: ordinalDistrict }
-    });
-    if (foundDistrict) {
-      seedDataRow.districtId = foundDistrict.id;
-      seedDataRow.district = null;
-    }
-  }
-};
-
-const updateOfficers = async officersToUpdate => {
-  if (officersToUpdate.length === 0) {
-    winston.info("No officers to update");
-    return;
-  }
-  let totalCount = 0;
-  let errorCount = 0;
-  for (let officerData of officersToUpdate) {
-    await models.officer
-      .update(officerData, {
-        where: { officerNumber: officerData.officerNumber }
-      })
-      .catch(error => {
-        winston.error(
-          `Error updating officer number ${officerData.officerNumber}; `,
-          error.message
-        );
-        errorCount++;
-      })
-      .then(() => {
-        totalCount++;
-      });
-  }
-  winston.info(
-    `Finished updating ${totalCount - errorCount} officers successfully`
-  );
-  winston.error(`${errorCount} officer updates failed`);
-};
-
-const parseNullValues = value => {
-  if (value === "" || value === "NULL") return null;
-  return value;
-};
-
-const isEqualIgnoringType = (value1, value2) => {
-  if (value1 == value2) {
-    return true;
-  }
-};
-
-const rowDataIsOfficerToBeUpdated = (seedDataRow, existingOfficer) => {
-  const officerValuesToCompare = _.omit(existingOfficer.dataValues, [
-    "id",
-    "createdAt",
-    "updatedAt",
-    "districtId",
-    "dob"
-  ]);
-
-  const keysToPick = Object.keys(officerValuesToCompare);
-  const seedDataToCompare = _.pick(seedDataRow, keysToPick);
-
-  return !_.isEqualWith(
-    officerValuesToCompare,
-    seedDataToCompare,
-    isEqualIgnoringType
-  );
 };
 
 module.exports = createSeedOfficerDataFromS3;
