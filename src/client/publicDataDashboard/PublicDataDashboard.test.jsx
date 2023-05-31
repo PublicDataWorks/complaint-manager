6ac8cdf0508383180dc56b536e06705e59a7859a
Isabel Olson <isabel.olson@thoughtworks.com>
2023-05-31 16:05:52 -0500

    chore: Isabel [#000] adds jsx test for public data dashboard

--- src/client/publicDataDashboard/PublicDataDashboard.test.jsx
+++ src/client/publicDataDashboard/PublicDataDashboard.test.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import React from "react";
 import PublicDataDashboard from "./PublicDataDashboard";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-
-import { render } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
+import createConfiguredStore from "../createConfiguredStore";
 expect.extend(toHaveNoViolations);
 
 it("should have no a11y errors caught by jest-axe", async () => {
+  const store = createConfiguredStore();
+
   const { container } = render(
     <Provider store={store}>
       <Router>
