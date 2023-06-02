b8672798ea2cb2d9bd302b26dbd4a479f5a64fc3
Andrew Rist <andrew.rist@thoughtworks.com>
2023-06-02 12:03:27 -0500

    test: Andrew [#271] adds test for adding allegation

--- src/client/policeDataManager/allegations/AllegationDetailsForm.test.js
+++ src/client/policeDataManager/allegations/AllegationDetailsForm.test.js
@@ -6,7 +6,10 @@ import { mount } from "enzyme";
 import { Provider } from "react-redux";
 import AllegationDetailsForm from "./AllegationDetailsForm";
 import { changeInput, selectDropdownOption } from "../../testHelpers";
-import { ALLEGATION_SEVERITY } from "../../../sharedUtilities/constants";
+import {
+  ALLEGATION_SEVERITY,
+  GET_RULE_CHAPTERS_SUCCESS
+} from "../../../sharedUtilities/constants";
 
 jest.mock(
   "../cases/thunks/createOfficerAllegation",
@@ -36,6 +39,18 @@ describe("AllegationDetailsForm", () => {
     caseOfficerId = "4";
     successCallbackFunction = jest.fn();
     onSubmit = jest.fn();
+
+    store.dispatch({
+      type: GET_RULE_CHAPTERS_SUCCESS,
+      payload: [
+        { id: 1, name: "Chapter 1: Unexpected party" },
+        { id: 2, name: "Chapter 2: Roast mutton" },
+        { id: 3, name: "Chapter 3: Short rest" },
+        { id: 4, name: "Chapter 4: Over hill and under hill" },
+        { id: 5, name: "Chapter 5: Riddles in the dark" },
+        { id: 6, name: "Chapter 6: Out of the frying-pan into the fire" }
+      ]
+    });
 
     allegationDetailsForm = mount(
       <Provider store={store}>
@@ -118,5 +133,41 @@ describe("AllegationDetailsForm", () => {
       .last();
 
     expect(addButton.props().disabled).toBeFalsy();
+  });
+
+  test("should submit with all the stuff when when all the stuff is filled out", () => {
+    changeInput(
+      allegationDetailsForm,
+      '[data-testid="allegation-details-input"]',
+      "details"
+    );
+
+    selectDropdownOption(
+      allegationDetailsForm,
+      '[data-testid="allegation-severity-field"]',
+      ALLEGATION_SEVERITY.MEDIUM
+    );
+
+    selectDropdownOption(
+      allegationDetailsForm,
+      '[data-testid="rule-chapter-field"]',
+      "Chapter 4: Over hill and under hill"
+    );
+    allegationDetailsForm.update();
+
+    const addButton = allegationDetailsForm
+      .find('[data-testid="allegation-submit-btn"]')
+      .last();
+
+    addButton.simulate("click");
+    expect(onSubmit).toHaveBeenCalledWith(
+      {
+        ruleChapterId: 4,
+        severity: ALLEGATION_SEVERITY.MEDIUM,
+        details: "details"
+      },
+      dispatch,
+      expect.anything()
+    );
   });
 });
