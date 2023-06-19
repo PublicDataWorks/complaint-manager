ab0c45d14cbf18e6c7d29a14063c0468382790ec
Andrew Rist <andrew.rist@thoughtworks.com>
2023-06-19 17:15:29 -0500

    refactor: Andrew/Claire [#284] removes add case note dialog off of redux and onto local state WIP

--- src/client/policeDataManager/cases/CaseDetails/CaseNotes/CaseNotes.js
+++ src/client/policeDataManager/cases/CaseDetails/CaseNotes/CaseNotes.js
@@ -16,8 +16,17 @@ import { connect } from "react-redux";
 import { generateMenuOptions } from "../../../utilities/generateMenuOptions";
 import { userTimezone } from "../../../../common/helpers/userTimezone";
 import { USER_PERMISSIONS } from "../../../../../sharedUtilities/constants";
+import CaseNoteDialog from "../CaseNoteDialog/CaseNoteDialog";
 
 class CaseNotes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      caseNoteDialogOpen: false
+    };
+  }
+
   componentDidMount() {
     this.props.dispatch(getCaseNotes(this.props.caseId));
   }
@@ -84,6 +93,11 @@ class CaseNotes extends Component {
               })
             )}
           </div>
+          <CaseNoteDialog
+            open={this.state.caseNoteDialogOpen}
+            closeDialog={() => this.setState({ caseNoteDialogOpen: false })}
+            dialogType="Add"
+          />
           <RemoveCaseNoteDialog />
         </div>
         {this.props.permissions?.includes(USER_PERMISSIONS.CREATE_CASE_NOTE) ? (
@@ -96,7 +110,7 @@ class CaseNotes extends Component {
                     .format("YYYY-MM-DDTHH:mm")
                 })
               );
-              this.props.dispatch(openCaseNoteDialog("Add", {}));
+              this.setState({ caseNoteDialogOpen: true });
             }}
             style={{ margin: "0% 0% 5% 2%" }}
             data-testid="addCaseNoteButton"
