7c6868ce3ebd9c925b196e61de28c63cfc32fb73
Maria DelSignore <maria.delsignore@thoughtworks.com>
2023-07-19 10:23:16 -0600

    chore: Maria [#292] add section for desktop view to pass test to pass story along

--- src/client/publicDataDashboard/newPublicInfo/staffingShortage/StaffingShortage.js
+++ src/client/publicDataDashboard/newPublicInfo/staffingShortage/StaffingShortage.js
@@ -165,7 +165,73 @@ const StaffingShortage = props => {
       </section>
     );
   } else {
-    return <h1>HELLO</h1>;
+    return (
+      <section className={props.classes.staffingShortageContainer}>
+        <Typography
+          variant="h2"
+          className={`${props.classes.h2} ${props.classes.staffingShortageSubHeader}`}
+        >
+          Overall Impression
+        </Typography>
+        <Typography
+          variant="h2"
+          className={props.classes.h2}
+          style={{ margin: "0px" }}
+        >
+          Staffing Shortage
+        </Typography>
+
+        <div
+          className={props.classes[`staffingShortageGrid-${props.screenSize}`]}
+        >
+          <Typography
+            style={cardStyles.staffingCard}
+            className={props.classes.greyBackground}
+          >
+            Serious overcrowding attributing to inhumane conditions,
+            specifically for jails.
+          </Typography>
+          <Typography
+            style={(cardStyles.staffingCard, cardStyles.statisticCard)}
+            className={`${props.classes.navyBackground} ${props.classes.lightFontColor}`}
+          >
+            <span style={cardStyles.statisticSpan}>93%</span>
+            <br /> Prisons Are Above Capacity
+          </Typography>
+          <img
+            style={cardStyles.imageTablet}
+            className={props.classes.hawaiiMapImg}
+            src={`${config.frontendUrl}/images/Hawaii-Map-Prison.png`}
+          />
+          <Typography
+            style={(cardStyles.staffingCard, cardStyles.statisticCard)}
+            className={`${props.classes.forestBackground} ${props.classes.lightFontColor} ${props.classes.imgOverHawaiiMap}`}
+          >
+            <span style={cardStyles.statisticSpan}>156%</span>
+            <br /> Jails Are Above Capacity
+          </Typography>
+          <Typography
+            style={cardStyles.staffingCard}
+            className={props.classes.greyBackground}
+          >
+            Average rate across the country is 40-50%. Serious lack of movement
+            and out-of-cell time for those in custody.
+          </Typography>
+          <Typography
+            style={(cardStyles.staffingCard, cardStyles.statisticCard)}
+            className={`${props.classes.forestBackground} ${props.classes.lightFontColor}`}
+          >
+            <span style={cardStyles.statisticSpan}>80%</span>
+            <br /> Nearly 80% of People in Custody Are in Secure Settings
+          </Typography>
+          <img
+            style={cardStyles.imageTablet}
+            className={props.classes.inPrisonImg}
+            src={`${config.frontendUrl}/images/MenPrison_StockImg.jpeg`}
+          />
+        </div>
+      </section>
+    );
   }
 };
 
