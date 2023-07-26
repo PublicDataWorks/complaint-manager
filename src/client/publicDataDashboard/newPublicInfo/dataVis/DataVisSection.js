d1caa3a962aba26adf9c0d40ebbce469eec3dd50
Maria DelSignore <maria.delsignore@thoughtworks.com>
2023-07-26 10:35:01 -0600

    chore: Maria [#299] add error visual and other data about graph

--- src/client/publicDataDashboard/newPublicInfo/dataVis/DataVisSection.js
+++ src/client/publicDataDashboard/newPublicInfo/dataVis/DataVisSection.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import {
-  FormControl,
-  InputLabel,
+  Button,
+  Menu,
   MenuItem,
-  Select,
   Typography,
   withStyles
 } from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 import publicInfoStyles from "../publicInfoStyles";
 import { SCREEN_SIZES } from "../../../../sharedUtilities/constants";
+import useMenuControl from "../../../common/hooks/useMenuControl";
 
 const DataVisSection = ({ classes, screenSize }) => {
-  const [category, setCategory] = useState("");
-
-  console.log("anchorEl: ", anchorEl);
+  const [category, setCategory] = useState("Demographics");
+  const { menuOpen, anchorEl, handleMenuOpen, handleMenuClose } =
+    useMenuControl();
 
   const dropdownOptions = [
     screenSize === SCREEN_SIZES.DESKTOP
@@ -21,6 +23,45 @@ const DataVisSection = ({ classes, screenSize }) => {
       : "Facility Capacity",
     "Demographics"
   ];
+
+  const getCategoryTitle = selection => {
+    if (selection === "Facility Capacity") {
+      return "Facility Overcrowding Rates";
+    } else if (selection === "Demographics") {
+      return "Demographic Breakdown";
+    } else {
+      return "";
+    }
+  };
+
+  const getGraphInfo = selection => {
+    if (selection === "Facility Capacity") {
+      return "Hawaii’s jail facilities are chronically overcrowded.";
+    } else if (selection === "Demographics") {
+      return "Hawaiian and Black communities are disproportionally impacted by incarceration";
+    } else {
+      return "";
+    }
+  };
+
+  const failedToLoad = () => {
+    return (
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <ErrorOutlineIcon fontSize="large" />
+        <Typography style={{ textAlign: "center", fontFamily: "inherit" }}>
+          Unable to load data. Please wait a moment and try again.
+        </Typography>
+      </div>
+    );
+  };
 
   return (
     <section style={{ margin: "1.5em", height: "90vh", fontFamily: "inherit" }}>
@@ -39,33 +80,94 @@ const DataVisSection = ({ classes, screenSize }) => {
           width: "100vw"
         }}
       />
-      <FormControl
-        variant="filled"
-        sx={{ m: 1, minWidth: 160 }}
-        style={{ width: "65%" }}
-        size="small"
-      >
-        <InputLabel
-          id="demo-simple-select-filled-label"
-          style={{ color: "black", fontFamily: "inherit" }}
+      <div style={{ width: "211px" }}>
+        <Button
+          variant="contained"
+          style={{
+            width: "211px",
+            fontFamily: "inherit",
+            fontWeight: "500",
+            fontSize: "20px",
+            color: "black",
+            display: "flex",
+            justifyContent: "space-between",
+            textTransform: "none",
+            borderRadius: "0",
+            backgroundColor: "#ECF1F4",
+            borderColor: "#ECF1F4",
+            boxShadow: "none"
+          }}
+          data-testid={"generate-letter-button"}
+          onClick={handleMenuOpen}
         >
           Category
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={category}
-          onChange={e => setCategory(e.target.value)}
-          label="Category"
-          style={{ fontFamily: "inherit" }}
+          <ExpandMoreIcon />
+        </Button>
+        <Menu
+          style={{ fontFamily: "Montserrat" }}
+          open={menuOpen}
+          onClose={handleMenuClose}
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: "bottom" }}
+          getContentAnchorEl={null}
         >
           {dropdownOptions.map(option => (
-            <MenuItem key={option} value={option}>
+            <MenuItem
+              key={option}
+              value={option}
+              style={{
+                width: "211px",
+                fontFamily: "inherit"
+              }}
+              onClick={() => (setCategory(option), handleMenuClose())}
+            >
               {option}
             </MenuItem>
           ))}
-        </Select>
-      </FormControl>
+        </Menu>
+      </div>
+      <div
+        style={{
+          fontFamily: "Montserrat",
+          height: "77%",
+          marginTop: "28px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between"
+        }}
+      >
+        <Typography
+          variant="h3"
+          style={{
+            fontFamily: "inherit",
+            textAlign: "center",
+            fontWeight: "700",
+            fontSize: "24px"
+          }}
+        >
+          {getCategoryTitle(category)}
+        </Typography>
+        <Typography
+          style={{
+            fontFamily: "inherit",
+            textAlign: "center",
+            fontWeight: "500",
+            fontSize: "16px"
+          }}
+        >
+          {getGraphInfo(category)}
+        </Typography>
+        <div style={{ height: "65%", border: ".5px solid black" }}>
+          {failedToLoad()}
+        </div>
+        <Typography
+          style={{ fontFamily: "inherit", fontWeight: "500", fontSize: "14px" }}
+        >
+          Source: Source: Bureau of Justice Statistics, Federal Justice
+          Statistics Program, 2021 (preliminary); US Census, 2022; and National
+          Prisoner Statistics, 2021.
+        </Typography>
+      </div>
     </section>
   );
 };
