d57202cf75983b2f20130d7e22eca7e6aa781355
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2023-08-02 13:50:43 -0400

    chore: Shakeel/Claire fix test for banner content

--- src/client/publicDataDashboard/newPublicInfo/PublicInfoBanner.test.js
+++ src/client/publicDataDashboard/newPublicInfo/PublicInfoBanner.test.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
-import PublicInfoBanner, { SUBTEXT, BUTTON_TEXT, BUTTON_URL } from "./PublicInfoBanner";
+import PublicInfoBanner, { SUBTEXT, BUTTON_TEXT } from "./PublicInfoBanner";
 import { SCREEN_SIZES } from "../../../sharedUtilities/constants";
 
 describe("Banner", () => {
-    Object.values(SCREEN_SIZES).forEach(size => {
-        test(`Expect Banner to contain text for ${size}`, () => {
-            render(<PublicInfoBanner classes={{}} screenSize={size} />);
-            expect(screen.getByText("for fairness, equality, and justice")).toBeInTheDocument;
-        })
-        test(`Expect Banner to contain subtext for ${size}`, () => {
-            render(<PublicInfoBanner classes={{}} screenSize={size} />);
-            expect(screen.getByText(SUBTEXT)).toBeInTheDocument;
-        }) 
-        test(`Expect Banner to contain button for ${size}`, () => {
-            render(<PublicInfoBanner classes={{}} screenSize={size} />);
-            expect(screen.getByText(BUTTON_TEXT)).toBeInTheDocument;
-        })
-    })
+  Object.values(SCREEN_SIZES).forEach(size => {
+    test(`Expect Banner to contain text for ${size}`, () => {
+      render(<PublicInfoBanner classes={{}} screenSize={size} />);
+      expect(screen.getByText("Inspiring Change")).toBeInTheDocument;
+    });
+    test(`Expect Banner to contain subtext for ${size}`, () => {
+      render(<PublicInfoBanner classes={{}} screenSize={size} />);
+      expect(screen.getByText(SUBTEXT)).toBeInTheDocument;
+    });
+    test(`Expect Banner to contain button for ${size}`, () => {
+      render(<PublicInfoBanner classes={{}} screenSize={size} />);
+      expect(screen.getByText(BUTTON_TEXT)).toBeInTheDocument;
+    });
+  });
 });
