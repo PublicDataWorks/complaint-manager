80a58dc27b3df77bab18dfa831a6c16aac13afe4
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2023-08-02 14:31:57 -0400

    chore: Shakeel/Claire remove space typo

--- src/client/publicDataDashboard/newPublicInfo/PublicInfoBanner.js
+++ src/client/publicDataDashboard/newPublicInfo/PublicInfoBanner.js
@@ -4,7 +4,7 @@ import publicInfoStyles from "./publicInfoStyles";
 import { Typography } from "@material-ui/core";
 
 export const SUBTEXT =
-  "A new report finds overcrowded prisons, inhumane facilities conditions, and low staffing is unprecedented. Learn how these issues are address to the community, legislators,  and the Department of Public Safety to ensure safer conditions.";
+  "A new report finds overcrowded prisons, inhumane facilities conditions, and low staffing is unprecedented. Learn how these issues are address to the community, legislators, and the Department of Public Safety to ensure safer conditions.";
 export const BUTTON_TEXT = "READ THE REPORT";
 export const BUTTON_URL =
   "https://hcsoc.hawaii.gov/wp-content/uploads/2023/01/2022-HCSOC-Annual-Report-FINAL.pdf";
