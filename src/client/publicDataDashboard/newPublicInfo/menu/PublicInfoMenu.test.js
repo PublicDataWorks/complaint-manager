422ecc89daf9e078f747a6a970364eb8681bf195
Maria DelSignore <maria.delsignore@thoughtworks.com>
2023-08-28 12:07:58 -0600

    test: Maria [#000] fix test for publicInfoMenu with new lingo

--- src/client/publicDataDashboard/newPublicInfo/menu/PublicInfoMenu.test.js
+++ src/client/publicDataDashboard/newPublicInfo/menu/PublicInfoMenu.test.js
@@ -6,17 +6,15 @@ import { SCREEN_SIZES } from "../../../../sharedUtilities/constants";
 const expectMenuLinksToExist = () => {
   expect(screen.getByText("About")).toBeInTheDocument;
   expect(screen.getByText("Public Data")).toBeInTheDocument;
+  expect(screen.getByText("Myths and Facts")).toBeInTheDocument;
   expect(screen.getByText("Issues")).toBeInTheDocument;
-  expect(screen.getByText("Stories")).toBeInTheDocument;
-  expect(screen.getByText("FAQ")).toBeInTheDocument;
 };
 
 const expectMenuLinksToNotExist = () => {
   expect(screen.queryByText("About")).toBeNull();
   expect(screen.queryByText("Public Data")).toBeNull();
+  expect(screen.queryByText("Myths and Facts")).toBeNull();
   expect(screen.queryByText("Issues")).toBeNull();
-  expect(screen.queryByText("Stories")).toBeNull();
-  expect(screen.queryByText("FAQ")).toBeNull();
 };
 
 describe("Public Info Menu", () => {
