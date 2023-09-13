952088bd8d97ddea758beff804cca8629893ce66
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2023-09-13 15:05:40 -0400

    feat: Shak/Maria [#302] Adds Facility Capacity graph to public data page

--- src/client/publicDataDashboard/newPublicInfo/dataVis/facilityCapacitySection/FacilityCapacityGraph.js
+++ src/client/publicDataDashboard/newPublicInfo/dataVis/facilityCapacitySection/FacilityCapacityGraph.js
@@ -4,21 +4,32 @@ import { withStyles } from "@material-ui/core";
 import Plot from "react-plotly.js";
 
 const FacilityCapacityGraph = ({ classes, screenSize }) => {
-    const data = [
-      {
-        x: ['Apples', 'Oranges', 'Bananas'],
-        y: [3, 2, 4],
-        type: 'bar'
-      }
-    ];
-  
-    const layout = {
-      title: 'Fruit Sales',
-      xaxis: { title: 'Fruit' },
-      yaxis: { title: 'Number of Sales' }
-    };
-  
-    return <Plot data={data} layout={layout} />;
+  const data = [
+    {
+      x: ["HCCC", "MCCC", "OCCC", "KCCC", "WCCC", "WCF", "KCF", "HCF"],
+      y: [
+        (272 / 126) * 100,
+        (278 / 296) * 100,
+        (994 / 778) * 100,
+        (137 / 128) * 100,
+        (182 / 240) * 100,
+        (165 / 334) * 100,
+        (95 / 160) * 100,
+        (879 / 1124) * 100
+      ],
+      type: "bar"
+    }
+  ];
+
+  const layout = {
+    yaxis: {
+      tickvals: ["50%", "100%", "150%"],
+      ticktext: ["50%", "100%", "150%"],
+      range: [0, 151]
+    }
   };
+
+  return <Plot data={data} layout={layout} />;
+};
 
 export default withStyles(dataVisStyles)(FacilityCapacityGraph);
