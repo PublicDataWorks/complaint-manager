48648f73004cdb7526a4de4cbab31b74061eb9ff
Helio Rui Ramos Freitas <helio.r@thoughtworks.com>
2023-11-30 14:13:01 -0600

    chore: Isabel/Helio [#344] Add priority_reason and priority_level to cases table

---
+++ src/server/migrations/20231130000000-add-prioritiy-level-to-cases.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const TABLE = "cases";
+
+const ADD_COLUMN_QUERY = `
+  ALTER TABLE ${TABLE}
+  ADD COLUMN IF NOT EXISTS priority_level_id INT
+`;
+
+const ADD_CONSTRAINT_QUERY = `
+  ALTER TABLE ${TABLE}
+  ADD CONSTRAINT case_priority_level_id_fk
+  FOREIGN KEY (priority_level_id)
+  REFERENCES priority_levels(id)
+`;
+
+const DROP_CONSTRAINT_QUERY = `
+  ALTER TABLE ${TABLE}
+  DROP CONSTRAINT case_priority_level_id_fk
+`;
+
+const DROP_COLUMN_QUERY = `
+  ALTER TABLE ${TABLE}
+  DROP COLUMN IF EXISTS priority_level_id
+`;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(ADD_COLUMN_QUERY, {
+          transaction
+        });
+        await queryInterface.sequelize.query(ADD_CONSTRAINT_QUERY, {
+          transaction
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while adding foreign key to ${TABLE} table. Internal Error: ${error}`
+      );
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(DROP_CONSTRAINT_QUERY, {
+          transaction
+        });
+        await queryInterface.sequelize.query(DROP_COLUMN_QUERY, {
+          transaction
+        });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while removing foreign key from ${TABLE} table. Internal Error: ${error}`
+      );
+    }
+  }
+};
