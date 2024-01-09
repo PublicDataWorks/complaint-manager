6fa4dc73ffb14b012a1cd747cb345822603d933d
justin anthony <10365068@bd.com>
2024-01-09 15:54:18 -0700

    Add error handling

--- src/client/policeDataManager/cases/thunks/getCaseDetails.js
+++ src/client/policeDataManager/cases/thunks/getCaseDetails.js
@@ -5,7 +5,9 @@ const getCaseDetails = caseId => async dispatch => {
   try {
     const caseDetailsResponse = await axios.get(`api/cases/${caseId}`);
     return dispatch(getCaseDetailsSuccess(caseDetailsResponse.data));
-  } catch (error) {}
+  } catch (error) {
+    console.error(` There was an error retrieving caseid: ${caseId}. Error from server: ${error});
+  }
 };
 
 export default getCaseDetails;
