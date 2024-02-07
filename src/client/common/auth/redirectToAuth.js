6287e958be4bd5021df8604a09023b98732b496c
Fara Akhatova <fara.akhatova@thoughtworks.com>
2024-02-07 14:22:10 -0700

    Fara[#321] add window onpopstate event

--- src/client/common/auth/redirectToAuth.js
+++ src/client/common/auth/redirectToAuth.js
@@ -1,5 +1,15 @@
 import { push } from "connected-react-router";
 
+// const redirectToAuth = dispatch => {
+//   if (
+//     window.location.pathname !== "/login" &&
+//     window.location.pathname !== "/callback"
+//   ) {
+//     localStorage.setItem("redirectUri", window.location.pathname);
+//   }
+//   dispatch(push("/login"));
+// };
+
 const redirectToAuth = dispatch => {
   if (
     window.location.pathname !== "/login" &&
@@ -7,6 +17,12 @@ const redirectToAuth = dispatch => {
   ) {
     localStorage.setItem("redirectUri", window.location.pathname);
   }
+
+  // Listen for the popstate event, which is fired when the user clicks the back button
+  window.onpopstate = () => {
+    dispatch(push(localStorage.getItem("redirectUri") || "/login"));
+  };
+
   dispatch(push("/login"));
 };
 
