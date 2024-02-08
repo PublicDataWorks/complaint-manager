38f9be406e56cc43da3a7f63e7cad9533f5c2f3b
Fara Akhatova <fara.akhatova@thoughtworks.com>
2024-02-08 10:51:15 -0700

    Fara[#321] remove commented out code

--- src/client/common/auth/redirectToAuth.js
+++ src/client/common/auth/redirectToAuth.js
@@ -1,15 +1,5 @@
 import { push } from "connected-react-router";
 
-// const redirectToAuth = dispatch => {
-//   if (
-//     window.location.pathname !== "/login" &&
-//     window.location.pathname !== "/callback"
-//   ) {
-//     localStorage.setItem("redirectUri", window.location.pathname);
-//   }
-//   dispatch(push("/login"));
-// };
-
 const redirectToAuth = dispatch => {
   if (
     window.location.pathname !== "/login" &&
