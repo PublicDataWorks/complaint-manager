bd3ee5e55a402ef542414954e77d7f2499ab04c7
Helio Rui Ramos Freitas <helio.r@thoughtworks.com>
2024-02-23 15:18:48 -0600

    chore: Helio [#000] Add swagger to document APIs

--- src/server/handlers/cases/caseNotes/retrieveCaseNotes.js
+++ src/server/handlers/cases/caseNotes/retrieveCaseNotes.js
@@ -9,6 +9,98 @@ const {
 const asyncMiddleWare = require("../../asyncMiddleware");
 const models = require("../../../policeDataManager/models/index");
 
+/**
+ * @swagger
+ * /cases/:caseId/case-notes:
+ *   get:
+ *     tags:
+ *      - case notes
+ *     summary: Retrieve a list of notes from a case
+ *     description: Returns a list of notes associated with the case.
+ *     parameters:
+ *      - name: caseId
+ *        in: path
+ *        description: Id of the case
+ *        required: true
+ *        type: integer
+ *        example: 1
+ *     responses:
+ *       200:
+ *         description: A list of notes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The note id
+ *                         example: 1
+ *                       notes:
+ *                         type: string
+ *                         description: notes
+ *                         example: notes taken in the case
+ *                       actionTakenAt:
+ *                         type: string
+ *                         description: ???
+ *                         example: 2024-02-23T19:33:00.000Z
+ *                       createdAt:
+ *                         type: string
+ *                         description: creation date
+ *                         example: 2024-02-23T19:33:30.546Z
+ *                       updatedAt:
+ *                         type: string
+ *                         description: creation date
+ *                         example: 2024-02-23T19:33:30.546Z
+ *                       deletedAt:
+ *                         type: string
+ *                         description: creation date
+ *                         example: null
+ *                       actionId:
+ *                         type: int
+ *                         description: creation date
+ *                         example: 2
+ *                       caseId:
+ *                         type: int
+ *                         description: creation date
+ *                         example: 1
+ *                       caseNoteActionId:
+ *                         type: int
+ *                         description: creation date
+ *                         example: 1
+ *                       caseNoteAction:
+ *                         type: object
+ *                         properties:
+ *                          id:
+ *                           type: integer
+ *                          name:
+ *                           type: string
+ *                          createdAt:
+ *                           type: string
+ *                          updatedAt:
+ *                           type: string
+ *                         example:
+ *                          id: 1
+ *                          name: string
+ *                          createdAt: 2024-02-22T20:04:30.682Z
+ *                          updatedAt: 2024-02-22T20:04:30.682Z
+ *                       author:
+ *                         type: object
+ *                         properties:
+ *                          name:
+ *                           type: string
+ *                          email:
+ *                           type: string
+ *                         example:
+ *                          name: string
+ *                          email: email@email.com
+ */
+
 const retrieveCaseNotes = asyncMiddleWare(async (request, response) => {
   const rawCaseNotes = await models.sequelize.transaction(async transaction => {
     const caseNotesAndAuditDetails = await getAllCaseNotesAndAuditDetails(
@@ -45,6 +137,7 @@ const retrieveCaseNotes = asyncMiddleWare(async (request, response) => {
       );
     })
     .catch(err => {
+      console.error(err);
       // Transaction has been rolled back
       throw err;
     });
