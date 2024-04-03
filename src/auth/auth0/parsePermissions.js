a20235ac99329f65f893fc12e51e7cc057a6792a
Helio Rui Ramos Freitas <helio.r@thoughtworks.com>
2024-04-03 08:35:10 -0500

    chore: Refactor parsePermissions.js

--- src/auth/auth0/parsePermissions.js
+++ src/auth/auth0/parsePermissions.js
@@ -1,15 +1,5 @@
-import { OPENID, PROFILE } from "../../sharedUtilities/constants";
-
 export const parsePermissions = decodedToken => {
-  const auth0Scope = Array.isArray(decodedToken.permissions)
-    ? decodedToken.permissions.join(" ")
+  return Array.isArray(decodedToken.permissions)
+    ? decodedToken.permissions
     : [];
-
-  if (!Boolean(auth0Scope)) {
-    return [];
-  }
-
-  const allScopeValues = auth0Scope.split(" ");
-  const ignoredValues = [OPENID, PROFILE];
-  return allScopeValues.filter(value => !ignoredValues.includes(value));
 };
