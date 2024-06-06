f33d5ccc6c56f37b3e32aeb0e3d6f813a6818664
Helio Rui Ramos Freitas <helio.r@thoughtworks.com>
2024-06-06 14:23:09 -0500

    chore: [#547] Add script to create housing_units table
    Co-authored-by: Mariah Sager <mariah.sager@thoughtworks.com>

---
+++ src/server/migrations/2024060600000-add-housing-unit-table.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const TABLE = "housing_units";
+const CREATE_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS ${TABLE} (
+  id serial PRIMARY KEY,
+  name VARCHAR (50) NOT NULL,
+  facility_id INT NOT NULL,
+  created_at TIMESTAMPTZ,
+  updated_at TIMESTAMPTZ
+)`;
+
+const ADD_FOREIGN_KEY = `ALTER TABLE ${TABLE}
+  ADD CONSTRAINT fk_facility_id
+  FOREIGN KEY (facility_id)
+  REFERENCES facilities(id)
+`;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async transaction => {
+        await queryInterface.sequelize.query(CREATE_TABLE_QUERY, {
+          transaction
+        });
+        await queryInterface.sequelize.query(ADD_FOREIGN_KEY, { transaction });
+      });
+    } catch (error) {
+      throw new Error(
+        `Error while creating ${TABLE} table. Internal Error: ${error}`
+      );
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {}
+};
