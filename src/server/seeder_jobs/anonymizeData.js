3890ed57e1b8c87f1e614cb72b48906b2622a0f4
Andrew Rist <andrew.rist@thoughtworks.com>
2024-06-10 14:46:52 -0500

    chore: Andrew [#630] creates basic anonymization script

--- src/server/seeder_jobs/anonymizeData.js
+++ src/server/seeder_jobs/anonymizeData.js
@@ -2,6 +2,23 @@ const models = require("../policeDataManager/models");
 const DataAnonymizer = require("data-anonymizer");
 
 const ANONYMIZED_FIELDS = {
+  action_audit: ["user"],
+  address: [
+    "streetAddress",
+    "streetAddress2",
+    "city",
+    "state",
+    "zipCode",
+    "country",
+    "intersection",
+    "lat",
+    "lng",
+    "placeId",
+    "additionalLocationInfo"
+  ],
+  attachment: ["fileName", "description"],
+  audit: ["user"],
+  case_note: ["user", "notes"],
   cases: [
     "narrativeDetails",
     "createdBy",
@@ -9,18 +26,132 @@ const ANONYMIZED_FIELDS = {
     "narrativeSummary",
     "caseNumber",
     "pibCaseNumber"
+  ],
+  caseInmate: [
+    "notes",
+    "firstName",
+    "middleInitial",
+    "lastName",
+    "suffix",
+    "inmateId",
+    "notFoundInmateId",
+    "facility"
+  ],
+  case_officer: [
+    "notes",
+    "firstName",
+    "middleName",
+    "lastName",
+    "employeeId",
+    "supervisorFirstName",
+    "supervisorMiddleName",
+    "supervisorLastName",
+    "supervisorEmployeeId",
+    "dob",
+    "endDate",
+    "hireDate",
+    "phoneNumber",
+    "email"
+  ],
+  civilian: [
+    "firstName",
+    "middleInitial",
+    "lastName",
+    "birthDate",
+    "suffix",
+    "email",
+    "phoneNumber",
+    "additionalInfo"
+  ],
+  complainant_letter: ["finalPdfFilename"],
+  data_change_audit: ["modelDescription", "snapshot", "changes"],
+  facility: ["name", "abbreviation", "address"],
+  file_audit: ["fileName"],
+  inmate: [
+    "inmateId",
+    "firstName",
+    "lastName",
+    "region",
+    "facility",
+    "locationSub1",
+    "locationSub2",
+    "locationSub3",
+    "locationSub4",
+    "housing",
+    "currentLocation",
+    "muster",
+    "classificationDate",
+    "bookingStartDate",
+    "tentativeReleaseDate",
+    "bookingEndDate",
+    "actualReleaseDate",
+    "dateOfBirth",
+    "age",
+    "dateDeathRecorded",
+    "sentenceLength",
+    "transferDate",
+    "address"
+  ],
+  legacy_data_change_audit: ["snapshot", "changes", "modelDescription", "user"],
+  letter_officer: ["historicalBehaviorNotes", "recommendedActionNotes"],
+  letter: [
+    "recipient",
+    "recipientAddress",
+    "sender",
+    "transcribedBy",
+    "editedLetterHtml",
+    "finalPdfFilename"
+  ],
+  notification: ["user"],
+  officer: [
+    "firstName",
+    "middleName",
+    "lastName",
+    "employeeId",
+    "supervisorEmployeeId",
+    "dob",
+    "endDate",
+    "hireDate"
+  ],
+  officer_allegation: ["details"],
+  referral_letter_officer_history_note: ["details", "pibCaseNumber"],
+  referral_letter: [
+    "recipient",
+    "sender",
+    "transcribedBy",
+    "editedLetterHtml",
+    "finalPdfFilename",
+    "recipientAddress"
   ]
 };
 
-const anonymizeTable = async (modelName, data) => {
+const anonymizeField = value => {
   const anonymizer = new DataAnonymizer();
+  if (value === null || value === undefined) {
+    return undefined;
+  } else if (typeof value === "number") {
+    return parseInt(anonymizer.anonymize("" + value));
+  } else if (Array.isArray(value)) {
+    return value.map(anonymizeField);
+  } else if (typeof value === "object") {
+    return Object.keys(value).reduce((acc, key) => {
+      acc[key] = anonymizeField(value[key]);
+      return acc;
+    }, {});
+  } else if (typeof value === "string") {
+    return anonymizer.anonymize(value);
+  } else {
+    return value;
+  }
+};
+
+const anonymizeTable = async (modelName, data) => {
   const fields = ANONYMIZED_FIELDS[modelName];
   return data.map(row => {
     const jsonRow = row.toJSON();
     return Object.keys(jsonRow).reduce((acc, key) => {
-      console.log(key, jsonRow[key]);
-      if (fields.includes(key) && jsonRow[key]) {
-        acc[key] = anonymizer.anonymize(jsonRow[key]);
+      if (fields && fields.includes(key) && jsonRow[key]) {
+        acc[key] = anonymizeField(jsonRow[key]);
       } else {
         acc[key] = jsonRow[key];
       }
@@ -32,14 +163,13 @@ const anonymizeTable = async (modelName, data) => {
 async function pullAnonymizedData() {
   const data = {};
 
-  //   for (const modelName in models) {
-  const modelName = "cases";
-  const model = models[modelName];
-  if (model.findAll) {
-    const results = await model.findAll();
-    data[modelName] = anonymizeTable(modelName, results);
+  for (const modelName in models) {
+    const model = models[modelName];
+    if (model.findAll) {
+      const results = await model.findAll();
+      data[modelName] = await anonymizeTable(modelName, results);
+    }
   }
-  //   }
 
   const jsonData = JSON.stringify(data);
   console.log(jsonData); // You can save or use the JSON data as per your requirement
