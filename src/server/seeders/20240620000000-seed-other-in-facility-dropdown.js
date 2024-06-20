16b3f940a5991fe73d44309740f7626237574ac3
Claire Holt <claire.holt@thoughtworks.com>
2024-06-20 14:47:14 -0600

    chore: Devin/Claire [#546] Delete cases with facility of other in facility seeder down

--- src/server/seeders/20240620000000-seed-other-in-facility-dropdown.js
+++ src/server/seeders/20240620000000-seed-other-in-facility-dropdown.js
@@ -1,14 +1,18 @@
 "use strict";
 const TABLE = "facilities";
 
-const INSERT_OTHER = `INSERT INTO ${TABLE}(abbreviation, "name")
+const INSERT_OTHER_INTO_FACILITIES = `INSERT INTO ${TABLE}(abbreviation, "name")
 VALUES ('Other','Other')`;
-const DELETE_OTHER = `DELETE FROM ${TABLE} WHERE abbreviation = 'Other'`;
+
+const DELETE_CASES_WITH_OTHER_FACILITYID = `DELETE FROM cases WHERE facility_id = (SELECT id FROM ${TABLE} WHERE abbreviation = 'Other')`;
+
+const DELETE_OTHER_FROM_FACILITIES = `DELETE FROM ${TABLE} WHERE abbreviation = 'Other'`;
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     try {
       if (process.env.ORG === "HAWAII") {
-        await queryInterface.sequelize.query(INSERT_OTHER);
+        await queryInterface.sequelize.query(INSERT_OTHER_INTO_FACILITIES);
       }
     } catch (error) {
       throw new Error(
@@ -20,7 +24,10 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     try {
       if (process.env.ORG === "HAWAII") {
-        await queryInterface.sequelize.query(DELETE_OTHER);
+        await queryInterface.sequelize.query(
+          DELETE_CASES_WITH_OTHER_FACILITYID
+        );
+        await queryInterface.sequelize.query(DELETE_OTHER_FROM_FACILITIES);
       }
     } catch (error) {
       throw new Error(
