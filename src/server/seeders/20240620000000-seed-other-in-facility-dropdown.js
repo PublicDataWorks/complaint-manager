b30d3533369d566983ef5aae0d67ab9569c928f4
Devon Ivory <devon.ivory1@thoughtworks.com>
2024-06-21 16:26:13 -0400

    chore:Claire/Devon[546] updated query to set facilityid to null in cases table

--- src/server/seeders/20240620000000-seed-other-in-facility-dropdown.js
+++ src/server/seeders/20240620000000-seed-other-in-facility-dropdown.js
@@ -1,11 +1,17 @@
 "use strict";
+
 const TABLE = "facilities";
 
 const INSERT_OTHER_INTO_FACILITIES = `INSERT INTO ${TABLE}(abbreviation, "name")
 VALUES ('Other','Other')`;
 
-const DELETE_CASES_WITH_OTHER_FACILITYID = `DELETE FROM cases WHERE facility_id = (SELECT id FROM ${TABLE} WHERE abbreviation = 'Other')`;
-
+const UPDATE_CASES_WITH_OTHER_FACILITYID = `UPDATE cases
+SET facility_id = NULL
+WHERE facility_id IN (
+    SELECT id
+    FROM facilities
+    WHERE name = 'other'
+)`;
 const DELETE_OTHER_FROM_FACILITIES = `DELETE FROM ${TABLE} WHERE abbreviation = 'Other'`;
 
 module.exports = {
@@ -24,10 +30,15 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     try {
       if (process.env.ORG === "HAWAII") {
-        await queryInterface.sequelize.query(
-          DELETE_CASES_WITH_OTHER_FACILITYID
-        );
-        await queryInterface.sequelize.query(DELETE_OTHER_FROM_FACILITIES);
+        await queryInterface.sequelize.transaction(async transaction => {
+          await queryInterface.sequelize.query(
+            UPDATE_CASES_WITH_OTHER_FACILITYID,
+            { transaction }
+          );
+          await queryInterface.sequelize.query(DELETE_OTHER_FROM_FACILITIES, {
+            transaction
+          });
+        });
       }
     } catch (error) {
       throw new Error(
