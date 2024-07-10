d4ae6252516a95ca060e13ca4ec27deed1b88425
Hanika D <hanikad@hanikas-mbp.mynetworksettings.com>
2024-07-10 14:36:06 -0400

    Hanika/Victoria --Adding log out and env variables in commands.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,26 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  env: {
+    url_okta_hawaii: "https://dev-68895481.okta.com/",
+    url_hawaii: "https://hcsoc-staging-0171a859e889.herokuapp.com",
+    username_hawaii: "pdm@publicdata.works",
+    password_hawaii: "",
+    url_auth0_noipm: "https://noipm-staging.auth0.com",
+    url_noipm: "https://noipm-staging.herokuapp.com",
+    username_noipm: "vwong@thoughtworks.com",
+    password_noipm: ""
+  },
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack"
     }
   },
-
   e2e: {
+    defaultCommandTimeout: 3000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     experimentalOriginDependencies: true
-  }
+  },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,35 @@
 
 // imports attachFile command
 import "cypress-file-upload";
+// require('@4tw/cypress-drag-drop')
+// require('cypress-downloadfile/lib/downloadFileCommand');
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+Cypress.Commands.add('hawaii_Logout', () => {
+  // Assuming the logout process involves clicking on a button to open a logout menu
+  cy.get('.MuiIconButton-label > :nth-child(1) > [data-testid="tooltip-CM"]').click();
+  // And then clicking on the actual logout button
+  cy.get('[data-testid="logOutButton"]').click();
+  // Optionally, add assertions here to verify successful logout, e.g., checking for the login URL
+  cy.url().should('contain', 'https://hcsoc-staging-0171a859e889.herokuapp.com/login'); // Adjust the URL to match your application's logged-out state
+});
+Cypress.Commands.add('noipmLogout', () => {
+    // Assuming the logout process involves clicking on a button to open a logout menu
+    cy.get('.MuiIconButton-label > :nth-child(1) > [data-testid="tooltip-VW"]').click();
+    // And then clicking on the actual logout button
+    cy.get('[data-testid="logOutButton"]').click();
+  
+    // Optionally, add assertions here to verify successful logout, e.g., checking for the login URL
+    cy.url().should('contain', 'https://noipm-staging.auth0.com/login'); // Adjust the URL to match your application's logged-out state
+  });
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
