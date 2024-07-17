4aa43da4b6bc2068c09ee57e5ee65baf74787f58
Devon Ivory <devon.ivory1@thoughtworks.com>
2024-07-17 13:21:06 -0400

    chore:Devon/Elleni/Julio[#508] created sendemail handler and added emails to fake users

--- src/sharedUtilities/constants.js
+++ src/sharedUtilities/constants.js
@@ -695,8 +695,14 @@ export const FAKE_USERS = [
   { email: "sameen.shaw@gmail.com", name: "Sameen Shaw" },
   { email: "seanathon@gmail.com", name: "Seanathon" },
   { email: "jsimms@oipm.gov", name: "John A Simms" },
+<<<<<<< Updated upstream
   { email: "devon.ivory1@thoughtworks.com", name: "Devon Ivory" },
   { email: "helio.r@thoughtworks.com", name: "Helio R" }
+=======
+  { email: "devon.ivory1@thoughtworks.com", name: "Devon I" },
+  { email: "elleni.tessema1@thougthworks.com", name: "Elleni T" },
+  { email: "julio.espinola1@thoughtworks.com", name: "Julio E" }
+>>>>>>> Stashed changes
 ];
 
 export const SCREEN_SIZES = {
